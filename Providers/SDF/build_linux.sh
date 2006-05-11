e52d79123b63026066f5053572541be07fd46967
gregboone <gregboone@example.org>
2006-05-11 13:16:06 +0000

    FDO7 Add updated parameterized build_linux script
    git-svn-id: http://svn.osgeo.org/fdo/trunk@2142 d98eaee6-4212-0410-900c-dcc49bf69b99

--- Providers/SDF/build_linux.sh
+++ Providers/SDF/build_linux.sh
@@ -1,9 +1,88 @@
-aclocal
-libtoolize --force
-automake --add-missing --copy
-autoconf
-# uncomment to build in debug
-#./configure --enable-debug=yes
-./configure 
-make
+
+TYPEACTION=buildinstall
+TYPEBUILD=release
+
+### study parameters ###
+while test $# -gt 0
+do
+  arg="$1"
+  shift
+  case $arg in
+  --h | --help)
+    SHOWHELP=yes
+    break
+    ;;
+  --a | --action)
+    if test "$1" == buildinstall; then
+        TYPEACTION=buildinstall
+    elif test "$1" == buildonly; then
+        TYPEACTION=buildonly
+    elif test "$1" == installonly; then
+        TYPEACTION=installonly
+    else
+        echo "Invalid parameter"
+	exit 1
+    fi
+    shift
+    ;;
+  --c | --config)
+    if test "$1" == debug; then
+        TYPEBUILD=debug
+    elif test "$1" == release; then
+        TYPEBUILD=release
+    else
+        echo "Invalid parameter"
+	exit 1
+    fi
+    shift
+    ;;
+  -*)
+    echo "The command option is not recognized: $arg"
+    echo "Please use the format:"
+    SHOWHELP=yes
+    break
+    ;;
+
+  *)
+    echo "The command is not recognized."
+    echo "Please use the format:"
+    SHOWHELP=yes
+    break
+    ;;
+  esac
+done
+### end of study parameters ###
+
+if test "$SHOWHELP" == yes; then
+   echo "**************************************************************************"
+   echo "build_linux.sh [--h] [-c=BuildType] [-a=Action] "
+   echo "*"
+   echo "Help:           --h[elp]"
+   echo "BuildType:      --c[onfig] release(default), debug"
+   echo "Action:         --a[ction] buildinstall(default), buildonly, installonly"
+   echo "**************************************************************************"
+   exit 0
+fi
+
+### start build ###
+if test "$TYPEACTION" == buildinstall && test "$TYPEACTION" == buildonly ; then
+   aclocal
+   libtoolize --force
+   automake --add-missing --copy
+   autoconf
+
+   if test "$TYPEBUILD" == release; then
+      ./configure
+   else
+      ./configure --enable-debug=yes
+   fi
+   
+   make
+fi
+
+if test "$TYPEACTION" == buildinstall && test "$TYPEACTION" == installonly ; then
+   make install
+fi
+
+exit 0
 
