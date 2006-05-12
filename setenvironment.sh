db59a6694f6dd6933c3f69fc850d4b8086c384a5
gregboone <gregboone@example.org>
2006-05-12 20:13:04 +0000

    FDO2
    git-svn-id: http://svn.osgeo.org/fdo/trunk@77 d98eaee6-4212-0410-900c-dcc49bf69b99

--- setenvironment.sh
+++ setenvironment.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-
 export FDO=$PWD/Fdo
 export FDOTHIRDPARTY=$PWD/Thirdparty
 export FDOUTILITIES=$PWD/Utilities
 export SDEHOME=$FDOTHIRDPARTY/ESRI/ArcSDEClient91/Linux
 export FDOMYSQL=$FDOTHIRDPARTY/mysql/rhlinux
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/fdo-3.2.0/lib:$FDOTHIRDPARTY/linux/cppunit/lib:$(FDOTHIRDPARTY)/apache/xml-xalan/c/lib:$FDOTHIRDPARTY/apache/xml-xerces/c/lib
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/fdo-3.2.0/lib:$FDOTHIRDPARTY/linux/cppunit/lib:$FDOTHIRDPARTY/apache/xml-xalan/c/lib:$FDOTHIRDPARTY/apache/xml-xerces/c/lib
+
