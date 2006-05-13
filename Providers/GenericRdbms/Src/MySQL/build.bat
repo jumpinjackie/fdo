b4bb1bc3193e6f59713053756767f92dbe98ad6d
gregboone <gregboone@example.org>
2006-05-13 13:07:51 +0000

    FDO6
    git-svn-id: http://svn.osgeo.org/fdo/trunk@1264 d98eaee6-4212-0410-900c-dcc49bf69b99

--- Providers/GenericRdbms/Src/MySQL/build.bat
+++ Providers/GenericRdbms/Src/MySQL/build.bat
@@ -77,11 +77,6 @@ if not exist "%FDOUTILITIES%" goto env_path_error
 SET FDOACTENVSTUDY="FDOMYSQL"
 if ("%FDOMYSQL%")==("") goto env_error
 if not exist "%FDOMYSQL%" goto env_path_error
-SET FDOACTENVSTUDY="Sed & Bison"
-if exist %FDO%\Err.log del /F /Q %FDO%\Err.log
-SET FDOACTIVEPATHCHECK=GnuWin32\bin
-cscript //job:envcheck ../../../../preparebuilds.wsf
-if exist %FDO%\Err.log goto env_path_error_ex
 
 if "%TYPEACTIONMYSQL%"=="buildonly" goto start_exbuild
 if "%TYPEACTIONMYSQL%"=="clean" goto start_exbuild
@@ -158,19 +153,19 @@ time /t
 exit /B 1
 
 :env_path_error
-echo Environment variable invalid path: %FDOACTENVSTUDY%
+echo Invalid path contained in FDO environment variable: %FDOACTENVSTUDY%
 SET FDOERROR=1
 time /t
 exit /B 1
 
 :env_path_error_ex
-echo Unable to find path (in $PATH) to the : %FDOACTENVSTUDY%
+echo Unable to find location of %FDOACTENVSTUDY% in the Windows System PATH
 SET FDOERROR=1
 time /t
 exit /B 1
 
 :error
-echo There was a %MSACTIONMYSQL% error.
+echo There was a build error executing action: %MSACTIONFDO%
 time /t
 exit /B 1
 
