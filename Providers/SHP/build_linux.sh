705d94038b6b75cf1f9d7b8128980ee198e7db19
romicadascalescu <romicadascalescu@example.org>
2006-05-18 22:20:17 +0000

    FDO17
    git-svn-id: http://svn.osgeo.org/fdo/trunk@1980 d98eaee6-4212-0410-900c-dcc49bf69b99

--- Providers/SHP/build_linux.sh
+++ Providers/SHP/build_linux.sh
@@ -124,6 +124,28 @@ if test "$BUILDDOCS" == yes ; then
    pushd Docs/doc_src >& /dev/null
    doxygen Doxyfile_SHP >& /dev/null
    popd >& /dev/null
+
+fi
+
+if test "$TYPEACTION" == buildinstall || test "$TYPEACTION" == installonly ; then
+   if test -e "/usr/local/fdo-3.2.0/Docs/HTML/Providers/SHP"; then
+      rm -rf "/usr/local/fdo-3.2.0/Docs/HTML/Providers/SHP"
+   fi
+   if test ! -e "/usr/local/fdo-3.2.0"; then
+         mkdir "/usr/local/fdo-3.2.0"
+   fi
+   if test ! -e "/usr/local/fdo-3.2.0/Docs"; then
+      mkdir "/usr/local/fdo-3.2.0/Docs"
+   fi
+   if test ! -e "/usr/local/fdo-3.2.0/Docs/HTML"; then
+      mkdir "/usr/local/fdo-3.2.0/Docs/HTML"
+   fi
+   if test ! -e "/usr/local/fdo-3.2.0/Docs/HTML/Providers"; then
+      mkdir "/usr/local/fdo-3.2.0/Docs/HTML/Providers"
+   fi
+   if test -e "../Docs/HTML/Providers/SHP"; then
+      cp --force --recursive "../Docs/HTML/Providers/SHP" "/usr/local/fdo-3.2.0/Docs/HTML/Providers"
+   fi
 fi
 
 exit 0
