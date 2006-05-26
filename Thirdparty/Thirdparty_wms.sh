1f55df79bcecaea374eb6e0cb5d672599b64e6f1
romicadascalescu <romicadascalescu@example.org>
2006-05-26 18:48:23 +0000

    FDO25: R041-BJ2 WFS fix Defect 759651.01-Authentication Errors(port)
    git-svn-id: http://svn.osgeo.org/fdo/trunk@119 d98eaee6-4212-0410-900c-dcc49bf69b99

--- Thirdparty/Thirdparty_wms.sh
+++ Thirdparty/Thirdparty_wms.sh
@@ -24,9 +24,6 @@ make
 mkdir -p lib/linux
 rm -f ./lib/linux/*.*
 cp -f ./lib/.libs/libcurl.a ./lib/linux
-cp -f ./lib/.libs/libcurl.so ./lib/linux
-cp -f ./lib/.libs/libcurl.so.3 ./lib/linux
-cp -f ./lib/.libs/libcurl.so.3.0.0 ./lib/linux
 popd >& /dev/null
 
 pushd . >& /dev/null
@@ -35,17 +32,11 @@ echo Building openssl
 mkdir -p lib/linux
 rm -f ./lib/linux/*.*
 chmod a+x ./config
-./config shared
+./config
 make clean
 make
 mv -f ./libssl.a ./lib/linux/libssl.a
-mv -f ./libssl.so ./lib/linux/libssl.so
-mv -f ./libssl.so.0 ./lib/linux/libssl.so.0
-mv -f ./libssl.so.0.9.7 ./lib/linux/libssl.so.0.9.7
 mv -f ./libcrypto.a ./lib/linux/libcrypto.a
-mv -f ./libcrypto.so ./lib/linux/libcrypto.so
-mv -f ./libcrypto.so.0 ./lib/linux/libcrypto.so.0
-mv -f ./libcrypto.so.0.9.7 ./lib/linux/libcrypto.so.0.9.7
 popd >& /dev/null
 
 
