2cbeb92cbeb06c661a9f4439ab6d65dab294faa9
gregboone <gregboone@example.org>
2006-06-22 21:08:09 +0000

    FDO74: Update and Simplify FDO Build Scripts
    git-svn-id: http://svn.osgeo.org/fdo/trunk@170 d98eaee6-4212-0410-900c-dcc49bf69b99

--- setenvironment.bat
+++ setenvironment.bat
@@ -1,68 +1,54 @@
 @echo off
-time /t
-echo Start Settings
-
-SET ACTIVEPATHCHECK="C:\WINDOWS\Microsoft.NET\Framework\v2.0.50727"
-SET ACTIVENAMECHECK="Framework"
-if not exist %ACTIVEPATHCHECK% goto error
-set PATH=%PATH%;%ACTIVEPATHCHECK%
-
-SET ACTIVEPATHCHECK="C:\Program Files\ATT\Graphviz\bin"
-SET ACTIVENAMECHECK="Graphviz"
-if not exist %ACTIVEPATHCHECK% goto error
-set PATH=%PATH%;%ACTIVEPATHCHECK%
-
-SET ACTIVEPATHCHECK="c:\Program Files\GnuWin32\bin"
-SET ACTIVENAMECHECK="Sed & Bison"
-if not exist %ACTIVEPATHCHECK% goto error
-set PATH=%PATH%;%ACTIVEPATHCHECK%
-
-SET ACTIVEPATHCHECK="C:\Program Files\doxygen\bin"
-SET ACTIVENAMECHECK="doxygen"
-if not exist %ACTIVEPATHCHECK% goto error
-set PATH=%PATH%;%ACTIVEPATHCHECK%
-
-SET ACTIVEPATHCHECK="C:\Program Files\Microsoft Visual Studio 8\Common7\IDE"
-SET ACTIVENAMECHECK="Microsoft Visual Studio 8 IDE"
-if not exist %ACTIVEPATHCHECK% goto error
-set PATH=%PATH%;%ACTIVEPATHCHECK%
 
 SET FDO=%cd%\Fdo
 SET ACTIVEPATHCHECK=%FDO%
-SET ACTIVENAMECHECK="Fdo"
+SET ACTIVENAMECHECK="FDO"
 if not exist "%FDO%" goto error
+
+SET FDOUTILITIES=%cd%\Utilities
+SET ACTIVEPATHCHECK=%FDOUTILITIES%
+SET ACTIVENAMECHECK="FDO Utilities"
+if not exist "%FDOUTILITIES%" goto error
 
 SET FDOTHIRDPARTY=%cd%\Thirdparty
 SET ACTIVEPATHCHECK=%FDOTHIRDPARTY%
-SET ACTIVENAMECHECK="Fdo Thirdparty"
+SET ACTIVENAMECHECK="FDO Thirdparty"
 if not exist "%FDOTHIRDPARTY%" goto error
 
 SET XALANROOT=%FDOTHIRDPARTY%\apache\xml-xalan\c
 SET XERCESCROOT=%FDOTHIRDPARTY%\apache\xml-xerces\c
 SET NLSDIR=%FDOTHIRDPARTY%\apache\xml-xalan\c\Src\xalanc\NLS
 
-SET FDOUTILITIES=%cd%\Utilities
-SET ACTIVEPATHCHECK=%FDOUTILITIES%
-SET ACTIVENAMECHECK="Fdo Utilities"
-if not exist "%FDOUTILITIES%" goto error
+if not exist "%SDEHOME%" SET SDEHOME=%FDOTHIRDPARTY%\ESRI\ArcSDEClient91\Windows
+if not exist "%SDEHOME%" echo Optional FDO environment variable SDEHOME is not set
 
-SET SDEHOME=%FDOTHIRDPARTY%\ESRI\ArcSDEClient91\Windows
-SET ACTIVEPATHCHECK=%SDEHOME%
-SET ACTIVENAMECHECK="Fdo thirdparty ESRI libraries"
-if not exist "%SDEHOME%" goto error
+if not exist "%FDOMYSQL%" SET FDOMYSQL=C:\Program Files\MySQL\MySQL Server 5.0
+if not exist "%FDOMYSQL%" echo Optional FDO environment variable FDOMYSQL is not set
 
-SET FDOMYSQL=C:\Program Files\MySQL\MySQL Server 5.0
-SET ACTIVEPATHCHECK=%FDOMYSQL%
-SET ACTIVENAMECHECK="Fdo thirdparty MySQL libraries"
-if not exist "%FDOMYSQL%" goto error
+SET ACTIVEPATHCHECK="C:\WINDOWS\Microsoft.NET\Framework\v2.0.50727"
+SET ACTIVENAMECHECK="Microsoft .Net Framework"
+if not exist %ACTIVEPATHCHECK% goto error
+set PATH=%PATH%;%ACTIVEPATHCHECK%
+
+SET ACTIVENAMECHECK="Graphviz"
+SET ACTIVEPATHCHECK="C:\Program Files\ATT\Graphviz\bin"
+if exist %ACTIVEPATHCHECK% set PATH=%PATH%;%ACTIVEPATHCHECK%
+if not exist %ACTIVEPATHCHECK% echo Optional %ACTIVENAMECHECK% documentation package not recognized in the Windows PATH. Update the setenvironment.bat script file with the correct path
+
+SET ACTIVENAMECHECK="Doxygen"
+SET ACTIVEPATHCHECK="C:\Program Files\doxygen\bin"
+if exist %ACTIVEPATHCHECK% set PATH=%PATH%;%ACTIVEPATHCHECK%
+if not exist %ACTIVEPATHCHECK% echo Optional %ACTIVENAMECHECK% documentation package not recognized in the Windows PATH. Update the setenvironment.bat script file with the correct path
+
+SET ACTIVENAMECHECK="GnuWin32 Bison/Sed"
+SET ACTIVEPATHCHECK="c:\Program Files\GnuWin32\bin"
+if exist %ACTIVEPATHCHECK% set PATH=%PATH%;%ACTIVEPATHCHECK%
+if not exist %ACTIVEPATHCHECK% echo Optional %ACTIVENAMECHECK% package not recognized in the Windows PATH. Update the setenvironment.bat script file with the correct path
 
 :end
-echo End Settings
-time /t
 exit /B 0
 
 :error
 echo Unable to find %ACTIVEPATHCHECK%
-echo Please update script file with the right %ACTIVENAMECHECK% path
-time /t
+echo Please update setenvironment script file with the right %ACTIVENAMECHECK% path
 exit /B 1
