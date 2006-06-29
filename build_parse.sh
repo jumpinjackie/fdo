225814142a020ddf448a6aa6e0542bf0edbfb7f6
gregboone <gregboone@example.org>
2006-06-29 15:38:33 +0000

    FDO74
    git-svn-id: http://svn.osgeo.org/fdo/trunk@197 d98eaee6-4212-0410-900c-dcc49bf69b99

---
+++ build_parse.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+echo Parsing Fdo/Unmanaged/Src/Fdo/Parse/yyExpression.y
+yacc -dv Fdo/Unmanaged/Src/Fdo/Parse/yyExpression.y
+sed -f Fdo/Unmanaged/Src/Fdo/Parse/script_linux < y.tab.c >Fdo/Unmanaged/Src/Fdo/Parse/yyExpression.cpp
+rm y.tab.c
+sed -e '/^# *line/s/\\/\\\\/g' -e '/^# *line/b' -e 's/yy/fdo_expression_yy/g' -e 's/YY/FDO_EXPRESSION_YY/g' -e '/extern /D' < y.tab.h >Fdo/Unmanaged/Src/Fdo/Parse/yyExpression.h
+rm y.tab.h
+rm y.output
+
+echo Parsing Fdo/Unmanaged/Src/Fdo/Parse/yyFilter.y
+yacc -dv Fdo/Unmanaged/Src/Fdo/Parse/yyFilter.y
+sed -f Fdo/Unmanaged/Src/Fdo/Parse/script2_linux < y.tab.c >Fdo/Unmanaged/Src/Fdo/Parse/yyFilter.cpp
+rm y.tab.c
+sed -e '/^# *line/s/\\/\\\\/g' -e '/^# *line/b' -e 's/yy/fdo_filter_yy/g' -e 's/YY/FDO_FILTER_YY/g' -e '/extern /D' < y.tab.h >Fdo/Unmanaged/Src/Fdo/Parse/yyFilter.h
+rm  y.tab.h
+rm  y.output
+
+echo Parsing Fdo/Unmanaged/Src/Geometry/Parse/yyFgft.y
+yacc -d Fdo/Unmanaged/Src/Geometry/Parse/yyFgft.y
+sed -f Fdo/Unmanaged/Src/Geometry/Parse/script_linux < y.tab.c >Fdo/Unmanaged/Src/Geometry/Parse/yyFgft.cpp
+rm y.tab.c
+sed -e '/^# *line/s/\\/\\\\/g' -e '/^# *line/b' -e 's/yy/fdo_fgft_yy/g' -e 's/YY/FDO_FGFT_YY/g' -e '/extern /D' < y.tab.h > Fdo/Unmanaged/Src/Geometry/Parse/yyFgft.h
+rm y.tab.h
+
+echo Parsing Utilities/Common/Src/Parse/yyConstraint.y
+yacc -d Utilities/Common/Src/Parse/yyConstraint.y
+sed -f Utilities/Common/Src/Parse/script3_linux < y.tab.c >Utilities/Common/Src/Parse/yyConstraint.cpp
+rm y.tab.c
+sed -e '/^# *line/s/\\/\\\\/g' -e '/^# *line/b' -e 's/yy/fdo_constraint_yy/g' -e 's/YY/FDO_CONSTRAINT_YY/g' -e '/extern /D' < y.tab.h > Utilities/Common/Src/Parse/yyConstraint.h
+rm y.tab.h
+
+exit 0
+
