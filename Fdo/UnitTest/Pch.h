62436d9a45d294e586ce0276713ae36f2e54f1f0
gregboone <gregboone@example.org>
2006-07-11 23:37:48 +0000

    FDO89: Support Windows development using VCExpress (Remove Default Support For MFC)
    git-svn-id: http://svn.osgeo.org/fdo/trunk@252 d98eaee6-4212-0410-900c-dcc49bf69b99

--- Fdo/UnitTest/Pch.h
+++ Fdo/UnitTest/Pch.h
@@ -16,14 +16,7 @@
 #include <Fdo.h>
 
 #ifdef _WIN32
-
 #define VC_EXTRALEAN        // Exclude rarely-used stuff from Windows headers
-
-//#include <afxwin.h>         // MFC core and standard components
-//#include <afxext.h>         // MFC extensions
-//#ifndef _AFX_NO_AFXCMN_SUPPORT
-//#include <afxcmn.h>         // MFC support for Windows Common Controls
-//#endif // _AFX_NO_AFXCMN_SUPPORT
 #endif
 
 #include <cppunit/TestCase.h>
