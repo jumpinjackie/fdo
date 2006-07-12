196180bbe128259d627414c6f4a19a9f733c56ea
gregboone <gregboone@example.org>
2006-07-12 23:39:01 +0000

    FDO89 (Support Windows development using VCExpress)
    git-svn-id: http://svn.osgeo.org/fdo/trunk@264 d98eaee6-4212-0410-900c-dcc49bf69b99

--- Utilities/SchemaMgr/Src/Sm/Lp/stdafx.h
+++ Utilities/SchemaMgr/Src/Sm/Lp/stdafx.h
@@ -23,19 +23,8 @@
 
 #ifdef _WIN32
 #pragma once
-
 #define WIN32_LEAN_AND_MEAN		// Exclude rarely-used stuff from Windows headers
-#define _ATL_CSTRING_EXPLICIT_CONSTRUCTORS	// some Cwchar_t* constructors will be explicit
-
-#ifndef VC_EXTRALEAN
-#define VC_EXTRALEAN		// Exclude rarely-used stuff from Windows headers
 #endif
-
-#include <afx.h>
-#include <afxwin.h>         // MFC core and standard components
-
-#endif
-// TODO: reference additional headers your program requires here
 
 #include <stdio.h>
 #include <Fdo.h>
