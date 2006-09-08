355a4642f33063a0b323ee91b3f20103f004da01
romicadascalescu <romicadascalescu@example.org>
2006-09-08 16:57:33 +0000

    FDO31 - Fixed comment
    git-svn-id: http://svn.osgeo.org/fdo/trunk@933 d98eaee6-4212-0410-900c-dcc49bf69b99

--- Providers/GDAL/Src/Provider/FdoRfpConnection.cpp
+++ Providers/GDAL/Src/Provider/FdoRfpConnection.cpp
@@ -336,7 +336,7 @@ FdoConnectionState FdoRfpConnection::Open()
     m_defaultRasterLocation = dictionary->GetProperty (FdoGrfpGlobals::DefaultRasterFileLocation);
     
     FdoCommonConnStringParser parser (NULL, GetConnectionString ());
-    // check to see if connection string is valid and if it have unknown properties 
+    // check the validity of the connection string, i.e. it doesn’t contain unknown properties 
     // e.g. DefaultFLocation instead of DefaultFileLocation
     if (!parser.IsConnStringValid())
         throw FdoException::Create (NlsMsgGet1(GRFP_109_INVALID_CONNECTION_STRING, "Invalid connection string '%1$ls'", GetConnectionString ()));
