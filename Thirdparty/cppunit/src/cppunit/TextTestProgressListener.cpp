393c9a180e7676801b9d0dcbc42e83d199ea0ab2
pierredalcourt <pierredalcourt@example.org>
2006-09-19 18:59:27 +0000

    FDO180: cppunit: output test name and date/time (ported from Reliant R067-PD1)
    git-svn-id: http://svn.osgeo.org/fdo/trunk@371 d98eaee6-4212-0410-900c-dcc49bf69b99

--- Thirdparty/cppunit/src/cppunit/TextTestProgressListener.cpp
+++ Thirdparty/cppunit/src/cppunit/TextTestProgressListener.cpp
@@ -2,6 +2,18 @@
 #include <cppunit/TextTestProgressListener.h>
 #include <iostream>
 
+// CUSTOMIZED CODE - BEGIN:
+#include <cppunit/Test.h>
+#ifdef _WIN32
+#include "mbstring.h" // for multibyte functions
+#include <time.h>
+#else //_WIN32
+#include <wctype.h>  // for wide-char functions
+#include <inttypes.h>
+#include <unistd.h>
+#include <termios.h>
+#endif
+// CUSTOMIZED CODE - END.
 
 namespace CppUnit
 {
@@ -20,7 +32,23 @@ TextTestProgressListener::~TextTestProgressListener()
 void 
 TextTestProgressListener::startTest( Test *test )
 {
-  std::cerr << ".";
+  // CUSTOMIZED CODE - BEGIN:
+  struct tm systime;
+#ifdef _WIN32
+#pragma warning(disable : 4996)
+  _getsystime (&systime);
+#pragma warning(default : 4996)
+#else
+  time_t current;
+  time (&current);
+  localtime_r (&current, &systime);
+#endif
+  char datetimestr[100];
+  strftime(datetimestr, 100, "%c", &systime);
+
+  std::cerr << "\n" << test->getName() << " (" << datetimestr << "):\n";
+  // CUSTOMIZED CODE - END.
+
   std::cerr.flush();
 }
 
