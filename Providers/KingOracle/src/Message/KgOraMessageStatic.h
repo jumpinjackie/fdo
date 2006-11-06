4ff254b302326f7cf4baa22eb7eaadaecbffc7e2
Haris <Haris@example.org>
2006-11-06 23:55:09 +0000

    FDO253
    git-svn-id: http://svn.osgeo.org/fdo/trunk@991 d98eaee6-4212-0410-900c-dcc49bf69b99

---
+++ Providers/KingOracle/src/Message/KgOraMessageStatic.h
@@ -0,0 +1,40 @@
+/*
+* Copyright (C) 2006  SL-King d.o.o
+* 
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of version 2.1 of the GNU Lesser
+* General Public License as published by the Free Software Foundation.
+* 
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+* Lesser General Public License for more details.
+* 
+* You should have received a copy of the GNU Lesser General Public
+* License along with this library; if not, write to the Free Software
+* Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+
+
+#ifdef KGORA_MESSAGE_DEFINE
+
+#ifdef _WIN32
+char *g_KgOraMsgCat = "KingOracleMessage.dll";
+#else
+char *g_KgOraMsgCat = "KingOracleMessage.cat";
+#endif
+GisString* NlsMsgGet(int msg_num, char* default_msg, ...)
+{
+    va_list varargs;
+    va_start(varargs, default_msg);
+    GisString* ret = GisException::NLSGetMessage(msg_num, default_msg, g_KgOraMsgCat, varargs);
+    va_end(varargs);
+
+    return ret;
+}
+#else // KGORA_MESSAGE_DEFINE
+extern char *g_KgOraMsgCat;
+extern GisString* NlsMsgGet(int msg_num, char* default_msg, ...);
+#endif // KGORA_MESSAGE_DEFINE
+
