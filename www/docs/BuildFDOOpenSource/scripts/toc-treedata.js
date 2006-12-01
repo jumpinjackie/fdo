c97ee76b78a21eaaace3396d49440f507e512d2f
donaldcameron <donaldcameron@example.org>
2006-12-01 13:31:04 +0000

    FDO280
    git-svn-id: http://svn.osgeo.org/fdo/trunk@770 d98eaee6-4212-0410-900c-dcc49bf69b99

---
+++ www/docs/BuildFDOOpenSource/scripts/toc-treedata.js
@@ -0,0 +1,87 @@
+var tocItems = [["Building FDO on Windows", "./files/WS73099cc142f487551e5a0cb10850d4cd7c-596c.htm", "book-closed", ["Introduction", "./files/WS1a9193826455f5ff9110c71085341391d-24f9.htm", "page"],
+["Disk Space", "./files/WS73099cc142f48755b0eae110a8ea4f1d97d9.htm", "page"],
+["Install the Repository Tool", "./files/WS73099cc142f487551d92abb10dc573c45d-7e3e.htm", "page"],
+["Get the Source", "./files/WS1a9193826455f5ff36b67310b29445ec2-7fbc.htm", "book-closed", ["Subversion Repositories", "./files/WS1a9193826455f5ff36b67310b29445ec2-7fba.htm", "page"],
+],
+["Review the Copyright and Licensing Information", "./files/WS73099cc142f4875518cef0a10b4f5d51a8-7b49.htm", "page"],
+["Update the Source", "./files/WS1a9193826455f5ff36b67310b29445ec2-7f46.htm", "page"],
+["Install the Build Tools", "./files/WS1a9193826455f5ff13ee121107bdda7722-6898.htm", "book-closed", ["Binary Build Tools", "./files/WS73099cc142f48755b0eae110a8ea4f1d97d0.htm", "page"],
+["Documentation Build Tools", "./files/WS73099cc142f487551d92abb10dc573c45d-7e3c.htm", "page"],
+],
+["Install RDBMS Clients", "./files/WS73099cc142f48755b0eae110a8ea4f1d97d5.htm", "book-closed", ["ArcSDE 9.1 Client SDK", "./files/WS73099cc142f48755b0eae110a8ea4f1d97d6.htm", "page"],
+["MySQL Client", "./files/WS73099cc142f48755b0eae110a8ea4f1d97d8.htm", "page"],
+],
+["Set the Environment Variables", "./files/WS1a9193826455f5ff9110c71085341391d-4031.htm", "page"],
+["Modifying Microsoft Solution or Project Files", "./files/WS73099cc142f4875518cef0a10b4f5d51a8-7b47.htm", "page"],
+["Building FDO", "./files/WS73099cc142f487551e5a0cb10850d4cd7c-596a.htm", "book-closed", ["Build Scripts", "./files/WS73099cc142f487551d92abb10dc573c45d-7e3a.htm", "page"],
+["Build Order", "./files/WS1a9193826455f5ff1955f1510a86ab16de-57e1.htm", "page"],
+["Run the build_thirdparty.bat Script", "./files/WS73099cc142f4875518cef0a10b4f5d51a8-7b46.htm", "page"],
+["Optionally Run the build_parse.bat Script", "./files/WS73099cc142f4875518cef0a10b4f5d51a8-7b45.htm", "page"],
+["Run the build.bat Script", "./files/WS1a9193826455f5ff1955f1510a86ab16de-57f5.htm", "page"],
+],
+["Unit Tests", "./files/WS1a9193826455f5ff36b67310b29445ec2-7fb0.htm", "book-closed", ["Introduction", "./files/WS1a9193826455f5ff36b67310b29445ec2-7faf.htm", "page"],
+["Run the FDO Unit Tests", "./files/WS1a9193826455f5ff36b67310b29445ec2-7fac.htm", "page"],
+["Configure the ArcSDE Server", "./files/WS1a9193826455f5ff85ce5a10defe5d82a-7ffe.htm", "page"],
+["Run the ArcSDE Unit Tests", "./files/WS73099cc142f487551d92abb10dc573c45d-7e36.htm", "page"],
+["Run the GDAL Unit Tests", "./files/WS1a9193826455f5ff85ce5a10defe5d82a-7ffb.htm", "page"],
+["Run the MySQL Unit Tests", "./files/WS1a9193826455f5ff85ce5a10defe5d82a-7ff9.htm", "page"],
+["Configure the ODBC Unit Tests", "./files/WS1a9193826455f5ff85ce5a10defe5d82a-7f0d.htm", "page"],
+["Run the ODBC Unit Tests", "./files/WS1a9193826455f5ff85ce5a10defe5d82a-7ff7.htm", "page"],
+["Run the SDF Unit Tests", "./files/WS1a9193826455f5ff36b67310b29445ec2-7faa.htm", "page"],
+["Run the SHP Unit Tests", "./files/WS1a9193826455f5ff36b67310b29445ec2-7fa8.htm", "page"],
+["Run the WMS Unit Tests", "./files/WS73099cc142f487551d92abb10dc573c45d-7e34.htm", "page"],
+],
+],
+["Building FDO on Linux", "./files/WS1a9193826455f5ff1955f1510a86ab16de-56a1.htm", "book-closed", ["Introduction", "./files/WS73099cc142f4875520a47a10f192cc9dc-75d0.htm", "page"],
+["Disk Space", "./files/WS73099cc142f4875520a47a10f192cc9dc-75cf.htm", "page"],
+["Install the Repository Tool", "./files/WS73099cc142f4875520a47a10f192cc9dc-75ce.htm", "page"],
+["Get the Source", "./files/WS73099cc142f4875520a47a10f192cc9dc-75cd.htm", "page"],
+["Get the Source", "./files/WS73099cc142f4875520a47a10f192cc9dc-75cc.htm", "book-closed", ["Subversion Repositories", "./files/WS73099cc142f4875520a47a10f192cc9dc-75cb.htm", "page"],
+],
+["Review the Copyright and Licensing Information", "./files/WS73099cc142f4875520a47a10f192cc9dc-75ca.htm", "page"],
+["Updating the Source", "./files/WS1a9193826455f5ff36b67310b29445ec2-7f44.htm", "page"],
+["Install the Build Tools", "./files/WS73099cc142f4875520a47a10f192cc9dc-75c9.htm", "book-closed", ["Binary Build Tools", "./files/WS73099cc142f4875520a47a10f192cc9dc-75c8.htm", "page"],
+["Documentation Build Tools", "./files/WS1a9193826455f5ff1b17d1110e119dca16-7fe5.htm", "page"],
+],
+["Install RDBMS Clients", "./files/WS73099cc142f4875520a47a10f192cc9dc-75c7.htm", "book-closed", ["ArcSDE 9.1 Client SDK", "./files/WS73099cc142f4875520a47a10f192cc9dc-75c6.htm", "page"],
+["MySQL Client", "./files/WS73099cc142f4875520a47a10f192cc9dc-75c5.htm", "page"],
+["ODBC Oracle Driver", "./files/WS1a9193826455f5ff1eae43410b4d164b91-7f4a.htm", "page"],
+],
+["Environment Variables", "./files/WS1a9193826455f5ff36b67310b29445ec2-7fb7.htm", "page"],
+["Building FDO", "./files/WS73099cc142f4875520a47a10f192cc9dc-75c4.htm", "book-closed", ["Build Scripts", "./files/WS73099cc142f4875520a47a10f192cc9dc-75c3.htm", "page"],
+["Build Order", "./files/WS73099cc142f4875520a47a10f192cc9dc-75c2.htm", "page"],
+["Run the build_thirdparty.sh Script", "./files/WS73099cc142f4875520a47a10f192cc9dc-75c1.htm", "page"],
+["Optionally Run the build_parse.sh Script", "./files/WS73099cc142f4875520a47a10f192cc9dc-75c0.htm", "page"],
+["Run the build_linux.sh Script", "./files/WS73099cc142f4875520a47a10f192cc9dc-75bf.htm", "page"],
+],
+["Unit Tests", "./files/WS1a9193826455f5ff12ae99810b26fdce1b-7ff3.htm", "book-closed", ["Introduction", "./files/WS1a9193826455f5ff12ae99810b26fdce1b-7ff2.htm", "page"],
+["Run the FDO Unit Tests", "./files/WS73099cc142f4875520a47a10f192cc9dc-75be.htm", "page"],
+["Run the ArcSDE Unit Tests", "./files/WS73099cc142f4875520a47a10f192cc9dc-75bd.htm", "page"],
+["Run the GDAL Unit Tests", "./files/WS73099cc142f4875520a47a10f192cc9dc-75bc.htm", "page"],
+["Run the MySQL Unit Tests", "./files/WS73099cc142f4875520a47a10f192cc9dc-75bb.htm", "page"],
+["Run the ODBC Unit Tests", "./files/WS73099cc142f4875520a47a10f192cc9dc-75ba.htm", "page"],
+["Run the SDF Unit Tests", "./files/WS73099cc142f4875520a47a10f192cc9dc-75b9.htm", "page"],
+["Run the SHP Unit Tests", "./files/WS73099cc142f4875520a47a10f192cc9dc-75b8.htm", "page"],
+["Run the WMS Unit Tests", "./files/WS73099cc142f4875520a47a10f192cc9dc-75b7.htm", "page"],
+],
+],
+];
+            var fileItems = getTOCFileItems(tocItems);
+
+            function getTOCFileItems(tocItems)
+            {
+               var fItems = [];
+               if(!tocItems) return fItems;
+               for (var i = 0; i < tocItems.length; i++)
+               {
+                  if (typeof(tocItems[i]) == "object")
+                  {
+                     var childFItems = getTOCFileItems(tocItems[i]);
+                     for (var j = 0; j < childFItems.length; j++)
+                         fItems[fItems.length] = childFItems[j];
+                  }
+                  else if(i == 1) fItems[fItems.length] = tocItems[i];
+               }
+               return fItems;
+            }
+            
