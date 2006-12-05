55c4dccc4f06d6fc6dd9cd12c45232fb9f3ae811
gregboone <gregboone@example.org>
2006-12-05 15:18:37 +0000

    FDO112: Fix FDO Build Issues
    git-svn-id: http://svn.osgeo.org/fdo/branches/3.2.x@583 d98eaee6-4212-0410-900c-dcc49bf69b99

--- setenvironment.bat
+++ setenvironment.bat
@@ -36,37 +36,37 @@ SET XALANROOT=%FDOTHIRDPARTY%\apache\xml-xalan\c
 SET XERCESCROOT=%FDOTHIRDPARTY%\apache\xml-xerces\c
 SET NLSDIR=%FDOTHIRDPARTY%\apache\xml-xalan\c\Src\xalanc\NLS
 
-if not exist "%SDEHOME%" SET SDEHOME=%FDOTHIRDPARTY%\ESRI\ArcSDEClient91\Windows
-if not exist "%SDEHOME%" echo Optional FDO environment variable SDEHOME is not set
+if not exist %SDEHOME% SET SDEHOME="%FDOTHIRDPARTY%\ESRI\ArcSDEClient91\Windows"
+if not exist %SDEHOME% echo Optional FDO environment variable SDEHOME is not set
 
-if not exist "%FDOMYSQL%" SET FDOMYSQL=C:\Program Files\MySQL\MySQL Server 5.0
-if not exist "%FDOMYSQL%" echo Optional FDO environment variable FDOMYSQL is not set
+if not exist %FDOMYSQL% SET FDOMYSQL="%PROGRAMFILES%\MySQL\MySQL Server 5.0"
+if not exist %FDOMYSQL% echo Optional FDO environment variable FDOMYSQL is not set
 
 SET ACTIVENAMECHECK="Microsoft .Net Framework"
-SET ACTIVEPATHCHECK="C:\WINDOWS\Microsoft.NET\Framework\v2.0.50727"
+SET ACTIVEPATHCHECK="%windir%\Microsoft.NET\Framework\v2.0.50727"
 if not exist %ACTIVEPATHCHECK% goto error
 set PATH=%PATH%;%ACTIVEPATHCHECK%
 
 SET ACTIVENAMECHECK="Microsoft Visual Studio 8 IDE"
-SET ACTIVEPATHCHECK="C:\Program Files\Microsoft Visual Studio 8\Common7\IDE"
+SET ACTIVEPATHCHECK="%PROGRAMFILES%\Microsoft Visual Studio 8\Common7\IDE"
 if exist %ACTIVEPATHCHECK% set PATH=%PATH%;%ACTIVEPATHCHECK%
 
 SET ACTIVENAMECHECK="7-Zip"
-SET ACTIVEPATHCHECK="C:\Program Files\7-Zip"
+SET ACTIVEPATHCHECK="%PROGRAMFILES%\7-Zip"
 if exist %ACTIVEPATHCHECK% set PATH=%PATH%;%ACTIVEPATHCHECK%
 
 SET ACTIVENAMECHECK="Graphviz"
-SET ACTIVEPATHCHECK="C:\Program Files\ATT\Graphviz\bin"
+SET ACTIVEPATHCHECK="%PROGRAMFILES%\ATT\Graphviz\bin"
 if exist %ACTIVEPATHCHECK% set PATH=%PATH%;%ACTIVEPATHCHECK%
 if not exist %ACTIVEPATHCHECK% echo Optional %ACTIVENAMECHECK% documentation package not recognized in the Windows PATH. Update the setenvironment.bat script file with the correct path
 
 SET ACTIVENAMECHECK="Doxygen"
-SET ACTIVEPATHCHECK="C:\Program Files\doxygen\bin"
+SET ACTIVEPATHCHECK="%PROGRAMFILES%\doxygen\bin"
 if exist %ACTIVEPATHCHECK% set PATH=%PATH%;%ACTIVEPATHCHECK%
 if not exist %ACTIVEPATHCHECK% echo Optional %ACTIVENAMECHECK% documentation package not recognized in the Windows PATH. Update the setenvironment.bat script file with the correct path
 
 SET ACTIVENAMECHECK="GnuWin32 Bison/Sed"
-SET ACTIVEPATHCHECK="c:\Program Files\GnuWin32\bin"
+SET ACTIVEPATHCHECK="%PROGRAMFILES%\GnuWin32\bin"
 if exist %ACTIVEPATHCHECK% set PATH=%PATH%;%ACTIVEPATHCHECK%
 if not exist %ACTIVEPATHCHECK% echo Optional %ACTIVENAMECHECK% package not recognized in the Windows PATH. Update the setenvironment.bat script file with the correct path
 
