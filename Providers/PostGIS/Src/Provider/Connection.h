24fb41e6d92b2d6500019be55998e3f97d69cf30
mloskot <mloskot@example.org>
2007-02-02 02:36:18 +0000

    Skeleton of impl. of FdoIConnection and FdoIDisposable interfaces in Connection class.
    git-svn-id: http://svn.osgeo.org/fdo/trunk@2773 d98eaee6-4212-0410-900c-dcc49bf69b99

--- Providers/PostGIS/Src/Provider/Connection.h
+++ Providers/PostGIS/Src/Provider/Connection.h
@@ -26,18 +26,111 @@ namespace fdo { namespace postgis {
 
 
 /// Implementation of connection interface for PostGIS provider.
+/// It represents a unique session with FDO datastore.
+///
+/// \todo Make it noncopyable
 ///
 class Connection : public FdoIConnection
 {
 public:
 
+    /// Default constructor.
     Connection();
+
+    /// Destructor.
     virtual ~Connection();
+
+    //
+    // FdoIDisposable interface
+    //
+
+    /// Dispose this object.
+    void Dispose();
+
+    //
+    // FdoIConnection interface
+    //
+
+    // Get pointer to FdoIConnectionCapabilities interface describing
+    // capabilities of the connection.
+    FdoIConnectionCapabilities* GetConnectionCapabilities();
+
+    // Get pointer to FdoISchemaCapabilities interface describing
+    // support for the feature schema provided by PostGIS Provider.
+    FdoISchemaCapabilities* GetSchemaCapabilities();
+ 	
+    // Get pointer to FdoICommandCapabilities interface describing supported commands.
+    FdoICommandCapabilities* GetCommandCapabilities();
+
+    // Get pointer to FdoIFilterCapabilities interface describing 
+    // support for filters.
+    FdoIFilterCapabilities* GetFilterCapabilities();
+
+    // Get pointer to FdoIExpressionCapabilities interface describing
+    // support for expressions.
+    FdoIExpressionCapabilities* GetExpressionCapabilities();
+
+    // Get pointer to FdoIRasterCapabilities interface describing
+    // support for raster images.
+    FdoIRasterCapabilities* GetRasterCapabilities();
+
+    // Get pointer to FdoITopologyCapabilities interface describing support for topology.
+    FdoITopologyCapabilities* GetTopologyCapabilities();
+
+    // Get pointer to FdoIGeometryCapabilities interface describing support for geometry.
+    FdoIGeometryCapabilities* GetGeometryCapabilities();
+
+    // Get connection string used to establish connection with a DataStore.
+    FdoString* GetConnectionString();
+
+    // Set connection string used to establish connection with a DataStore.
+    void SetConnectionString(FdoString* value);
+
+    // Get pointer to FdoIConnectionInfo interface that can be used to interrogate
+    // and set connection properties.
+    FdoIConnectionInfo* GetConnectionInfo();
+
+    // Get state of current connection.
+    FdoConnectionState GetConnectionState();
+
+    // Get number of milliseconds to wait while trying to establish a connection
+    // before terminating the attempt and generating an error.
+    FdoInt32 GetConnectionTimeout();
+
+    // Set number of milliseconds to wait while trying to establish a connection
+    // before terminating the attempt and generating an error.
+    void SetConnectionTimeout(FdoInt32 value);
+
+    // Establish connection with a datastore using settings
+    // specified by ConnectionString attribute.
+    FdoConnectionState Open();
+
+    // Close current connection.
+    void Close();
+
+    // Begin transaction and return pointer to FdoITransaction interface realizing
+    // transaction operations support.
+    FdoITransaction* BeginTransaction();
+
+    // Create command object of specified type and return pointer to interface realizing
+    // command capabilities associated with the connection.
+    FdoICommand* CreateCommand(FdoInt32 type);
+
+    // Factory function that creates an empty Schema Override mapping specific
+    // for PostGIS Provider.
+    FdoPhysicalSchemaMapping* CreateSchemaMapping();
+
+    // Set XML configuration stream used to configure connected datastore.
+    void SetConfiguration(FdoIoStream* configStream);
+
+    // Force to write any cached data to currently connected datastore
+    void Flush();
 
 protected:
 
 private:
-	
+
+
 };
 
 }} // namespace fdo::postgis
