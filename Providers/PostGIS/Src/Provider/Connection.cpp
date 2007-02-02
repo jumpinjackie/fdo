edc5a1b8c484c3fdab5a5326d3f992871f5ed159
mloskot <mloskot@example.org>
2007-02-02 02:36:18 +0000

    Skeleton of impl. of FdoIConnection and FdoIDisposable interfaces in Connection class.
    git-svn-id: http://svn.osgeo.org/fdo/trunk@2773 d98eaee6-4212-0410-900c-dcc49bf69b99

--- Providers/PostGIS/Src/Provider/Connection.cpp
+++ Providers/PostGIS/Src/Provider/Connection.cpp
@@ -17,7 +17,9 @@
 #include "stdafx.h"
 
 #include "PostGisProvider.h"
-#include "FdoConnection.h"
+#include "Connection.h"
+
+#include <cassert>  
 
 // External access to connection for client services
 extern "C" FDOPOSTGIS_API FdoIConnection* CreateConnection()
@@ -33,6 +35,140 @@ Connection::Connection()
 
 Connection::~Connection()
 {
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// FdoIDisposable interface
+///////////////////////////////////////////////////////////////////////////////
+
+void Connection::Dispose()
+{
+    delete this;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// FdoIConnection interface
+///////////////////////////////////////////////////////////////////////////////
+
+FdoIConnectionCapabilities* Connection::GetConnectionCapabilities()
+{
+    assert(!"NOT IMPLEMENTED");
+    return NULL;
+}
+
+FdoISchemaCapabilities* Connection::GetSchemaCapabilities()
+{
+    assert(!"NOT IMPLEMENTED");
+    return NULL;
+}
+
+FdoICommandCapabilities* Connection::GetCommandCapabilities()
+{
+    assert(!"NOT IMPLEMENTED");
+    return NULL;
+}
+
+FdoIFilterCapabilities* Connection::GetFilterCapabilities()
+{
+    assert(!"NOT IMPLEMENTED");
+    return NULL;
+}
+
+FdoIExpressionCapabilities* Connection::GetExpressionCapabilities()
+{
+    assert(!"NOT IMPLEMENTED");
+    return NULL;
+}
+
+FdoIRasterCapabilities* Connection::GetRasterCapabilities()
+{
+    assert(!"NOT IMPLEMENTED");
+    return NULL;
+}
+
+FdoITopologyCapabilities* Connection::GetTopologyCapabilities()
+{
+    assert(!"NOT IMPLEMENTED");
+    return NULL;
+}
+
+FdoIGeometryCapabilities* Connection::GetGeometryCapabilities()
+{
+    assert(!"NOT IMPLEMENTED");
+    return NULL;
+}
+
+FdoString* Connection::GetConnectionString()
+{
+    assert(!"NOT IMPLEMENTED");
+    return NULL;
+}
+
+void Connection::SetConnectionString(FdoString* value)
+{
+    assert(!"NOT IMPLEMENTED");
+}
+
+FdoIConnectionInfo* Connection::GetConnectionInfo()
+{
+    assert(!"NOT IMPLEMENTED");
+    return NULL;
+}
+
+FdoConnectionState Connection::GetConnectionState()
+{
+    assert(!"NOT IMPLEMENTED");
+    return FdoConnectionState_Closed;
+}
+
+FdoInt32 Connection::GetConnectionTimeout()
+{
+    assert(!"NOT IMPLEMENTED");
+    return 0;
+}
+
+void Connection::SetConnectionTimeout(FdoInt32 value)
+{
+    assert(!"NOT IMPLEMENTED");
+}
+
+FdoConnectionState Connection::Open()
+{
+    assert(!"NOT IMPLEMENTED");
+    return FdoConnectionState_Closed;
+}
+
+void Connection::Close()
+{
+    assert(!"NOT IMPLEMENTED");
+}
+
+FdoITransaction* Connection::BeginTransaction()
+{
+    assert(!"NOT IMPLEMENTED");
+    return NULL;
+}
+
+FdoICommand* Connection::CreateCommand(FdoInt32 type)
+{
+    assert(!"NOT IMPLEMENTED");
+    return NULL;
+}
+
+FdoPhysicalSchemaMapping* Connection::CreateSchemaMapping()
+{
+    assert(!"NOT IMPLEMENTED");
+    return NULL;
+}
+
+void Connection::SetConfiguration(FdoIoStream* configStream)
+{
+    assert(!"NOT IMPLEMENTED");
+}
+
+void Connection::Flush()
+{
+    assert(!"NOT IMPLEMENTED");
 }
 
 }} // namespace fdo::postgis
