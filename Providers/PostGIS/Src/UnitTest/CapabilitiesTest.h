9c6e933ebaa8e37dee5ee9b96980d9c388eb8cc1
mloskot <mloskot@example.org>
2007-02-03 10:11:20 +0000

    Added CapabilitiesTest test case. Fixed minor issues to make whole solution buildable.
    git-svn-id: http://svn.osgeo.org/fdo/trunk@2805 d98eaee6-4212-0410-900c-dcc49bf69b99

---
+++ Providers/PostGIS/Src/UnitTest/CapabilitiesTest.h
@@ -0,0 +1,55 @@
+//
+// Copyright (C) 2007 Refractions Research, Inc. 
+// 
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of version 2.1 of the GNU Lesser
+// General Public License as published by the Free Software Foundation.
+// 
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+//
+#ifndef FDOPOSTGIS_CAPABILITIESTEST_H_INCLUDED
+#define FDOPOSTGIS_CAPABILITIESTEST_H_INCLUDED
+
+#include "BaseTestCase.h"
+
+class CapabilitiesTest : public fdo::postgis::test::BaseTestCase
+{
+    CPPUNIT_TEST_SUITE(CapabilitiesTest);
+    CPPUNIT_TEST(testConnectionCapabilities);
+    CPPUNIT_TEST(testCommandsCapabilities);
+    CPPUNIT_TEST(testSchemaCapabilities);
+    CPPUNIT_TEST(testFilterCapabilities);
+    CPPUNIT_TEST(testExpressionCapabilities);
+    CPPUNIT_TEST(testGeometryCapabilities);
+    CPPUNIT_TEST(testTopologyCapabilities);
+    CPPUNIT_TEST_SUITE_END();
+
+public:
+
+    CapabilitiesTest();
+    virtual ~CapabilitiesTest();
+
+    void setUp();
+	void tearDown();
+
+    //
+    // Tests
+    //
+
+    void testConnectionCapabilities();
+    void testCommandsCapabilities();
+    void testSchemaCapabilities();
+    void testFilterCapabilities();
+    void testExpressionCapabilities();
+    void testGeometryCapabilities();
+    void testTopologyCapabilities();
+};
+
+#endif // FDOPOSTGIS_CAPABILITIESTEST_H_INCLUDED
