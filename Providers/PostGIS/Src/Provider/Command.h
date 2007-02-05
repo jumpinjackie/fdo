874d36a0b9810279e98cd90cfdb5c0552ec6a97e
mloskot <mloskot@example.org>
2007-02-05 02:35:54 +0000

    Added FdoICommand interface impl. stub to Command class.
    git-svn-id: http://svn.osgeo.org/fdo/trunk@2815 d98eaee6-4212-0410-900c-dcc49bf69b99

--- Providers/PostGIS/Src/Provider/Command.h
+++ Providers/PostGIS/Src/Provider/Command.h
@@ -19,10 +19,12 @@
 
 #include "Connection.h"
 
+#include <cassert>
+
 namespace fdo { namespace postgis {
 
-
-/// Implementation of ...
+/// Implementation of interface defining common behavior to all
+/// commands in PostGIS provider for FDO.
 ///
 template <typename T>
 class Command : public T
@@ -31,17 +33,135 @@ class Command : public T
 
 public:
 
-    Command() {}
-    virtual ~Command() {}
+    //
+    // FdoICommand interface
+    //
+
+    /// Get instance of connection that this command will operate on.
+    virtual FdoIConnection* GetConnection();
+ 	
+    /// Get transaction in frames of which the command executes.
+    virtual FdoITransaction* GetTransaction();
+
+    /// Set transaction for the command.
+    virtual void SetTransaction(FdoITransaction* value);
+ 	
+    /// Get number of milliseconds to wait before terminating execution of the command.
+    /// Termination take place before generating any error.
+    virtual FdoInt32 GetCommandTimeout();
+ 	
+    /// Set number of milliseconds to wait before terminating execution of the command.
+    /// Termination take place before generating any error.
+    virtual void SetCommandTimeout(FdoInt32 value);
+ 	
+    /// Return collection of command parameters and its values.
+    /// If the command requires any parameters, the literal values to bind to
+    /// each of those named parameters must be added to this collection.
+    virtual FdoParameterValueCollection* GetParameterValues();
+ 	
+    /// Prepare command for execution.
+    /// Preparation is used to validate and optimization of the command.
+    virtual void Prepare();
+ 	
+    /// Send request to cancel command execution.
+    virtual void Cancel();
 
 protected:
 
     // The connection this command was created from.
-    FdoPtr<Connection> mConnection;
+    FdoPtr<Connection> mConn;
+
+    /// Construct instance of a command for the given connection.
+    Command(Connection* conn);
+
+    /// Destructor.
+    virtual ~Command();
+
+    //
+    // FdoIDisposable interface
+    //
+    
+    void Dispose();
 
 private:
 	
-};
+}; // class Command
+
+template <typename T>
+Command<T>::Command(Connection* conn) : mConn(conn)
+{
+    FDO_SAFE_ADDREF(mConn.p);
+}
+
+template <typename T>
+Command<T>::~Command()
+{
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// FdoIDisposable interface
+///////////////////////////////////////////////////////////////////////////////
+
+template <typename T>
+void Command<T>::Dispose()
+{
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// FdoICommand interface
+///////////////////////////////////////////////////////////////////////////////
+
+template <typename T>
+FdoIConnection* Command<T>::GetConnection()
+{
+    assert(!"NOT IMPLEMENTED");
+    return NULL;
+}
+
+template <typename T>
+FdoITransaction* Command<T>::GetTransaction()
+{
+    assert(!"NOT IMPLEMENTED");
+    return NULL;
+}
+
+template <typename T>
+void Command<T>::SetTransaction(FdoITransaction* value)
+{
+    assert(!"NOT IMPLEMENTED");
+}
+
+template <typename T>
+FdoInt32 Command<T>::GetCommandTimeout()
+{
+    assert(!"NOT IMPLEMENTED");
+    return 0;
+}
+
+template <typename T>
+void Command<T>::SetCommandTimeout(FdoInt32 value)
+{
+    assert(!"NOT IMPLEMENTED");
+}
+
+template <typename T>
+FdoParameterValueCollection* Command<T>::GetParameterValues()
+{
+    assert(!"NOT IMPLEMENTED");
+    return NULL;
+}
+
+template <typename T>
+void Command<T>::Prepare()
+{
+    assert(!"NOT IMPLEMENTED");
+}
+
+template <typename T>
+void Command<T>::Cancel()
+{
+    assert(!"NOT IMPLEMENTED");
+}
 
 }} // namespace fdo::postgis
 
