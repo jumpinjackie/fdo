634774dabb236b9529007ef52c2a8b4d18efcd56
mloskot <mloskot@example.org>
2007-02-06 02:08:12 +0000

    Added impl. stub of FdoISQLDataReader interface to the SQLDataReader class.
    git-svn-id: http://svn.osgeo.org/fdo/trunk@2830 d98eaee6-4212-0410-900c-dcc49bf69b99

--- Providers/PostGIS/Src/Provider/SQLDataReader.h
+++ Providers/PostGIS/Src/Provider/SQLDataReader.h
@@ -1,5 +1,5 @@
 //
-// Copyright (C) 2007 Refractions Research, Inc. 
+// Copyright(C) 2007 Refractions Research, Inc. 
 // 
 // This library is free software; you can redistribute it and/or
 // modify it under the terms of version 2.1 of the GNU Lesser
@@ -17,33 +17,111 @@
 #ifndef FDOPOSTGIS_SQLDATAREADER_H_INCLUDED
 #define FDOPOSTGIS_SQLDATAREADER_H_INCLUDED
 
+#include "Connection.h"
+#include <Fdo/Commands/Sql/ISQLDataReader.h>
+
 //
 // Forward declarations
 //
+class fdo::postgis::Connection;
+class FdoLOBValue;
+class FdoIStreamReader;
 
 
 namespace fdo { namespace postgis {
 
-
-/// Implementation of ...
+/// Implementation of interface providing forward-only and read-only iterator
+/// for reading relational table data.
+/// This implementation is specific for PostGIS provider for FDO.
+///
+/// \todo Check and move functions to protected area.
 ///
 class SQLDataReader : public FdoISQLDataReader
 {
 public:
 
-    SQLDataReader();
+    /// Construcor creates instance of SQL data reader associated
+    /// with given connection.
+    SQLDataReader(Connection* conn);
+    
+    /// Destructor.
     virtual ~SQLDataReader();
 
     //
-    // FdoISQLDataReader
+    // FdoIDisposable interface
     //
 
     virtual void Dispose();
 
+    //
+    // FdoISQLDataReader interface
+    //
+
+    /// Get number of columns in the result set.
+    FdoInt32 GetColumnCount();
+    
+    /// Get name of the column at the given ordinal position.
+    FdoString* GetColumnName(FdoInt32 index);
+    
+    /// Get data type of the column with the specified name.
+    FdoDataType GetColumnType(FdoString* columnName);
+    
+    /// Get FDO property type of the column with the specified name.
+    FdoPropertyType GetPropertyType(FdoString* columnName);
+    
+    /// Get Boolean value of the specified column.
+    bool GetBoolean(FdoString* columnName);
+    
+    /// Get byte value of the specified column.
+    FdoByte GetByte(FdoString* columnName);
+    
+    /// Get date time value of the specified column.
+    FdoDateTime GetDateTime(FdoString* columnName);
+    
+    /// Get double-precision floating point value of the specified column.
+    double GetDouble(FdoString* columnName);
+    
+    /// Get signed 16-bit integer value of the specified column.
+    FdoInt16 GetInt16(FdoString* columnName);
+    
+    /// Get signed 32-bit integer value of the specified column.
+    FdoInt32 GetInt32(FdoString* columnName);
+    
+    /// Get signed 64-bit integer value of the specified column.
+    FdoInt64 GetInt64(FdoString* columnName);
+    
+    /// Get single-precision floating point value of the specified column.
+    float GetSingle(FdoString* columnName);
+    
+    /// Get string value of the specified column.
+    FdoString* GetString(FdoString* columnName);
+    
+    /// Get LOBValue reference.
+    FdoLOBValue* GetLOB(FdoString* columnName);
+    
+    /// Get reference of the specified LOB property as an instance of
+    /// type of FdoBLOBStreamReader or FdoCLOBStreamReader etc.
+    FdoIStreamReader* GetLOBStreamReader(wchar_t const* columnName);
+    
+    /// Return true if the value of the specified column is null.
+    bool IsNull(FdoString* columnName);
+    
+    /// Get geometry value of the specified column as a byte array in FGF format.
+    FdoByteArray* GetGeometry(FdoString* columnName);
+    
+    /// Advance the reader to the next item.
+    bool ReadNext();
+    
+    /// Close the reader object and release any resources it's holding.
+    void Close();
+
 protected:
 
 private:
-	
+
+    /// \todo Weak reference of ref-counted?
+    Connection* mConn;
+
 };
 
 }} // namespace fdo::postgis
