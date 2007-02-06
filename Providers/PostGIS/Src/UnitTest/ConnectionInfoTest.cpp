01f74710624a355d03012698c12ca2cd1618bc24
mloskot <mloskot@example.org>
2007-02-06 19:10:11 +0000

    Added ConnectionInfoTest to UnitTest project.
    git-svn-id: http://svn.osgeo.org/fdo/trunk@2839 d98eaee6-4212-0410-900c-dcc49bf69b99

---
+++ Providers/PostGIS/Src/UnitTest/ConnectionInfoTest.cpp
@@ -0,0 +1,124 @@
+//
+// Copyright (C) 2007 Refractions Research, Inc. 
+// 
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of version 2.1 of the GNU Lesser
+// General Public License as published by the Free Software Foundation.
+// 
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+//
+#include "Pch.h"
+#include "ConnectionInfoTest.h"
+#include "BaseTestCase.h"
+
+CPPUNIT_TEST_SUITE_REGISTRATION(ConnectionInfoTest);
+CPPUNIT_TEST_SUITE_NAMED_REGISTRATION(ConnectionInfoTest, "ConnectionInfoTest");
+
+ConnectionInfoTest::ConnectionInfoTest()
+{
+}
+
+ConnectionInfoTest::~ConnectionInfoTest()
+{
+}
+
+void ConnectionInfoTest::testProviderName()
+{
+    try
+    {
+        CPPUNIT_ASSERT(false);
+    }
+    catch (FdoException* ex)
+    {
+        BaseTestCase::fail(ex);
+    }
+}
+
+void ConnectionInfoTest::testProviderDisplayName()
+{
+    try
+    {
+        CPPUNIT_ASSERT(false);
+    }
+    catch (FdoException* ex)
+    {
+        BaseTestCase::fail(ex);
+    }
+}
+
+void ConnectionInfoTest::testProviderDescription()
+{
+    try
+    {
+        CPPUNIT_ASSERT(false);
+    }
+    catch (FdoException* ex)
+    {
+        BaseTestCase::fail(ex);
+    }
+}
+void ConnectionInfoTest::testProviderVersion()
+{
+    try
+    {
+        CPPUNIT_ASSERT(false);
+    }
+    catch (FdoException* ex)
+    {
+        BaseTestCase::fail(ex);
+    }
+}
+
+void ConnectionInfoTest::testFeatureDataObjectsVersion()
+{
+    try
+    {
+        CPPUNIT_ASSERT(false);
+    }
+    catch (FdoException* ex)
+    {
+        BaseTestCase::fail(ex);
+    }
+}
+
+void ConnectionInfoTest::testConnectionProperties()
+{
+    try
+    {
+        CPPUNIT_ASSERT(false);
+    }
+    catch (FdoException* ex)
+    {
+        BaseTestCase::fail(ex);
+    }
+}
+
+void ConnectionInfoTest::testProviderDatastoreType(){
+    try
+    {
+        CPPUNIT_ASSERT(false);
+    }
+    catch (FdoException* ex)
+    {
+        BaseTestCase::fail(ex);
+    }
+}
+
+void ConnectionInfoTest::testDependentFileNames()
+{
+    try
+    {
+        CPPUNIT_ASSERT(false);
+    }
+    catch (FdoException* ex)
+    {
+        BaseTestCase::fail(ex);
+    }
+}
