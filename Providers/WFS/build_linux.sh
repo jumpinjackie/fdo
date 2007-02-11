fd784906e4bacecba24208cf577c01bec814c3db
gregboone <gregboone@example.org>
2007-02-11 01:45:49 +0000

    Update Linux install location to reference /usr/local/fdo-3.3.0/
    git-svn-id: http://svn.osgeo.org/fdo/trunk@2492 d98eaee6-4212-0410-900c-dcc49bf69b99

--- Providers/WFS/build_linux.sh
+++ Providers/WFS/build_linux.sh
@@ -153,10 +153,10 @@ if test "$BUILDDOCS" == yes ; then
 fi
 
 if test "$TYPEACTION" == buildinstall || test "$TYPEACTION" == install ; then
-   rm -rf "/usr/local/fdo-3.2.0/docs/HTML/Providers/WFS"
-   mkdir -p "/usr/local/fdo-3.2.0/docs/HTML/Providers"
+   rm -rf "/usr/local/fdo-3.3.0/docs/HTML/Providers/WFS"
+   mkdir -p "/usr/local/fdo-3.3.0/docs/HTML/Providers"
    if test -e "Docs/HTML/WFS"; then
-      cp --force --recursive "Docs/HTML/WFS" "/usr/local/fdo-3.2.0/docs/HTML/Providers"
+      cp --force --recursive "Docs/HTML/WFS" "/usr/local/fdo-3.3.0/docs/HTML/Providers"
    fi
 fi
 
