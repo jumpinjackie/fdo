506be38e25aa84ef46c467aa53715c16dfa2aee3
gregboone <gregboone@example.org>
2007-02-15 20:44:53 +0000

    FDO4: API - Update API documentation included in the FDO header files
    git-svn-id: http://svn.osgeo.org/fdo/trunk@2540 d98eaee6-4212-0410-900c-dcc49bf69b99

--- Providers/SHP/Docs/doc_src/group_and_page_definitions/define_pages.h
+++ Providers/SHP/Docs/doc_src/group_and_page_definitions/define_pages.h
@@ -7,31 +7,38 @@ data store. A provider is a specific implementation of the FDO API
 that provides access to data in a particular data store. The FDO 
 Provider for SHP provides FDO with access to a SHP-based data store.
 </p>
-<p>The FDO Provider for SHP uses a standalone file format that
-supports GIS data. The FDO Provider for SHP (Shape) has the 
-following characteristics: 
+
+<p>The standalone ESRI SHP file format supports GIS data using a number of 
+separate files: SHP (shape geometry), SHX (shape index), PRJ (projection 
+information), CPG (code page files), IDX (spatial index), and DBF (shape 
+attributes in dBASE format). You must have either a SHP or DBF file present to 
+connect to data or work with schemas. Otherwise, these files are optional. Files 
+with empty records are created, if they do not exist initially.
+</p>
+
+<p>In AutoCAD Map 3D 2008, the Schema Editor treats each SHP file, and its 
+associated DBF file, as a feature class with a single geometry property and, 
+optionally, data attribute properties. The SHP file format supports a single 
+writer at any time, with multiple readers.
+</p>
+
+<p>SHP files can include only one geometry type per file, but you can stored and 
+copy to multiple SHP files in a folder. You can use a configuration file to 
+support multiple schemas when you establish your connection to a folder 
+containing SHP files.
+</p>
+
+<p>A SHP schema can support the following:
 </p>
 <ul>
-  <li> Read-only access is provided to pre-existing
-    spatial and attribute data from an Environmental Systems
-    Research Institute (ESRI) Shape file (SHP).
-  <li> The FDO Provider for SHP can run in a multi-platform environment,
-    including Windows and Linux.
-  <li> A Shape file consists of three separate files: SHP (shape
-    geometry), SHX (shape index), and DBF (shape attributes in
-    dBASE format).
-  <li> The FDO Provider for SHP accesses the information in each of the 
-  three separate files, and treats each SHP, and its associated DBF file, 
-  as a feature class with a single geometry property, and optionally, 
-  with data attribute properties.
-  <li> Schema configuration of the data store is provided to the
-    FDO Provider for SHP through an XML file containing the
-    Geographic Markup Language (GML) definition of the schema
-    that maps SHP and DBF data in the data store to feature
-    classes and property mappings in the FDO data model.
-  <li> Although it does not support concurrency control (locking), the SHP 
-  file format is a valid alternative to RDBMS.
+  <li>A single geometry type per file
+  <li>Spatial contexts (determined by coordinate system information in the PRJ file)
+  <li>Auto ID generation
+  <li>Null value constraints
+  <li>These geometry types: point, line string, polygon, multi-point, multi-line 
+  string, multi-polygon, linear ring, and line string segment.
 </ul>
+ 
 <p>
 For more information, see <i>The Essential FDO</i> (FET_TheEssentialFDO.pdf) 
 and the <i>FDO Developer's Guide</i> (FDG_FDODevGuide.pdf).
