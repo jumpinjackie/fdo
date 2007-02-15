6a47e51aa9edc20f4686f6d4bd1866254ae4a64e
gregboone <gregboone@example.org>
2007-02-15 20:46:09 +0000

    FDO4: API - Update API documentation included in the FDO header files
    git-svn-id: http://svn.osgeo.org/fdo/trunk@2541 d98eaee6-4212-0410-900c-dcc49bf69b99

--- Providers/WFS/Docs/doc_src/group_and_page_definitions/define_pages.h
+++ Providers/WFS/Docs/doc_src/group_and_page_definitions/define_pages.h
@@ -11,27 +11,30 @@ data store. A provider is a specific implementation of the FDO API
 that provides access to data in a particular data store. The FDO 
 Provider for WFS provides FDO with access to a WFS-based data store.
 </p>
-<p>An OGC Web Feature Service (WFS) provides access to geographic
-features that are stored in an opaque data store in a client/server
-environment. A client uses WFS to
-retrieve geospatial data that is encoded in Geography Markup Language
-(GML) from a single or multiple Web Feature Service. The
-communication between client and server is encoded in XML. If the
-WFS response includes feature geometries, it is encoded in 
-Geography Markup Language (GML), which is specified in the
-OpenGIS Geographic Markup Language Implementation
-Specification.
+01234567890123456789012345678901234567890123456789012345678901234567890123456789
+<p>An OGC Web Feature Service (WFS) provides access to geographic features that 
+are stored in an opaque data store in a client/server environment. A client 
+uses WFS to retrieve geospatial data that is encoded in Geography Markup 
+Language (GML) from a single or multiple WFS. The communication between client 
+and server is encoded in XML. If the WFS response includes feature geometries, 
+it is encoded in Geography Markup Language (GML), which is specified in the 
+OpenGIS Geographic Markup Language Implementation Specification. WFS is a 
+read-only provider.
 </p>
-<p>Using FDO Provider for WFS data manipulation operations, you an do the following: 
+
+<p>A WFS schema can support the following:
 </p>
 <ul>
-  <li> Query features based on spatial and non-spatial
-    constraints.
-  <li> Create new feature instances.
-  <li> Delete feature instances.
-  <li> Update feature instances.
-  <li> Lock feature instances.
+  <li>Inheritance
+  <li>Multiple schemas
+  <li>Object properties
+  <li>Association properties
+  <li>These geometry types: point, line string, polygon, multi-point, 
+  multi-line string, multi-polygon, multi-geometry, curve string, curve polygon, 
+  multi-curve string, multi-curve polygon, linear ring, line string segment, 
+  circular arc segment, and ring.
 </ul>
+
 <p>
 For more information, see <i>The Essential FDO</i> (FET_TheEssentialFDO.pdf) 
 and the <i>FDO Developer's Guide</i> (FDG_FDODevGuide.pdf).
