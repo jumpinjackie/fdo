65321aeeb7507509be95c937341a61b2331f5bae
gregboone <gregboone@example.org>
2007-02-15 20:40:29 +0000

    FDO4: API - Update API documentation included in the FDO header files
    git-svn-id: http://svn.osgeo.org/fdo/trunk@2538 d98eaee6-4212-0410-900c-dcc49bf69b99

--- Providers/GenericRdbms/Docs/doc_src/group_and_page_definitions/ODBC/define_pages.h
+++ Providers/GenericRdbms/Docs/doc_src/group_and_page_definitions/ODBC/define_pages.h
@@ -11,12 +11,13 @@ Provider for ODBC provides FDO with access to an ODBC-based data store.
 feature objects that can run in a multi-platform
 environment, including Windows, Linux, and UNIX.
 </p>
+
 <p>The FDO Provider for ODBC has the following characteristics:
 </p>
 <ul>
-  <li> The FDO Provider for ODBC supports the definition of one or more feature classes in terms of
-    any relational database table that contains an X, Y, and
-    optionally, Z columns.</li>
+  <li> The FDO Provider for ODBC supports the definition of one or more 
+    feature classes in terms of any relational database table that 
+    contains an X, Y, and optionally, Z columns.</li>
   <li> Metadata--that maps the table name, and X, Y, and optionally, Z
     columns to a feature class--is maintained outside the
     database in a configuration file. This information, in
@@ -41,6 +42,38 @@ environment, including Windows, Linux, and UNIX.
 \note
 Microsoft Excel (must have at least one named range; do not use DATABASE 
 or other reserved words as a range name) 
+
+<p>For more information about setting up an ODBC data store in AutoCAD Map 3D
+2008 and configuring Excel for database ranges, see the 'Managing Data' topic
+in the <i>AutoCAD Map 3D User's Guide</i> (acmap.chm).
+</p>
+
+<p>For ODBC, only a point geometry (X, Y, and optionally, Z columns define a 
+point) is supported. The default column names are X, Y, and Z, but when you pick 
+the columns to represent these, you override the defaults. In AutoCAD Map 3D 
+2008, this information is stored in the map file itself, so if you go through 
+the same process in another map, you will need to override the settings again 
+in the same way.
+</p>
+
+<p>There is no spatial indexing. Object locations are stored in separate 
+properties in the object definition.
+<p>
+
+<p>The existing schema is used, and you cannot add, change, or delete it, nor 
+can you add FDO metadata to the data store.
+</p>
+
+<p>When you set up a Data Source Name for your ODBC data store (by adding a DSN 
+in Windows), you will need to specify certain configuration options, depending 
+on the type of ODBC data store you use. The connection information (Login ID 
+and password) you enter here is used only for defining the DSN, and not at 
+connection time with the ODBC provider. 
+</p>
+
+\note
+For MySQL, make sure you have installed the MySQL driver from MyODBC 3.51. 
+You can find it at http://dev.mysql.com/downloads/connector/odbc/3.51.html.
 
 <p>
 For more information, see <i>The Essential FDO</i> (FET_TheEssentialFDO.pdf) 
