22207509a3390cdd84ff86c24133f6c434104dbe
mloskot <mloskot@example.org>
2007-02-19 22:30:28 +0000

    Non-query execution for SQL command. Replaced error code return with FDO exceptions. New messages in PostGisMessage.mc registry.
    git-svn-id: http://svn.osgeo.org/fdo/trunk@2885 d98eaee6-4212-0410-900c-dcc49bf69b99

--- Providers/PostGIS/Src/Provider/SQLCommand.cpp
+++ Providers/PostGIS/Src/Provider/SQLCommand.cpp
@@ -21,6 +21,7 @@
 #include "Connection.h"
 
 #include <cassert>
+#include <string>
 
 namespace fdo { namespace postgis {
 
@@ -32,21 +33,61 @@ SQLCommand::~SQLCommand()
 {
 }
 
+///////////////////////////////////////////////////////////////////////////////
+// FdoISQLCommand interface
+///////////////////////////////////////////////////////////////////////////////
+
 FdoString* SQLCommand::GetSQLStatement()
 {
-    assert(!"NOT IMPLEMENTED");
+    FDOLOG_MARKER("SQLCommand::+GetSQLStatement");
+
     return NULL;
 }
 
-void SQLCommand::SetSQLStatement(FdoString const* sql)
+void SQLCommand::SetSQLStatement(FdoString* sql)
 {
-    assert(!"NOT IMPLEMENTED");
+    FDOLOG_MARKER("SQLCommand::+SetSQLStatement");
+
+    if (NULL == sql)
+    {
+        throw FdoException::Create(NlsMsgGet(MSG_POSTGIS_COMMAND_SQL_INVALID,
+            "The passed SQL statement is invalid or empty: '%1$ls'.",
+            L""));
+    }
+    
+    mSql = sql;
+    
+    FDOLOG_WRITE("SQL: %s", static_cast<char const*>(mSql));
 }
     
 FdoInt32 SQLCommand::ExecuteNonQuery()
 {
-    assert(!"NOT IMPLEMENTED");
-    return 0;
+    FDOLOG_MARKER("SQLCommand::+ExecuteNonQuery");
+
+    // TODO: Add support for collection of input parameters
+    //FdoPtr<FdoParameterValueCollection> cmdParams = GetParameterValues();
+
+    ExecStatusType pgStatus = PGRES_FATAL_ERROR;
+    FdoSize cmdTuples = 0;
+    
+    try
+    {
+        std::string sql(static_cast<char const*>(mSql));
+        pgStatus = mConn->PgExecuteCommand(sql.c_str(), cmdTuples);
+        assert(PGRES_COMMAND_OK == pgStatus);
+    }
+    catch (FdoException* e)
+    {
+        FdoCommandException* de = NULL;
+        de = FdoCommandException::Create(NlsMsgGet(MSG_POSTGIS_COMMAND_SQL_FAILED,
+            "The execution of SQL command failed."), e);
+        e->Release();
+        throw de;
+    }
+
+    
+
+    return static_cast<FdoInt32>(cmdTuples);
 }
 
 FdoISQLDataReader* SQLCommand::ExecuteReader()
@@ -54,5 +95,10 @@ FdoISQLDataReader* SQLCommand::ExecuteReader()
     assert(!"NOT IMPLEMENTED");
     return NULL;
 }
+
+///////////////////////////////////////////////////////////////////////////////
+// Private operations
+///////////////////////////////////////////////////////////////////////////////
+
 
 }} // namespace fdo::postgis
