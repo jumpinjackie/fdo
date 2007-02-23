2fa6bbfa46c7322fc9e81b830e37e420a6b00c5d
mloskot <mloskot@example.org>
2007-02-23 01:54:13 +0000

    Added PgUtility.h file (empty) for various tools for PostgreSQL operations.
    git-svn-id: http://svn.osgeo.org/fdo/trunk@2895 d98eaee6-4212-0410-900c-dcc49bf69b99

---
+++ Providers/PostGIS/Src/Provider/PgUtility.h
@@ -0,0 +1,29 @@
+//
+// Copyright (C) 2007 Refractions Research, Inc. 
+// 
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of version 2.1 of the GNU Lesser
+// General Public License as published by the Free Software Foundation.
+// 
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+//
+#ifndef FDOPOSTGIS_PGUTILITY_H_INCLUDED
+#define FDOPOSTGIS_PGUTILITY_H_INCLUDED
+
+#include "Connection.h"
+#include <string>
+#include <boost/lexical_cast.hpp>
+#include <libpq-fe.h>
+
+namespace fdo { namespace postgis {
+
+}} // namespace fdo::postgis
+
+#endif // FDOPOSTGIS_PGUTILITY_H_INCLUDED
