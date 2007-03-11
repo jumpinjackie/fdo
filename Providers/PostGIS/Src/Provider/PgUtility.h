0d7ad99a4e469805a835e718c9187de6eda984b8
mloskot <mloskot@example.org>
2007-03-11 00:05:13 +0000

    Added new functions to the PgUtility. These new functions are used to translate PostgreSQl type and its characteristics to FDO world.
    git-svn-id: http://svn.osgeo.org/fdo/trunk@2955 d98eaee6-4212-0410-900c-dcc49bf69b99

--- Providers/PostGIS/Src/Provider/PgUtility.h
+++ Providers/PostGIS/Src/Provider/PgUtility.h
@@ -17,6 +17,7 @@
 #ifndef FDOPOSTGIS_PGUTILITY_H_INCLUDED
 #define FDOPOSTGIS_PGUTILITY_H_INCLUDED
 
+#include <Fdo/Schema/DataType.h>
 // std
 #include <string>
 #include <vector>
@@ -34,23 +35,46 @@ namespace details {
 
 /// Type of Septuple (7-elements tuple) for connection info parameters used to
 /// establish connection with PostgreSQL database server.
+///
 typedef boost::tuples::tuple
     <
         std::string, std::string, std::string, std::string, std::string, std::string, std::string
     >
 pgconn_params_t;
 
-
 /// Type of collection for parameters passed to command execution procedures.
 /// Every element of the collection is a pair of character representation o
 /// data value and NULL indicator (0 = non-null; 1 or higher = null value).
 /// If NULL indicator is set with a non-Zero value, then the data compund is ignored,
 /// and should be set to empty string.
+///
 typedef std::vector
     <
         std::pair<std::string, int>
     >
 pgexec_params_t;
+
+/// Convert PostgreSQL type of given name to FDO type enumerator.
+/// \param typeName [in] - name of PostgreSQL data type.
+/// \return FDO type enumerator mapped from PostgreSQL type.
+///
+FdoDataType FdoTypeFromPgTypeName(std::string const& typeName);
+
+/// Get value of type size from PQfmod mask.
+/// This function does only work for CHARACTER VARYING and TEXT types if length
+/// was specified during table creation.
+/// Caller is supposed to check type and decide about calling this function or not.
+int GetTypeMaxSize(int const& modifier);
+
+/// Get value of NUMERIC type precision from PQfmod mask.
+/// This function does only work for NUMERIC (DECIMAL) type.
+/// Caller is supposed to check type and decide about calling this function or not.
+int GetTypePrecision(int const& modifier);
+
+/// Get value of NUMERIC type scale from PQfmod mask.
+/// This function does only work for NUMERIC (DECIMAL) type.
+/// Caller is supposed to check type and decide about calling this function or not.
+int GetTypeScale(int const& modifier);
 
 }}} // namespace fdo::postgis::details
 
