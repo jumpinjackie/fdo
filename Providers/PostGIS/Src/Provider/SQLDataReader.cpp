9e365b95a3f753e9a9e5010d1e75c99ea48f9ee3
mloskot <mloskot@example.org>
2007-03-12 15:17:17 +0000

    Added base of SchemaDescribtion class.
    git-svn-id: http://svn.osgeo.org/fdo/trunk@2968 d98eaee6-4212-0410-900c-dcc49bf69b99

--- Providers/PostGIS/Src/Provider/SQLDataReader.cpp
+++ Providers/PostGIS/Src/Provider/SQLDataReader.cpp
@@ -15,7 +15,6 @@
 // Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
 //
 #include "stdafx.h"
-
 #include "PostGisProvider.h"
 #include "SQLDataReader.h"
 #include "Connection.h"
