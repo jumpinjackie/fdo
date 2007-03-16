80e3d1e2720282a8e7ba4a8565cc677f4d0c11fb
mloskot <mloskot@example.org>
2007-03-16 19:19:18 +0000

    Added stubs for other functions of ExpressionProcessor.
    git-svn-id: http://svn.osgeo.org/fdo/trunk@2999 d98eaee6-4212-0410-900c-dcc49bf69b99

--- Providers/PostGIS/Src/Provider/ExpressionProcessor.cpp
+++ Providers/PostGIS/Src/Provider/ExpressionProcessor.cpp
@@ -51,4 +51,72 @@ void ExpressionProcessor::ProcessUnaryExpression(FdoUnaryExpression& expr)
 {
 }
 
+void ExpressionProcessor::ProcessFunction(FdoFunction& expr)
+{
+}
+
+void ExpressionProcessor::ProcessIdentifier(FdoIdentifier& expr)
+{
+}
+ 	
+void ExpressionProcessor::ProcessComputedIdentifier(FdoComputedIdentifier& expr)
+{
+}
+
+void ExpressionProcessor::ProcessParameter(FdoParameter& expr)
+{
+}
+
+void ExpressionProcessor::ProcessBooleanValue(FdoBooleanValue& expr)
+{
+}
+
+void ExpressionProcessor::ProcessByteValue(FdoByteValue& expr)
+{
+}
+
+void ExpressionProcessor::ProcessDateTimeValue(FdoDateTimeValue& expr)
+{
+}
+
+void ExpressionProcessor::ProcessDecimalValue(FdoDecimalValue& expr)
+{
+}
+
+void ExpressionProcessor::ProcessDoubleValue(FdoDoubleValue& expr)
+{
+}
+
+void ExpressionProcessor::ProcessInt16Value(FdoInt16Value& expr)
+{
+}
+
+void ExpressionProcessor::ProcessInt32Value(FdoInt32Value& expr)
+{
+}
+
+void ExpressionProcessor::ProcessInt64Value(FdoInt64Value& expr)
+{
+}
+
+void ExpressionProcessor::ProcessSingleValue(FdoSingleValue& expr)
+{
+}
+
+void ExpressionProcessor::ProcessStringValue(FdoStringValue& expr)
+{
+}
+
+void ExpressionProcessor::ProcessBLOBValue(FdoBLOBValue& expr)
+{
+}
+
+void ExpressionProcessor::ProcessCLOBValue(FdoCLOBValue& expr)
+{
+}
+
+void ExpressionProcessor::ProcessGeometryValue(FdoGeometryValue& expr)
+{
+}
+
 }} // namespace fdo::postgis
