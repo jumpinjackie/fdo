baac914c1fc09ba6e74e5b29862f3438a705e915
mloskot <mloskot@example.org>
2007-03-29 07:03:21 +0000

    Implementation of ExpressionProcessor class. TODO: Composition of spatial SQL is not finished.
    git-svn-id: http://svn.osgeo.org/fdo/trunk@3025 d98eaee6-4212-0410-900c-dcc49bf69b99

--- Providers/PostGIS/Src/Provider/ExpressionProcessor.cpp
+++ Providers/PostGIS/Src/Provider/ExpressionProcessor.cpp
@@ -17,10 +17,19 @@
 #include "stdafx.h"
 #include "PostGisProvider.h"
 #include "ExpressionProcessor.h"
-
 #include <cassert>
+#include <string>
+#include <boost/lexical_cast.hpp>
 
 namespace fdo { namespace postgis {
+
+// TODO: Move this namespace to a separate reusable file
+//       combined with fdo::postgis::sql
+namespace
+{
+    char const* sepLeftTerm  = " ( ";
+    char const* sepRightTerm = " ) ";
+}
 
 ExpressionProcessor::ExpressionProcessor()
 {
@@ -57,66 +66,196 @@ void ExpressionProcessor::ProcessFunction(FdoFunction& expr)
 
 void ExpressionProcessor::ProcessIdentifier(FdoIdentifier& expr)
 {
+    FdoStringP name(expr.GetName());
+    mBuffer.append(static_cast<char const*>(name));
 }
  	
 void ExpressionProcessor::ProcessComputedIdentifier(FdoComputedIdentifier& expr)
 {
+    FdoPtr<FdoExpression> exprPtr(expr.GetExpression());
+    if (NULL == exprPtr)
+    {
+        throw FdoFilterException::Create(L"FdoComputedIdentifier is missing");
+    }
+
+    mBuffer.append(sepLeftTerm);
+    exprPtr->Process(this);
+    mBuffer.append(sepRightTerm);
 }
 
 void ExpressionProcessor::ProcessParameter(FdoParameter& expr)
 {
+    // TODO: How does parameters processing work?
+    //       Are there any predefined FDO parameters or name part of "name:" 
+    //       is a DBMS specific name, so for PostgreSQL we have:
+    //       $1, $2, $3,...
+    //       What about value?
+    throw FdoFilterException::Create(L"TODO: PARAMETERS NOT IMPLEMENTED");
 }
 
 void ExpressionProcessor::ProcessBooleanValue(FdoBooleanValue& expr)
 {
+    if( expr.IsNull() )
+    {  
+        mBuffer.append("NULL");
+    }
+    else
+    {
+        if (expr.GetBoolean())
+            mBuffer.append("TRUE");
+        else
+            mBuffer.append("FALSE");
+    }
 }
 
 void ExpressionProcessor::ProcessByteValue(FdoByteValue& expr)
 {
+    if( expr.IsNull() )
+    {  
+        mBuffer.append("NULL");
+    }
+    else
+    {
+        char value[2] = { 0 };
+        value[0] = expr.GetByte();
+        value[1] = '\0';
+        mBuffer.append("'");
+        mBuffer.append(value);
+        mBuffer.append("'");
+    }
 }
 
 void ExpressionProcessor::ProcessDateTimeValue(FdoDateTimeValue& expr)
 {
+    // TODO: Add datetime support
+    throw FdoFilterException::Create(L"TODO: DATETYPE NOT IMPLEMENTED");
 }
 
 void ExpressionProcessor::ProcessDecimalValue(FdoDecimalValue& expr)
 {
+    if (expr.IsNull())
+    {
+        mBuffer.append("NULL");
+    }
+    else
+    {
+        FdoDouble const value = expr.GetDecimal();
+        mBuffer.append(GetValueAsString(value));
+    }
 }
 
 void ExpressionProcessor::ProcessDoubleValue(FdoDoubleValue& expr)
 {
+    if (expr.IsNull())
+    {
+        mBuffer.append("NULL");
+    }
+    else
+    {
+        FdoDouble const value = expr.GetDouble();
+        mBuffer.append(GetValueAsString(value));
+    }
 }
 
 void ExpressionProcessor::ProcessInt16Value(FdoInt16Value& expr)
 {
+    if (expr.IsNull())
+    {
+        mBuffer.append("NULL");
+    }
+    else
+    {
+        FdoInt16 const value = expr.GetInt16();
+        mBuffer.append(GetValueAsString(value));
+    }
 }
 
 void ExpressionProcessor::ProcessInt32Value(FdoInt32Value& expr)
 {
+    if (expr.IsNull())
+    {
+        mBuffer.append("NULL");
+    }
+    else
+    {
+        FdoInt32 const value = expr.GetInt32();
+        mBuffer.append(GetValueAsString(value));
+    }
 }
 
 void ExpressionProcessor::ProcessInt64Value(FdoInt64Value& expr)
 {
+    if (expr.IsNull())
+    {
+        mBuffer.append("NULL");
+    }
+    else
+    {
+        FdoInt64 const value = expr.GetInt64();
+        mBuffer.append(GetValueAsString(value));
+    }
 }
 
 void ExpressionProcessor::ProcessSingleValue(FdoSingleValue& expr)
 {
+    if (expr.IsNull())
+    {
+        mBuffer.append("NULL");
+    }
+    else
+    {
+        FdoFloat const value = expr.GetSingle();
+        mBuffer.append(GetValueAsString(value));
+    }
 }
 
 void ExpressionProcessor::ProcessStringValue(FdoStringValue& expr)
 {
+    if (expr.IsNull())
+    {
+        mBuffer.append("NULL");
+    }
+    else
+    {
+        FdoStringP value(expr.GetString());
+        mBuffer.append(static_cast<char const*>(value));
+    }
 }
 
 void ExpressionProcessor::ProcessBLOBValue(FdoBLOBValue& expr)
 {
+    throw FdoFilterException::Create(L"BLOB value in expression is not supported");
 }
 
 void ExpressionProcessor::ProcessCLOBValue(FdoCLOBValue& expr)
 {
+    throw FdoFilterException::Create(L"CLOB value in expression is not supported");
 }
 
 void ExpressionProcessor::ProcessGeometryValue(FdoGeometryValue& expr)
 {
+    // TODO: Verify interpretation of NULL geometries
+    if (expr.IsNull())
+    {
+        mBuffer.append("NULL");
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// ExpressionProcessor interface
+///////////////////////////////////////////////////////////////////////////////
+
+void ExpressionProcessor::ReleaseExpressionText(std::string& exprText)
+{
+    exprText.assign(mBuffer);
+    mBuffer.clear();
+}
+    
+std::string ExpressionProcessor::ReleaseBuffer()
+{
+    std::string tmp(mBuffer);
+    mBuffer.clear();
+    return tmp;
 }
 
 }} // namespace fdo::postgis
