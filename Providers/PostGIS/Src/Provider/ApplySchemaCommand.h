4cd367ae84ce500771cfc84e8621c0e8ec12d13b
mloskot <mloskot@example.org>
2007-04-16 23:37:40 +0000

    Added ApplySchemaCommand class. TODO: Implemente Execute() member function.
    git-svn-id: http://svn.osgeo.org/fdo/trunk@3064 d98eaee6-4212-0410-900c-dcc49bf69b99

---
+++ Providers/PostGIS/Src/Provider/ApplySchemaCommand.h
@@ -0,0 +1,142 @@
+//
+// Copyright (C) 2007 Refractions Research, Inc. 
+// 
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of version 2.1 of the GNU Lesser
+// General Public License as published by the Free Software Foundation.
+// 
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+//
+#ifndef FDOPOSTGIS_APPLYSCHEMACOMMAND_H_INCLUDED
+#define FDOPOSTGIS_APPLYSCHEMACOMMAND_H_INCLUDED
+
+#include "Command.h"
+
+//
+// Forward declarations
+//
+
+
+namespace fdo { namespace postgis {
+
+/// Implementation of create command, that can be used to create new
+/// datastore on a particular PostgreSQL instance.
+///
+class ApplySchemaCommand : public Command<FdoIApplySchema>
+{
+public:
+
+    /// Type of FDO smart pointer for Connection class.
+    typedef FdoPtr<ApplySchemaCommand> Ptr;
+
+    /// Default constructor.
+    ApplySchemaCommand(Connection* conn);
+
+    /// Destructor.
+    virtual ~ApplySchemaCommand();
+
+    //
+    // FdoIApplySchema interface
+    //
+
+    /// Get name of the schema to create.
+    /// \return
+    /// Pointer to schema being created.
+    /// \exception
+    /// TO BE WRITTEN
+    ///
+    virtual FDOPOSTGIS_API FdoFeatureSchema* GetFeatureSchema();
+
+    /// Set name of the schema to create.
+    /// \param
+    /// schema [in] - name of schema being created.
+    /// \exception
+    /// TO BE WRITTEN
+    ///
+    virtual FDOPOSTGIS_API void SetFeatureSchema(FdoFeatureSchema* schema);
+
+    /// Get FdoPhysicalSchemaMapping used to specify how the schema definition
+    /// gets mapped into physical storage.
+    ///
+    /// \return
+    /// Mapping object describing how FDO schema is mapped to physical storage.
+    /// \exception
+    /// TO BE WRITTEN
+    ///
+    virtual FDOPOSTGIS_API FdoPhysicalSchemaMapping* GetPhysicalMapping();
+
+    /// Set the FdoPhysicalSchemaMapping used to specify how the schema definition
+    /// gets mapped into physical storage.
+    ///
+    /// \param
+    /// sm [in] - object describing how FDO schema is mapped to physical storage.
+    /// \exception
+    /// TO BE WRITTEN
+    ///
+    virtual FDOPOSTGIS_API void SetPhysicalMapping(FdoPhysicalSchemaMapping* sm);
+
+    /// Get flag indicating whether Execute() will ignore element states when
+    /// applying feature schema.
+    ///
+    /// \return true if actual state of schema element should be ignored,
+    /// false otherwise.
+    ///
+    virtual FDOPOSTGIS_API FdoBoolean GetIgnoreStates();
+
+    /// Set Changes the handling of element states by Execute().
+    ///
+    /// \param
+    /// ignoreStates [in] - flag indicating if schema element state
+    /// should be ignored during command execution,
+    ///
+    virtual FDOPOSTGIS_API void SetIgnoreStates(FdoBoolean ignoreStates);
+
+    /// Execute the ApplySchema command that creates metadata and
+    /// physical storage for the schema.
+    ///
+
+///////////// TODO
+/// Executes the ApplySchema command that creates metadata and physical
+/// storage for the schema. An exception is thrown if the schema already
+/// exists or if a schema already exists and the feature provider only
+/// supports a single schema. If schema changes include deletion of classes
+/// or the schema itself, then those classes (or the schema, if it is marked
+/// as deleted) must not contain any instance data. If they do, an exception
+/// will be thrown.
+/// Implementors must call FdoFeatureSchema::AcceptChanges() when Execute() succeeds.
+/// It must be called after the schema has been successfully applied, in
+/// order to synchronize the states of the schema elements.
+
+    /// \exception
+    /// TO BE WRITTEN
+    ///
+    virtual FDOPOSTGIS_API void Execute();
+
+private:
+
+    //
+    // Private data members
+    //
+
+    typedef Command<FdoIApplySchema> Base;
+
+    FdoPtr<FdoFeatureSchema> mFeatureSchema;
+    FdoPtr<FdoPhysicalSchemaMapping> mSchemaMapping;
+    FdoBoolean mIgnoreStates;
+
+
+    //
+    // Private operations
+    //
+};
+
+}} // namespace fdo::postgis
+
+#endif // FDOPOSTGIS_APPLYSCHEMACOMMAND_H_INCLUDED
