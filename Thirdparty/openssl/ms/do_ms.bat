32cac3f18c058075fecd4894600a3936c6e32f10
gregboone <gregboone@example.org>
2007-08-30 00:50:20 +0000

    Update Thirdparty openssl source
    git-svn-id: http://svn.osgeo.org/fdo/trunk@3252 d98eaee6-4212-0410-900c-dcc49bf69b99

--- Thirdparty/openssl/ms/do_ms.bat
+++
@@ -1,9 +0,0 @@
-
-perl util\mkfiles.pl %1 >MINFO
-perl util\mk1mf.pl %1 no-asm VC-WIN32 >ms\nt.mak
-perl util\mk1mf.pl %1 dll no-asm VC-WIN32 >ms\ntdll.mak
-perl util\mk1mf.pl %1 no-asm VC-CE >ms\ce.mak
-perl util\mk1mf.pl %1 dll no-asm VC-CE >ms\cedll.mak
-
-perl util\mkdef.pl 32 libeay > ms\libeay32.def
-perl util\mkdef.pl 32 ssleay > ms\ssleay32.def
