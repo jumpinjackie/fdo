3a4423ae3367ad5df2e9ec1a732ec64c5a67a6f5
thomasknoell <thomasknoell@example.org>
2007-09-04 23:49:50 +0000

    FDO Code changes related to accepted RFCs 5 and 8.
    git-svn-id: http://svn.osgeo.org/fdo/trunk@3268 d98eaee6-4212-0410-900c-dcc49bf69b99

--- Fdo/Unmanaged/Src/Fdo/Connections/Capabilities/ArgumentDefinition.cpp
+++ Fdo/Unmanaged/Src/Fdo/Connections/Capabilities/ArgumentDefinition.cpp
@@ -19,77 +19,118 @@
 #include "StringUtility.h"
 
 // Constructs an instance of a ArgumentDefinition using the specified arguments.
-FdoArgumentDefinition* FdoArgumentDefinition::Create(FdoString* name, FdoString* description, FdoDataType dataType)
+FdoArgumentDefinition *FdoArgumentDefinition::Create (FdoString   *name,
+                                                      FdoString   *description,
+                                                      FdoDataType dataType)
 {
 	return new FdoArgumentDefinition(name, description, dataType);
 }
 
 // Constructs an instance of a ArgumentDefinition using the specified arguments.
-FdoArgumentDefinition* FdoArgumentDefinition::Create(FdoString* name, FdoString* description, FdoPropertyType propertyType, FdoDataType dataType)
+FdoArgumentDefinition *FdoArgumentDefinition::Create (FdoString       *name,
+                                                      FdoString       *description,
+                                                      FdoPropertyType propertyType,
+                                                      FdoDataType     dataType)
 {
 	return new FdoArgumentDefinition(name, description, propertyType, dataType);
 }
 
 // Constructs an instance of a ArgumentDefinition using the specified arguments.
-FdoArgumentDefinition::FdoArgumentDefinition(FdoString* name, FdoString* description, FdoDataType dataType)
+FdoArgumentDefinition::FdoArgumentDefinition (FdoString   *name,
+                                              FdoString   *description,
+                                              FdoDataType dataType)
 {
 	m_name = FdoStringUtility::MakeString(name);
 	m_description = FdoStringUtility::MakeString(description);
     m_propertyType = FdoPropertyType_DataProperty;
 	m_dataType = dataType;
+    m_argumentValueList = NULL;
 }
 
 // Constructs an instance of a ArgumentDefinition using the specified arguments.
-FdoArgumentDefinition::FdoArgumentDefinition(FdoString* name, FdoString* description, FdoPropertyType propertyType, FdoDataType dataType)
+FdoArgumentDefinition::FdoArgumentDefinition (FdoString       *name,
+                                              FdoString       *description,
+                                              FdoPropertyType propertyType,
+                                              FdoDataType     dataType)
 {
 	m_name = FdoStringUtility::MakeString(name);
 	m_description = FdoStringUtility::MakeString(description);
     m_propertyType = propertyType;
 	m_dataType = dataType;
+    m_argumentValueList = NULL;
 }
 
 // Constructs an instance of a ArgumentDefinition using default property values.
-FdoArgumentDefinition::FdoArgumentDefinition()
+FdoArgumentDefinition::FdoArgumentDefinition ()
 {
 	m_name = NULL;
 	m_description = NULL;
     m_propertyType = FdoPropertyType_DataProperty;
     m_dataType = FdoDataType_String;
+    m_argumentValueList = NULL;
 }
 
 
-FdoArgumentDefinition::~FdoArgumentDefinition()
+FdoArgumentDefinition::~FdoArgumentDefinition ()
 {
 	FdoStringUtility::ClearString(m_name);
 	FdoStringUtility::ClearString(m_description);
+    FDO_SAFE_RELEASE(m_argumentValueList);
 }
 
-void FdoArgumentDefinition::Dispose()
+void FdoArgumentDefinition::Dispose ()
 {
     delete this;
 }
 
 // Gets the name of the argument.
-FdoString* FdoArgumentDefinition::GetName()
+FdoString *FdoArgumentDefinition::GetName ()
 {
     return m_name;
 }
 
 // Gets a brief description of the argument.
-FdoString* FdoArgumentDefinition::GetDescription()
+FdoString *FdoArgumentDefinition::GetDescription ()
 {
     return m_description;
 }
 
 // Gets the DataType of the argument.
-FdoPropertyType FdoArgumentDefinition::GetPropertyType()
+FdoPropertyType FdoArgumentDefinition::GetPropertyType ()
 {
     return m_propertyType;
 }
 
 // Gets the DataType of the argument.
-FdoDataType FdoArgumentDefinition::GetDataType()
+FdoDataType FdoArgumentDefinition::GetDataType ()
 {
     return m_dataType;
+}
+
+// Returns the argument value list of the argument
+FdoPropertyValueConstraintList *FdoArgumentDefinition::GetArgumentValueList ()
+{
+    return FDO_SAFE_ADDREF(m_argumentValueList);
+}
+
+// Sets the argument value list of the argument
+void FdoArgumentDefinition::SetArgumentValueList (FdoPropertyValueConstraintList *argumentValueList)
+{
+    FDO_SAFE_RELEASE(m_argumentValueList);
+    m_argumentValueList = NULL;
+
+    if (argumentValueList == NULL)
+        return;
+
+    m_argumentValueList = FdoPropertyValueConstraintList::Create();
+
+    FdoPtr<FdoDataValueCollection> dest_values = m_argumentValueList->GetConstraintList();
+    FdoPtr<FdoDataValueCollection> src_values = argumentValueList->GetConstraintList();
+    FdoInt32 count = src_values->GetCount();
+    for (FdoInt32 i = 0; i < count; i++)
+    {
+        FdoPtr<FdoDataValue> data_value = src_values->GetItem(i);
+        dest_values->Add(data_value);
+    }
 }
 
