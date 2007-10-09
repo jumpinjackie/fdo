ab3f6a08a8c8e812b960db4c73c4d02184f0884e
thomasknoell <thomasknoell@example.org>
2007-10-09 19:40:21 +0000

    track id #113 (Code changes related to RFC 5 and 8): This enhances the MySQL related filter processor to support additional expression functions.
    git-svn-id: http://svn.osgeo.org/fdo/trunk@3382 d98eaee6-4212-0410-900c-dcc49bf69b99

--- Providers/GenericRdbms/Src/MySQL/Fdo/FdoRdbmsMySqlFilterProcessor.h
+++ Providers/GenericRdbms/Src/MySQL/Fdo/FdoRdbmsMySqlFilterProcessor.h
@@ -25,24 +25,65 @@
 
 #include "../../Fdo/Filter/FdoRdbmsFilterProcessor.h"
 
+// FDO specifies a set of expression functions. The names of those functions may not
+// match the function name in MySQL and hence a mapping of the names must be executed.
+// The following defines the MySQL function name constants for the functions FDO supports
+// that need name mappings.
+//
+// CONVERSION FUNCTIONS
+//
+#define  MYSQL_FUNCTION_NULLVALUE     L"IFNULL"         // Evaluates two expressions and returns the first one if it does not
+                                                        // evaluate to NULL, the second otherwise
+#define  MYSQL_FUNCTION_TODATE_A      L"CONVERT"        // Converts a string to a date if no format information is present
+#define  MYSQL_FUNCTION_TODATE_B      L"STR_TO_DATE"    // Converts a string to a date if format information is present
+#define  MYSQL_FUNCTION_TONUM         L"CONVERT"        // Converts a numeric or string expression to a numeric value
+#define  MYSQL_FUNCTION_TOSTRING_DATE L"DATE_FORMAT"    // Converts a date expression to a string
+#define  MYSQL_FUNCTION_TOSTRING_NUM  L"CONVERT"        // Converts a numeric expression to a string
+//
+// DATE FUNCTIONS
+//
+#define  MYSQL_FUNCTION_ADDMONTHS      L"ADDDATE"       // Adds a specified number of months to a date expression
+#define  MYSQL_FUNCTION_CURRENTDATE    L"CURRENT_DATE"  // Returns the current date
+//
+// NUMERIC FUNCTIONS
+//
+#define  MYSQL_FUNCTION_TRUNC_NUM      L"TRUNCATE"      // Truncates a numeric expression
+//
+// STRING FUNCTIONS
+//
+#define  MYSQL_FUNCTION_SUBSTR         L"SUBSTRING"     // Extracts a substring from a string expression.
+
+
 class FdoRdbmsMySqlFilterProcessor: public FdoRdbmsFilterProcessor
 {
-public:
-    FdoRdbmsMySqlFilterProcessor(FdoRdbmsConnection *connection);
 
+public:
+
+    FdoRdbmsMySqlFilterProcessor(FdoRdbmsConnection *connection);
     ~FdoRdbmsMySqlFilterProcessor(void);
 
 protected:
 
-    virtual void ProcessDistanceCondition(FdoDistanceCondition& filter);
+    virtual void ProcessDistanceCondition (FdoDistanceCondition &filter);
+    virtual void ProcessSpatialCondition (FdoSpatialCondition &filter);
+    const FdoSmLpGeometricPropertyDefinition *GetGeometricProperty (
+                                const FdoSmLpClassDefinition *currentClass,
+                                const wchar_t                *geomPropName);
+    FdoStringP GetGeometryColumnNameForProperty (
+                            const FdoSmLpGeometricPropertyDefinition *pGeomProp,
+                            bool                                      bChangeFilter = false);
+    void ProcessFunction (FdoFunction &expr);
+    void ProcessAggregateFunction (FdoFunction& expr);
+    void ProcessToDoubleFloatFunction (FdoFunction& expr);
+    void ProcessToInt32Int64Function (FdoFunction& expr);
+    void ProcessCurrentDateFunction (FdoFunction& expr);
+    void ProcessTrimFunction (FdoFunction& expr);
+    void ProcessFunctionName (FdoFunction &expr);
+    bool IsDataValue (FdoExpression *expr);
+    virtual bool IsAggregateFunctionName (FdoString *wFunctionName) const;
+    virtual bool IsNotNativeSupportedFunction(FdoString* wFunctionName) const;
+    virtual bool HasNativeSupportedFunctionArguments(FdoFunction& expr) const;
 
-    virtual void ProcessSpatialCondition(FdoSpatialCondition& filter);
-
-    const FdoSmLpGeometricPropertyDefinition* GetGeometricProperty( const FdoSmLpClassDefinition* currentClass, const wchar_t *geomPropName );
-	
-    FdoStringP GetGeometryColumnNameForProperty( const FdoSmLpGeometricPropertyDefinition *pGeomProp, bool bChangeFilter = false);
-
-    virtual bool IsAggregateFunctionName(FdoString* wFunctionName) const;
 };
 
 #endif // FDORDBMSMYSQLFILTERPROCESSOR_H_
