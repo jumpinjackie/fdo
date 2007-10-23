69abde025986cadb14892ac22350c3ce39a62ef5
gregboone <gregboone@example.org>
2007-10-23 23:06:32 +0000

    Ticket [https://trac.osgeo.org/fdo/ticket/144]: Add PostGIS Provider to top level build processes
    git-svn-id: http://svn.osgeo.org/fdo/trunk@3455 d98eaee6-4212-0410-900c-dcc49bf69b99

--- Providers/PostGIS/Src/Overrides/PostGisOverrides.cpp
+++ Providers/PostGIS/Src/Overrides/PostGisOverrides.cpp
@@ -21,7 +21,11 @@
 #include "stdafx.h"
 #include "PostGisOverrides.h"
 
+#ifdef WIN32
+
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
 {
    return TRUE;
 }
+
+#endif
