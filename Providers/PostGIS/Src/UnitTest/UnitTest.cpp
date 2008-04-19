311f75f1f226a1aad43c51e6802b82d0948290a7
mloskot <mloskot@example.org>
2008-04-19 20:59:22 +0000

    [PostGIS] Fixed building of UnitTest project. Added sample configuration file.
    git-svn-id: http://svn.osgeo.org/fdo/trunk@3896 d98eaee6-4212-0410-900c-dcc49bf69b99

--- Providers/PostGIS/Src/UnitTest/UnitTest.cpp
+++ Providers/PostGIS/Src/UnitTest/UnitTest.cpp
@@ -36,17 +36,16 @@ int main(int argc, char* argv[])
 
     try
     {
-#pragma message ("!!TODO!!: Fix UnitTest calss to implicit() that stopped working after migration to boost 1_34_1")
         //
         // Parse command line and configure tests
         //
         bpo::options_description desc("Usage options");
         desc.add_options()
             ("help", "print help message")
-            ("config-file", bpo::value<std::string>(), "set input file with tests configuration")
+            ("config", bpo::value<std::string>(), "path to configuration file with connection settings")
             ("run-test", bpo::value<std::string>(), "name of test case to run")
-            ("run-all", bpo::value<std::string>()/*->implicit()*/, "run all tests (default)")
-            ("wait", bpo::value<std::string>()/*->implicit()*/, "run all tests (default: no)")
+            ("run-all", bpo::value<std::string>(), "run all tests (default)")
+            ("wait", bpo::value<std::string>(), "run all tests (default: no)")
             ;
 
         bpo::variables_map vm;        
@@ -59,9 +58,9 @@ int main(int argc, char* argv[])
             return 1;
         }
 
-        if (vm.count("config-file"))
+        if (vm.count("config"))
         {
-            std::string const& fn = vm["config-file"].as<std::string>();
+            std::string const& fn = vm["config"].as<std::string>();
 
             fdo::postgis::test::gTestConfig.setup(fn);
         }
@@ -90,10 +89,6 @@ int main(int argc, char* argv[])
         rc = 1;
     }
 
-
-    
-
-    //
     //// Run specified test
     //if (!strstr(argv[i], "="))
     //{
@@ -102,93 +97,5 @@ int main(int argc, char* argv[])
     //    all = false;
     //}
 
-
-
     return (rc);
 }
-
-
-// OLD & OBSOLETE
-//
-//int _tmain(int argc, _TCHAR* argv[])
-//{
-//    // Parameters:
-//    //    -GUI          Run the tests using the Windows dialog interface
-//    //    -NoWAIT       When using the text-based interface, do not wait
-//    //                  for the user to press RETURN before exiting.
-//    //    <test>*       When using the text based interface, run only the
-//    //                  specified test(s). Requires support in each tests
-//    //                  cpp file to add the test name to a registry of the
-//    //                  same name, e.g.:
-//    //          CPPUNIT_TEST_SUITE_REGISTRATION (CreateDeleteTests);
-//    //          CPPUNIT_TEST_SUITE_NAMED_REGISTRATION (CreateDeleteTests, "CreateDeleteTests");
-//    //
-//    //  The connect strings values can be passed thru the commandline.
-//    //  The valid options are
-//    //    service=<service value>
-//    //    username=<username value>
-//    //    password=<password value>
-//    //    datastore=<datastore value>
-//
-//    LPTSTR cmd_line = GetCommandLine();
-//
-//    char *tmpCmdLine = new char[strlen(cmd_line)+1];
-//    strcpy(tmpCmdLine, cmd_line);
-//    char *tok = strtok(tmpCmdLine, " ");
-//    while (tok != NULL)
-//    {
-//        char *str;
-//        if (str = strstr(tok, "="))
-//            _putenv(tok);
-//        tok = strtok(NULL, " ");
-//    }
-//    bool gui = false;
-//    strcpy (tmpCmdLine, cmd_line);
-//    _strupr (tmpCmdLine);
-//    gui = (NULL != strstr (tmpCmdLine, "-GUI"));
-//    delete [] tmpCmdLine;
-//
-//	#ifndef __AFXWIN_H__
-//        gui = false; // Revert command line requirement to false
-//	#endif
-//
-//    if (gui)
-//	#ifdef __AFXWIN_H__
-//        return (AfxWinMain (GetModuleHandle (NULL), NULL, cmd_line, SW_SHOW));
-//	#else
-//		return 1; // GUI required and MFC not included....
-//	#endif
-//    else
-//    {
-//        CppUnit::TextUi::TestRunner runner;
-//        bool wait;
-//        bool all;
-//        int ret;
-//
-//        wait = true;
-//        all = true;
-//        if (1 < argc)
-//        {
-//            for (int i = 1; i < argc; i++)
-//                if ('-' == argv[i][0])
-//                    wait &= (0 != _stricmp (argv[i], "-NoWAIT"));
-//                else
-//                {
-//                    if (!strstr(argv[i], "="))
-//                    {
-//                        CppUnit::TestFactoryRegistry& registry = CppUnit::TestFactoryRegistry::getRegistry (argv[i]);
-//                        runner.addTest (registry.makeTest ());
-//                        all = false;
-//                    }
-//                }
-//        }
-//        if (all)
-//            runner.addTest (CppUnit::TestFactoryRegistry::getRegistry().makeTest ());
-//        if (runner.run ("", wait))
-//            ret = 0;
-//        else
-//            ret = 1;
-//	    return (ret);
-//    }
-//}
-
