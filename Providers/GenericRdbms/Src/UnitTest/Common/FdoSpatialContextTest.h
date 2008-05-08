5c3379764d3cd2a9998dfab1504e2c70ce575712
brentrobinson <brentrobinson@example.org>
2008-05-08 17:52:54 +0000

    Ticket[https://trac.osgeo.org/fdo/ticket/213]: SQLServerSpatial: ApplySchema to non-FDO Datastore: Phase III.
    Added unit tests for Create and Destroy Spatial Contexts.
    
    git-svn-id: http://svn.osgeo.org/fdo/trunk@3953 d98eaee6-4212-0410-900c-dcc49bf69b99

---
+++ Providers/GenericRdbms/Src/UnitTest/Common/FdoSpatialContextTest.h
@@ -0,0 +1,58 @@
+ //
+ // Copyright (C) 2004-2006  Autodesk, Inc.
+ // 
+ // This library is free software; you can redistribute it and/or
+ // modify it under the terms of version 2.1 of the GNU Lesser
+ // General Public License as published by the Free Software Foundation.
+ // 
+ // This library is distributed in the hope that it will be useful,
+ // but WITHOUT ANY WARRANTY; without even the implied warranty of
+ // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ // Lesser General Public License for more details.
+ // 
+ // You should have received a copy of the GNU Lesser General Public
+ // License along with this library; if not, write to the Free Software
+ // Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+#ifndef CPP_UNIT_FDOSPATIALCONTEXTTEST_H
+#define CPP_UNIT_FDOSPATIALCONTEXTTEST_H
+
+class StaticConnection;
+
+/* 
+ * A test case for the schema.
+ * Loads a set of Feature schemas, in logical-physical 
+ * form, from a database.
+ *
+ * The schemas are then written to an XML file.
+ */
+class FdoSpatialContextTest : public CppUnit::TestCase
+{
+  CPPUNIT_TEST_SUITE( FdoSpatialContextTest );
+  CPPUNIT_TEST( testMeta );
+  CPPUNIT_TEST( testNoMeta );
+  CPPUNIT_TEST_SUITE_END();
+
+public:
+    FdoSpatialContextTest(void);
+    virtual ~FdoSpatialContextTest(void);
+    void setUp ();
+
+protected:
+    virtual void set_provider() {};
+
+    // Unit test methods:
+    virtual void testMeta ();
+	virtual void testNoMeta();
+
+    // Helper methods:
+    virtual void DoTest( bool hasMetaSchema );
+    virtual void CreateSchema( FdoIConnection* connection, bool hasMetaSchema );
+    virtual void InsertFeatures( FdoIConnection* connection );
+
+    virtual void CreateTables( StaticConnection* staticConn, FdoStringP datastore );
+
+    FdoStringP GenFileName( int fileNum, bool hasMetaSchema, bool isMaster );
+};
+
+#endif // CPP_UNIT_FDOSPATIALCONTEXTTEST_H
