dece1b2c313c4793b65f752072226540c3bddf8e
traianstanev <traianstanev@example.org>
2008-11-17 17:20:55 +0000

    Cleanup of SpatialIndex project, moved some defines away from the precompiled header into a new header that can be included from other projects.
    git-svn-id: http://svn.osgeo.org/fdo/trunk@4238 d98eaee6-4212-0410-900c-dcc49bf69b99

--- Providers/SQLite/SpatialIndex/stdafx.h
+++ Providers/SQLite/SpatialIndex/stdafx.h
@@ -38,17 +38,6 @@
 //FDO headers
 #include "Fdo.h"
 
-//define this if linking to FdoSpatial utility is OK
-#define SLT_USE_FDO_SPATIAL
-
-#ifdef SLT_USE_FDO_SPATIAL
-    #include "FdoSpatial.h"
-#endif
-
-#ifndef _MSC_VER
-  #define __restrict __restrict__
-  #define __forceinline __inline__
-  #define __fastcall 
-#endif
+#include "si_api.h"
 
 #endif
