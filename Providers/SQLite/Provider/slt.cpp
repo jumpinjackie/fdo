7927658e590907dad90621a56293f9de986d1294
traianstanev <traianstanev@example.org>
2008-11-20 15:42:09 +0000

    Removed unused function, converted some newlines from Unix to Windows format to avoid Visual Studio warnings.
    git-svn-id: http://svn.osgeo.org/fdo/trunk@4251 d98eaee6-4212-0410-900c-dcc49bf69b99

--- Providers/SQLite/Provider/slt.cpp
+++ Providers/SQLite/Provider/slt.cpp
@@ -17,24 +17,3 @@
 //  
 
 #include "stdafx.h"
-#include "slt.h"
-
-#ifdef _WINDOWS
-BOOL APIENTRY DllMain( HMODULE hModule,
-                       DWORD  ul_reason_for_call,
-                       LPVOID lpReserved
-					 )
-{
-	switch (ul_reason_for_call)
-	{
-	case DLL_PROCESS_ATTACH:
-	case DLL_THREAD_ATTACH:
-	case DLL_THREAD_DETACH:
-	case DLL_PROCESS_DETACH:
-		break;
-	}
-    return TRUE;
-}
-
-#endif
-
