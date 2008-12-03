dfcb0a0d425d8e0206e37aafb78ff4d488610659
jacklee <jacklee@example.org>
2008-12-03 20:12:17 +0000

    Ticket [https://trac.osgeo.org/fdo/ticket/416]: Change the version to 3.4
    git-svn-id: http://svn.osgeo.org/fdo/trunk@4290 d98eaee6-4212-0410-900c-dcc49bf69b99

--- Fdo/Python/UnitTest/UnitTests.sh
+++ Fdo/Python/UnitTest/UnitTests.sh
@@ -23,6 +23,6 @@
 # Description: Executes the unit tests for the Python wrappers on Linux
 #  
 
-export PYTHONPATH=/usr/local/fdo-3.3.0/lib:$FDO/Python/UnitTest/Lib:$PYTHONPATH
+export PYTHONPATH=/usr/local/fdo-3.4.0/lib:$FDO/Python/UnitTest/Lib:$PYTHONPATH
 
 python Src/main.py
