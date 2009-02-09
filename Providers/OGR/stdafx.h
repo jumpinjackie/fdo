f5f700a88a24d47391075676a99f55c66cfd1b7b
traianstanev <traianstanev@example.org>
2009-02-09 19:08:03 +0000

    Ticket [https://trac.osgeo.org/fdo/ticket/405]. I'm checking in the code necessary for the UTF8 build of the OGR provider. The code is (for now) commented out in preference to the locale-dependent string conversion code, until we figure out which version is better overall.
    git-svn-id: http://svn.osgeo.org/fdo/branches/3.3@4371 d98eaee6-4212-0410-900c-dcc49bf69b99

--- Providers/OGR/stdafx.h
+++ Providers/OGR/stdafx.h
@@ -3,48 +3,9 @@
 #include <Fdo.h>
 #include <wchar.h>
 
-//FDO 3.2 to 3.1 renaming -- remove these when FDO 3.2 is out.
-//#define FDO_31
-#ifdef FDO_31
-#define FdoGeometryType GisGeometryType
-#define FdoGeometryComponentType GisGeometryComponentType
-#define FdoIReader GisIReader
-#define FdoByte GisByte
-#define FdoByteArray GisByteArray
-#define FdoString GisString
-#define FdoInt16 GisInt16
-#define FdoInt32 GisInt32
-#define FdoInt64 GisInt64
-#define FdoIoStream GisIoStream
-#define FdoDateTime GisDateTime
-#define FdoIStreamReader GisIStreamReader
-#define FDO_SAFE_RELEASE GIS_SAFE_RELEASE
-#define FDO_SAFE_ADDREF GIS_SAFE_ADDREF
-#define FdoIPolygon GisIPolygon
-#define FdoIGeometry GisIGeometry
-#define FdoPtr GisPtr
-#define FdoFgfGeometryFactory GisAgfGeometryFactory
-#define CreateGeometryFromFgf CreateGeometryFromAgf
-#define FdoIEnvelope GisIEnvelope
-#define GetFgf GetAgf
-#define FdoILinearRing GisILinearRing
-#define FdoGeometryType_Point GisGeometryType_Point
-#define FdoGeometryType_LineString GisGeometryType_LineString
-#define FdoGeometryType_Polygon GisGeometryType_Polygon
-#define FdoGeometryType_MultiPoint GisGeometryType_MultiPoint
-#define FdoGeometryType_MultiLineString GisGeometryType_MultiLineString
-#define FdoGeometryType_MultiPolygon GisGeometryType_MultiPolygon
-#define FdoGeometryType_MultiGeometry GisGeometryType_MultiGeometry
-#define FdoGeometryComponentType_LinearRing GisGeometryComponentType_LinearRing
-#define FdoGeometryComponentType_LineStringSegment GisGeometryComponentType_LineStringSegment
-#define FdoGeometryComponentType_Ring GisGeometryComponentType_Ring
-#define FdoDimensionality_XY GisDimensionality_XY
-#define FdoDimensionality_Z GisDimensionality_Z
-#define FdoDimensionality_M GisDimensionality_M
-#endif
-
 #ifdef _WIN32
 #define OGR_API __declspec(dllexport)
+#include <windows.h>
 #else
 #define OGR_API
 #endif
@@ -57,7 +18,18 @@
 #include <ogrsf_frmts.h>
 
 
-//TODO: usage of wcstombs/mbstowcs is incorrect in most cases
+#ifdef _WIN32_DISABLE_XXX
+#define W2A(x) \
+    int len##x = (int)wcslen(x)*4+1; \
+    char* mb##x = (char*)alloca(len##x); \
+    WideCharToMultiByte(CP_UTF8, 0, x, -1, mb##x, len##x, 0, 0);
+
+#define A2W(x) \
+    int len##x = (int)strlen(x)*4+4; \
+    wchar_t* w##x = (wchar_t*)alloca(len##x); \
+    MultiByteToWideChar(CP_UTF8, 0, x, -1, w##x, len##x);
+#else
+//TODO: Fix this for Linux, so that it always does UTF8 conversion
 #define A2W(x) \
     size_t len##x = strlen(x)*4+4; \
     wchar_t* w##x = (wchar_t*)alloca(len##x); \
@@ -67,6 +39,6 @@
     size_t len##x = wcslen(x)*4+1; \
     char* mb##x = (char*)alloca(len##x); \
     wcstombs(mb##x, x, len##x);
-
+#endif
 
 
