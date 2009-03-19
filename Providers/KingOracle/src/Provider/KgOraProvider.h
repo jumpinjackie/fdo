cdd8ca63fbc92295a4e5798c89e3a997b1810f63
haris <haris@example.org>
2009-03-19 21:09:16 +0000

    git-svn-id: http://svn.osgeo.org/fdo/trunk@4453 d98eaee6-4212-0410-900c-dcc49bf69b99

--- Providers/KingOracle/src/Provider/KgOraProvider.h
+++ Providers/KingOracle/src/Provider/KgOraProvider.h
@@ -22,9 +22,9 @@
 #define KGORAPROVIDER_H
 
 
-#define D_KGORA_PROVIDER_NAME					L"King.Oracle.0.1.1"
-#define D_KGORA_PROVIDER_VERSION				L"0.1.1.0"
-#define D_KGORA_FDO_VERSION						L"3.1.0.0"
+#define D_KGORA_PROVIDER_NAME					L"King.Oracle.3.4"
+#define D_KGORA_PROVIDER_VERSION				L"3.4.0.0"
+#define D_KGORA_FDO_VERSION						L"3.4.0.0"
 
 #define D_KGORA_PROVIDER_DEFAULT_DISPLAY_NAME	"FDO Provider for Oracle"
 #define D_KGORA_PROVIDER_DEFAULT_DESCRIPTION	"Read/write access to spatial and attribute data in an Oracle Spatial."
