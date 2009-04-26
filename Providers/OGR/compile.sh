0913feeeabcbc19444e86407c973914cd9d2c669
gregboone <gregboone@example.org>
2009-04-26 18:48:06 +0000

    Ticket [https://trac.osgeo.org/fdo/ticket/512]: Upgrade FDO Version Number to 3.5.0
    git-svn-id: http://svn.osgeo.org/fdo/trunk@4577 d98eaee6-4212-0410-900c-dcc49bf69b99

--- Providers/OGR/compile.sh
+++ Providers/OGR/compile.sh
@@ -1,2 +1,2 @@
-g++ -O2 -fPIC OgrFdoUtil.cpp OgrProvider.cpp -I/usr/local/fdo-3.4.0/include -L/usr/local/fdo-3.4.0/lib -lFDO -lgdal -o libOGRProvider.so
+g++ -O2 -fPIC OgrFdoUtil.cpp OgrProvider.cpp -I/usr/local/fdo-3.5.0/include -L/usr/local/fdo-3.5.0/lib -lFDO -lgdal -o libOGRProvider.so
 
