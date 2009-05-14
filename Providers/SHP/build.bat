8be1c2eefe966510a63e99e26d141f7eb476862c
gregboone <gregboone@example.org>
2009-05-14 20:01:16 +0000

    #520 Update Build. Simplify ENV variable names in build.bat scripts.
    git-svn-id: http://svn.osgeo.org/fdo/trunk@4678 d98eaee6-4212-0410-900c-dcc49bf69b99

--- Providers/SHP/build.bat
+++ Providers/SHP/build.bat
@@ -17,17 +17,18 @@ rem License along with this library; if not, write to the Free Software
 rem Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 rem 
 
-SET TYPEACTIONSHP=build
-SET MSACTIONSHP=Build
-SET TYPEBUILDSHP=release
-SET TYPEPLATFORMSHP=Win32
-SET FDOORGPATHSHP=%cd%
-SET FDOINSPATHSHP=%cd%\Fdo
-SET FDOBINPATHSHP=%cd%\Fdo\Bin
-SET FDOINCPATHSHP=%cd%\Fdo\Inc
-SET FDOLIBPATHSHP=%cd%\Fdo\Lib
-SET FDODOCPATHSHP=%cd%\Fdo\Docs
-SET DOCENABLESHP=skip
+SET TYPEACTION=build
+SET MSACTION=Build
+SET TYPEBUILD=release
+SET TYPEPLATFORM=Win32
+SET INTERMEDIATEDIR=Win32
+SET FDOORGPATH=%cd%
+SET FDOINSPATH=%cd%\Fdo
+SET FDOBINPATH=%cd%\Fdo\Bin
+SET FDOINCPATH=%cd%\Fdo\Inc
+SET FDOLIBPATH=%cd%\Fdo\Lib
+SET FDODOCPATH=%cd%\Fdo\Docs
+SET DOCENABLE=skip
 SET FDOERROR=0
 
 :study_params
@@ -54,19 +55,19 @@ if "%1"=="-docs"    goto get_docs
 goto custom_error
 
 :get_docs
-SET DOCENABLESHP=%2
+SET DOCENABLE=%2
 if "%2"=="build" goto next_param
 if "%2"=="skip" goto next_param
 goto custom_error
 
 :get_conf 
-SET TYPEBUILDSHP=%2
+SET TYPEBUILD=%2
 if "%2"=="release" goto next_param
 if "%2"=="debug" goto next_param
 goto custom_error
 
 :get_action
-SET TYPEACTIONSHP=%2
+SET TYPEACTION=%2
 if "%2"=="install" goto next_param
 if "%2"=="build" goto next_param
 if "%2"=="buildinstall" goto next_param
@@ -74,19 +75,19 @@ if "%2"=="clean" goto next_param
 goto custom_error 
 
 :get_platform
-SET TYPEPLATFORMSHP=%2
+SET TYPEPLATFORM=%2
 if "%2"=="Win32" goto next_param
 if "%2"=="x64" goto next_param
 goto custom_error
 
 :get_path
 if (%2)==() goto custom_error
-SET FDOORGPATHSHP=%~2
-SET FDOINSPATHSHP=%~2\Fdo
-SET FDOBINPATHSHP=%~2\Fdo\Bin
-SET FDOINCPATHSHP=%~2\Fdo\Inc
-SET FDOLIBPATHSHP=%~2\Fdo\Lib
-SET FDODOCPATHSHP=%~2\Fdo\Docs
+SET FDOORGPATH=%~2
+SET FDOINSPATH=%~2\Fdo
+SET FDOBINPATH=%~2\Fdo\Bin
+SET FDOINCPATH=%~2\Fdo\Inc
+SET FDOLIBPATH=%~2\Fdo\Lib
+SET FDODOCPATH=%~2\Fdo\Docs
 
 :next_param
 shift
@@ -97,60 +98,66 @@ goto study_params
 SET FDOACTENVSTUDY="FDO"
 if ("%FDO%")==("") goto env_error
 if not exist "%FDO%" goto env_path_error
+
 SET FDOACTENVSTUDY="FDOTHIRDPARTY"
 if ("%FDOTHIRDPARTY%")==("") goto env_error
 if not exist "%FDOTHIRDPARTY%" goto env_path_error
+
 SET FDOACTENVSTUDY="FDOUTILITIES"
 if ("%FDOUTILITIES%")==("") goto env_error
 if not exist "%FDOUTILITIES%" goto env_path_error
 
-if "%TYPEACTIONSHP%"=="build" goto start_exbuild
-if "%TYPEACTIONSHP%"=="clean" goto start_exbuild
-if not exist "%FDOINSPATHSHP%" mkdir "%FDOINSPATHSHP%"
-if not exist "%FDOBINPATHSHP%" mkdir "%FDOBINPATHSHP%"
-if not exist "%FDOINCPATHSHP%" mkdir "%FDOINCPATHSHP%"
-if not exist "%FDOLIBPATHSHP%" mkdir "%FDOLIBPATHSHP%"
-if not exist "%FDODOCPATHSHP%" mkdir "%FDODOCPATHSHP%"
+if "%TYPEBUILD%"=="Win32" SET INTERMEDIATEDIR="Win32"
+if "%TYPEBUILD%"=="x64" SET INTERMEDIATEDIR="Win64"
+
+if "%TYPEACTION%"=="build" goto start_exbuild
+if "%TYPEACTION%"=="clean" goto start_exbuild
+
+if not exist "%FDOINSPATH%" mkdir "%FDOINSPATH%"
+if not exist "%FDOBINPATH%" mkdir "%FDOBINPATH%"
+if not exist "%FDOINCPATH%" mkdir "%FDOINCPATH%"
+if not exist "%FDOLIBPATH%" mkdir "%FDOLIBPATH%"
+if not exist "%FDODOCPATH%" mkdir "%FDODOCPATH%"
 
 :start_exbuild
-if "%TYPEACTIONSHP%"=="clean" SET MSACTIONSHP=Clean
-if "%TYPEACTIONSHP%"=="install" goto install_files_shp
+if "%TYPEACTION%"=="clean" SET MSACTION=Clean
+if "%TYPEACTION%"=="install" goto install_files_shp
 
-echo %MSACTIONSHP% %TYPEBUILDSHP% SHP provider dlls
+echo %MSACTION% %TYPEBUILD% SHP provider dlls
 SET FDOACTIVEBUILD=%cd%\Src\SHP
 cscript //Nologo //job:prepare preparebuilds.wsf
 pushd Src
-msbuild SHP_temp.sln /t:%MSACTIONSHP% /p:Configuration=%TYPEBUILDSHP% /p:Platform=%TYPEPLATFORMSHP% /nologo /consoleloggerparameters:NoSummary
+msbuild SHP_temp.sln /t:%MSACTION% /p:Configuration=%TYPEBUILD% /p:Platform=%TYPEPLATFORM% /nologo /consoleloggerparameters:NoSummary
 SET FDOERROR=%errorlevel%
 if exist SHP_temp.sln del /Q /F SHP_temp.sln
 popd
 if "%FDOERROR%"=="1" goto error
-if "%TYPEACTIONSHP%"=="clean" goto end
-if "%TYPEACTIONSHP%"=="build" goto generate_docs
+if "%TYPEACTION%"=="clean" goto end
+if "%TYPEACTION%"=="build" goto generate_docs
 
 :install_files_shp
-echo copy %TYPEBUILDSHP% SHP provider output files
-copy /y "Managed\bin\%TYPEBUILDSHP%\OSGeo.FDO.Providers.SHP.Overrides.dll" "%FDOBINPATHSHP%"
-copy /y "Managed\bin\%TYPEBUILDSHP%\OSGeo.FDO.Providers.SHP.Overrides.pdb" "%FDOBINPATHSHP%"
-copy /y "Bin\Win32\%TYPEBUILDSHP%\SHPProvider.dll" "%FDOBINPATHSHP%"
-copy /y "Bin\Win32\%TYPEBUILDSHP%\SHPProvider.pdb" "%FDOBINPATHSHP%"
-copy /y "Bin\Win32\%TYPEBUILDSHP%\SHPOverrides.dll" "%FDOBINPATHSHP%"
-copy /y "Bin\Win32\%TYPEBUILDSHP%\SHPOverrides.pdb" "%FDOBINPATHSHP%"
-copy /y "Bin\Win32\%TYPEBUILDSHP%\SHPMessage.dll" "%FDOBINPATHSHP%"
-copy /y "Bin\Win32\%TYPEBUILDSHP%\SHPMessage.pdb" "%FDOBINPATHSHP%"
-copy /y "Lib\Win32\%TYPEBUILDSHP%\SHPOverrides.lib" "%FDOLIBPATHSHP%"
-copy /y "%FDOUTILITIES%\ExpressionEngine\bin\win32\%TYPEBUILDSHP%\ExpressionEngine.dll" "%FDOBINPATHSHP%"
-copy /y "%FDOUTILITIES%\ExpressionEngine\bin\win32\%TYPEBUILDSHP%\ExpressionEngine.pdb" "%FDOBINPATHSHP%"
-copy /y "%FDOUTILITIES%\ExpressionEngine\lib\win32\%TYPEBUILDSHP%\ExpressionEngine.lib" "%FDOLIBPATHSHP%"
+echo copy %TYPEBUILD% SHP provider output files
+copy /y "Managed\bin\%TYPEBUILD%\OSGeo.FDO.Providers.SHP.Overrides.dll" "%FDOBINPATH%"
+copy /y "Managed\bin\%TYPEBUILD%\OSGeo.FDO.Providers.SHP.Overrides.pdb" "%FDOBINPATH%"
+copy /y "Bin\%INTERMEDIATEDIR%\%TYPEBUILD%\SHPProvider.dll" "%FDOBINPATH%"
+copy /y "Bin\%INTERMEDIATEDIR%\%TYPEBUILD%\SHPProvider.pdb" "%FDOBINPATH%"
+copy /y "Bin\%INTERMEDIATEDIR%\%TYPEBUILD%\SHPOverrides.dll" "%FDOBINPATH%"
+copy /y "Bin\%INTERMEDIATEDIR%\%TYPEBUILD%\SHPOverrides.pdb" "%FDOBINPATH%"
+copy /y "Bin\%INTERMEDIATEDIR%\%TYPEBUILD%\SHPMessage.dll" "%FDOBINPATH%"
+copy /y "Bin\%INTERMEDIATEDIR%\%TYPEBUILD%\SHPMessage.pdb" "%FDOBINPATH%"
+copy /y "Lib\%INTERMEDIATEDIR%\%TYPEBUILD%\SHPOverrides.lib" "%FDOLIBPATH%"
+copy /y "%FDOUTILITIES%\ExpressionEngine\bin\%INTERMEDIATEDIR%\%TYPEBUILD%\ExpressionEngine.dll" "%FDOBINPATH%"
+copy /y "%FDOUTILITIES%\ExpressionEngine\bin\%INTERMEDIATEDIR%\%TYPEBUILD%\ExpressionEngine.pdb" "%FDOBINPATH%"
+copy /y "%FDOUTILITIES%\ExpressionEngine\lib\%INTERMEDIATEDIR%\%TYPEBUILD%\ExpressionEngine.lib" "%FDOLIBPATH%"
 
 echo copy header files
-xcopy /S /C /Q /R /Y Inc\SHP\*.h "%FDOINCPATHSHP%\SHP\"
-xcopy /C /Q /R /Y /I "%FDOUTILITIES%\ExpressionEngine\Inc\*.h" "%FDOINCPATHSHP%\ExpressionEngine"
-del /Q/F "%FDOINCPATHSHP%\ExpressionEngine\FdoExpressionEngineImp.h"
-xcopy /C /Q /R /Y /I "%FDOUTILITIES%\ExpressionEngine\Inc\Util\*.h" "%FDOINCPATHSHP%\ExpressionEngine\Util"
+xcopy /S /C /Q /R /Y Inc\SHP\*.h "%FDOINCPATH%\SHP\"
+xcopy /C /Q /R /Y /I "%FDOUTILITIES%\ExpressionEngine\Inc\*.h" "%FDOINCPATH%\ExpressionEngine"
+del /Q/F "%FDOINCPATH%\ExpressionEngine\FdoExpressionEngineImp.h"
+xcopy /C /Q /R /Y /I "%FDOUTILITIES%\ExpressionEngine\Inc\Util\*.h" "%FDOINCPATH%\ExpressionEngine\Util"
 
 :generate_docs
-if "%DOCENABLESHP%"=="skip" goto install_docs
+if "%DOCENABLE%"=="skip" goto install_docs
 echo Creating SHP provider html and chm documentation
 if exist "Docs\HTML\SHP" rmdir /S /Q "Docs\HTML\SHP"
 if not exist "Docs\HTML\SHP" mkdir "Docs\HTML\SHP"
@@ -168,16 +175,16 @@ doxygen Doxyfile_SHP_managed
 popd
 
 :install_docs
-if "%TYPEACTIONSHP%"=="build" goto end
-if exist "%FDODOCPATHSHP%\HTML\Providers\SHP" rmdir /S /Q "%FDODOCPATHSHP%\HTML\Providers\SHP"
-if exist Docs\HTML\SHP xcopy/CQEYI Docs\HTML\SHP\* "%FDODOCPATHSHP%\HTML\Providers\SHP"
-if exist "Docs\SHP_Provider_API.chm" copy /y "Docs\SHP_Provider_API.chm" "%FDODOCPATHSHP%"
-if exist "%FDODOCPATHSHP%\HTML\Providers\SHP_managed" rmdir /S /Q "%FDODOCPATHSHP%\HTML\Providers\SHP_managed"
-if exist Docs\HTML\SHP_managed xcopy/CQEYI Docs\HTML\SHP_managed\* "%FDODOCPATHSHP%\HTML\Providers\SHP_managed"
-if exist "Docs\SHP_Provider_API_managed.chm" copy /y "Docs\SHP_Provider_API_managed.chm" "%FDODOCPATHSHP%"
+if "%TYPEACTION%"=="build" goto end
+if exist "%FDODOCPATH%\HTML\Providers\SHP" rmdir /S /Q "%FDODOCPATH%\HTML\Providers\SHP"
+if exist Docs\HTML\SHP xcopy/CQEYI Docs\HTML\SHP\* "%FDODOCPATH%\HTML\Providers\SHP"
+if exist "Docs\SHP_Provider_API.chm" copy /y "Docs\SHP_Provider_API.chm" "%FDODOCPATH%"
+if exist "%FDODOCPATH%\HTML\Providers\SHP_managed" rmdir /S /Q "%FDODOCPATH%\HTML\Providers\SHP_managed"
+if exist Docs\HTML\SHP_managed xcopy/CQEYI Docs\HTML\SHP_managed\* "%FDODOCPATH%\HTML\Providers\SHP_managed"
+if exist "Docs\SHP_Provider_API_managed.chm" copy /y "Docs\SHP_Provider_API_managed.chm" "%FDODOCPATH%"
 
 :end
-echo End SHP %MSACTIONSHP%
+echo End SHP %MSACTION%
 exit /B 0
 
 :env_error
@@ -196,7 +203,7 @@ SET FDOERROR=1
 exit /B 1
 
 :error
-echo There was a build error executing action: %MSACTIONSHP%
+echo There was a build error executing action: %MSACTION%
 exit /B 1
 
 :custom_error
