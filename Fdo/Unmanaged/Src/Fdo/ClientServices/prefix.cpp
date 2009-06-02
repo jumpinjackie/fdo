e9c5b13844503ac452018a493a6346e089fc87ef
helio <helio@example.org>
2009-06-02 15:46:19 +0000

    const char build fix against new gcc ( specific warning now raises errors ). Ack by Greg Boone on fdo-internals
    git-svn-id: http://svn.osgeo.org/fdo/trunk@4730 d98eaee6-4212-0410-900c-dcc49bf69b99

--- Fdo/Unmanaged/Src/Fdo/ClientServices/prefix.cpp
+++ Fdo/Unmanaged/Src/Fdo/ClientServices/prefix.cpp
@@ -380,7 +380,8 @@ br_strndup (char *str, size_t size)
 char *
 br_extract_dir (const char *path)
 {
-	char *end, *result;
+	const char *end;
+	char *result;
 
 	br_return_val_if_fail (path != NULL, NULL);
 
@@ -414,7 +415,8 @@ br_extract_dir (const char *path)
 char *
 br_extract_prefix (const char *path)
 {
-	char *end, *tmp, *result;
+	const char *end;
+	char *tmp, *result;
 
 	br_return_val_if_fail (path != NULL, NULL);
 
