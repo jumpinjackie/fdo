26eec5700ee9b3d98f7f33de4038886d98af4fbb
helio <helio@example.org>
2009-06-02 14:08:11 +0000

    - Missing include header required for newer gcc's
    git-svn-id: http://svn.osgeo.org/fdo/trunk@4728 d98eaee6-4212-0410-900c-dcc49bf69b99

--- Fdo/UnitTest/app.cpp
+++ Fdo/UnitTest/app.cpp
@@ -15,6 +15,7 @@
 
 #include <cstring>
 #include <cstdlib>
+#include <cstdio>
 #include <cppunit/CompilerOutputter.h>
 #include <cppunit/extensions/TestFactoryRegistry.h>
 #include <cppunit/ui/text/TestRunner.h>
