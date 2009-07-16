ba5ae8ad158f7be01d6fccf99b0bba3addaea59e
brentrobinson <brentrobinson@example.org>
2009-07-16 13:24:44 +0000

    Ticket#540: temporary rollback.
    git-svn-id: http://svn.osgeo.org/fdo/trunk@4842 d98eaee6-4212-0410-900c-dcc49bf69b99

--- Providers/WFS/Src/Provider/FdoWfsExpressionCapabilities.cpp
+++ Providers/WFS/Src/Provider/FdoWfsExpressionCapabilities.cpp
@@ -17,11 +17,6 @@
  */
 
 #include "stdafx.h"
-#include <FdoExpressionEngine.h>
-#include <Functions/Geometry/FdoFunctionX.h>
-#include <Functions/Geometry/FdoFunctionY.h>
-#include <Functions/Geometry/FdoFunctionZ.h>
-#include <Functions/Geometry/FdoFunctionM.h>
 
 FdoWfsExpressionCapabilities::FdoWfsExpressionCapabilities ()
 {
@@ -61,25 +56,6 @@ FdoFunctionDefinitionCollection* FdoWfsExpressionCapabilities::GetFunctions()
 	FdoPtr<FdoFunctionDefinitionCollection> wellKnownFunctions = GetWellKnownFunctions ();
 	FdoPtr<FdoFunctionDefinition> wellKnownFunction = wellKnownFunctions->GetItem(FDO_FUNCTION_SPATIALEXTENTS);
     ret->Add(wellKnownFunction);
-
-    // Add function X to the list of supported function
-    FdoPtr<FdoExpressionEngineIFunction> funcX = FdoFunctionX::Create();
-    ret->Add(FdoPtr<FdoFunctionDefinition>(funcX->GetFunctionDefinition()));
-
-    // Add function Y to the list of supported function
-    FdoPtr<FdoExpressionEngineIFunction> funcY = FdoFunctionY::Create();
-    ret->Add(FdoPtr<FdoFunctionDefinition>(funcY->GetFunctionDefinition()));
-
-    // Add function Z to the list of supported function
-    FdoPtr<FdoExpressionEngineIFunction> funcZ = FdoFunctionZ::Create();
-    ret->Add(FdoPtr<FdoFunctionDefinition>(funcZ->GetFunctionDefinition()));
-
-    // Add function M to the list of supported function
-    FdoPtr<FdoExpressionEngineIFunction> funcM = FdoFunctionM::Create();
-    ret->Add(FdoPtr<FdoFunctionDefinition>(funcM->GetFunctionDefinition()));
-
-    //TODO: it would be possible to support all functions handled by the Expression Engine.
-    //However, need modifications to FdoWfsSelectAggregatesCommand first.
 
     return (FDO_SAFE_ADDREF (ret.p));
 }
