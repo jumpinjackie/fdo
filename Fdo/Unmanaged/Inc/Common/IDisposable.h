81815ff50c1ae2db9f81e6c71ddf7ced7d1702ff
gregboone <gregboone@example.org>
2010-01-25 22:49:17 +0000

    RFC47: Add Thread Support to FdoIDisposable AddRef/Release
    git-svn-id: http://svn.osgeo.org/fdo/trunk@5254 d98eaee6-4212-0410-900c-dcc49bf69b99

--- Fdo/Unmanaged/Inc/Common/IDisposable.h
+++ Fdo/Unmanaged/Inc/Common/IDisposable.h
@@ -22,16 +22,6 @@
 #pragma once
 #endif
 
-#ifdef _WIN32
-#define WIN32_LEAN_AND_MEAN	// Exclude rarely-used stuff from Windows headers
-#include <windows.h>
-#ifdef UNICODE
-#define GetClassNameW  GetClassName
-#else
-#define GetClassNameA  GetClassName
-#endif // !UNICODE
-#endif
-
 /// \brief
 /// FdoIDisposable is the base interface for all classes that implement this
 /// standard interface for reference counting and object destruction.
@@ -116,72 +106,6 @@ private:
     bool m_objectThreadLockingEnabled;
     static bool m_globalThreadLockingEnabled;
 };
-
-#include <Common/Exception.h>
-
-/// \brief
-/// Default implementation of FdoIDisposable::AddRef()
-///
-inline
-FdoInt32 FdoIDisposable::AddRef()
-{
-    if (m_globalThreadLockingEnabled || m_objectThreadLockingEnabled)
-    {
-#if defined(__GNUC__) && defined (__GNUC_MINOR__) && ((__GNUC__ < 4) || (__GNUC__ == 4 && __GNUC_MINOR__ < 1))
-        int ret = 1;
-        __asm__ __volatile__("lock;xaddl %0,%1"
-            : "=r"(ret),"=m"(m_refCount)
-            : "0"(ret),"m"(m_refCount)
-            : "memory","cc");
-        return ret + 1;
-#elif defined (__GNUC__)
-        return __sync_add_and_fetch(&m_refCount,1);
-#elif defined(_WIN32)
-        return InterlockedIncrement((LONG*)&m_refCount);
-#endif
-    }
-
-    return ++m_refCount;
-}
-
-/// \brief
-/// Default implementation of FdoIDisposable::Release()
-///
-inline
-FdoInt32 FdoIDisposable::Release()
-{
-#ifdef _DEBUG
-    if (m_refCount <= 0)
-        throw FdoException::Create(FdoException::NLSGetMessage(FDO_NLSID(FDO_1_MEMORY_DEALLOCATION_ERROR), 
-                                                           L"FdoIDisposable::Release",
-                                                           L"FdoIDisposable"));
-#endif
-    
-    if (m_globalThreadLockingEnabled || m_objectThreadLockingEnabled)
-    {
-#if defined(__GNUC__) && defined (__GNUC_MINOR__) && ((__GNUC__ < 4) || (__GNUC__ == 4 && __GNUC_MINOR__ < 1))
-        int ret = -1;
-        __asm__ __volatile__("lock;xaddl %0,%1" 
-            : "=r"(ret),"=m"(m_refCount) 
-            : "0"(ret),"m"(m_refCount) 
-            : "memory","cc");
-#elif defined (__GNUC__)
-        __sync_sub_and_fetch(&m_refCount, 1);
-#elif defined(_WIN32)
-        InterlockedDecrement((LONG*)&m_refCount);
-#endif
-    }
-    else
-    {
-        --m_refCount;
-    }
-
-    if (0 != m_refCount)
-        return m_refCount;
-
-    Dispose();
-    return 0;
-}
 
 /// \brief
 /// Define helper macros FDO_SAFE_ADDREF/FDO_SAFE_RELEASE
