b18257e597a4bff369f1d1770e20ef1527bbfda7
gregboone <gregboone@example.org>
2010-03-26 17:44:42 +0000

    Ticket #631: Update Apache to Xerces 3.1 and Xalan 1.11
    git-svn-id: http://svn.osgeo.org/fdo/trunk@5418 d98eaee6-4212-0410-900c-dcc49bf69b99

---
+++ Thirdparty/apache/build2.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+##
+## Copyright (C) 2004-2010  Autodesk, Inc.
+## 
+## This library is free software; you can redistribute it and/or
+## modify it under the terms of version 2.1 of the GNU Lesser
+## General Public License as published by the Free Software Foundation.
+## 
+## This library is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+## Lesser General Public License for more details.
+## 
+## You should have received a copy of the GNU Lesser General Public
+## License along with this library; if not, write to the Free Software
+## Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+##
+
+
+export XERCESCROOT=$PWD/xerces
+export XALANCROOT=$PWD/xalan
+export NLSDIR=$XALANCROOT/src/xalanc/NLS
+
+cd xerces
+./runConfigure -p linux -P "$XERCESCROOT"
+make
+make install
+cd ..
+
+cd xalan
+./runConfigure -p linux -P "$XALANCROOT"
+make
+make install
+cd ..
+
--- Thirdparty/apache/xerces/src/xercesc/util/Xerces_autoconf_config.hpp
+++ Thirdparty/apache/xerces/src/xercesc/util/Xerces_autoconf_config.hpp
@@ -1,3 +1,4 @@
+/* src/xercesc/util/Xerces_autoconf_config.hpp.  Generated from Xerces_autoconf_config.hpp.in by configure.  */
 /*
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
@@ -5,9 +6,9 @@
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
- *
+ * 
  *      http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,11 +17,8 @@
  */
 
 /*
- * $Id: Xerces_autoconf_config.msvc.hpp 883299 2009-11-23 10:34:54Z amassari $
+ * $Id: Xerces_autoconf_config.hpp.in 834826 2009-11-11 10:03:53Z borisk $
  */
-
-#if !defined(XERCESC_INCLUDE_GUARD_XERCES_AUTOCONFIG_CONFIG_HPP)
-#define XERCESC_INCLUDE_GUARD_XERCES_AUTOCONFIG_CONFIG_HPP
 
 //
 // There are two primary xerces configuration header files:
@@ -31,76 +29,69 @@
 //	through public headers. This file has limited information
 //	and carefully works to avoid collision of macro names, etc.
 //
-//	This file is included by XercesDefs.h.
-//	This version of the file is specific for Microsoft Visual C++
-//	family of compilers
+//	This file is included by XercesDefs.h. In the event
+//	of a non-configured platform, a similar header specific
+//	to the platform will be included instead.
 //
-//	config.h
+//	config.h					
 //
-//      This file is not used with Microsoft Visual C++; the macros
-//	it would specify are instead hardcoded in the makefiles
+//      Generalized autoconf header file, with much more
+//	information, used to supply configuration information
+//	for use in implementation files.
 //
+// For autoconf based builds, this header is configured from by the configure
+// script from the .in template file of the same name.
 
-#include <basetsd.h>
 
-// silence the warning "while compiling class-template member function xxxx : identifier was truncated to '255'
-// characters in the browser information"
-#pragma warning( disable: 4786 )
+#ifndef XERCES_AUTOCONFIG_CONFIG_HPP
+#define XERCES_AUTOCONFIG_CONFIG_HPP
 
 // ---------------------------------------------------------------------------
-//  These defines have been hardcoded for the Microsoft Visual C++ compilers
+//  These defines are set by configure as appropriate for the platform.
 // ---------------------------------------------------------------------------
-#undef XERCES_AUTOCONF
-#undef XERCES_HAVE_SYS_TYPES_H
-#undef XERCES_HAVE_INTTYPES_H
+#define XERCES_AUTOCONF 1
+#define XERCES_HAVE_SYS_TYPES_H 1
+#define XERCES_HAVE_INTTYPES_H 1
+/* #undef XERCES_HAVE_INTRIN_H */
+#define XERCES_HAVE_EMMINTRIN_H 1
+/* #undef XERCES_INCLUDE_WCHAR_H */
 
-#define XERCES_S16BIT_INT   signed short
-#define XERCES_U16BIT_INT   unsigned short
-#define XERCES_S32BIT_INT   INT32
-#define XERCES_U32BIT_INT   UINT32
+#define XERCES_S16BIT_INT int16_t
+#define XERCES_S32BIT_INT int32_t
+#define XERCES_S64BIT_INT int64_t
+#define XERCES_U16BIT_INT uint16_t
+#define XERCES_U32BIT_INT uint32_t
+#define XERCES_U64BIT_INT uint64_t
+#define XERCES_XMLCH_T uint16_t
+#define XERCES_SIZE_T size_t
+#define XERCES_SSIZE_T ssize_t
 
-// While VC6 has 64-bit int, there is no support in the libraries
-// (e.g., iostream). So we are going to stick to  32-bit ints.
-//
-#if (_MSC_VER >= 1300)
-#  define XERCES_S64BIT_INT   INT64
-#  define XERCES_U64BIT_INT   UINT64
-#else
-#  define XERCES_S64BIT_INT   INT32
-#  define XERCES_U64BIT_INT   UINT32
+#define XERCES_HAS_CPP_NAMESPACE 1
+#define XERCES_STD_NAMESPACE 1
+#define XERCES_NEW_IOSTREAMS 1
+/* #undef XERCES_NO_NATIVE_BOOL */
+#define XERCES_LSTRSUPPORT 1
+
+/* #undef XERCES_HAVE_CPUID_INTRINSIC */
+#define XERCES_HAVE_SSE2_INTRINSIC 1
+/* #undef XERCES_HAVE_GETCPUID */
+
+#define XERCES_PLATFORM_EXPORT 
+#define XERCES_PLATFORM_IMPORT 
+
+/* #undef XERCES_NO_MATCHING_DELETE_OPERATOR */
+
+// ---------------------------------------------------------------------------
+//  Include standard headers, if available, that we may rely on below.
+// ---------------------------------------------------------------------------
+#if XERCES_HAVE_INTTYPES_H
+#	include <inttypes.h>
 #endif
-
-#ifdef _NATIVE_WCHAR_T_DEFINED
-#  define XERCES_XMLCH_T      wchar_t
-#else
-#  define XERCES_XMLCH_T      unsigned short
+#if XERCES_HAVE_SYS_TYPES_H
+#	include <sys/types.h>
 #endif
-
-#define XERCES_SIZE_T       SIZE_T
-#define XERCES_SSIZE_T      SSIZE_T
-
-#define XERCES_HAS_CPP_NAMESPACE    1
-#define XERCES_STD_NAMESPACE        1
-#define XERCES_NEW_IOSTREAMS        1
-#undef XERCES_NO_NATIVE_BOOL
-#define XERCES_LSTRSUPPORT          1
-
-#ifdef XERCES_STATIC_LIBRARY
-#  define XERCES_PLATFORM_EXPORT
-#  define XERCES_PLATFORM_IMPORT
-#else
-#  define XERCES_PLATFORM_EXPORT __declspec(dllexport)
-#  define XERCES_PLATFORM_IMPORT __declspec(dllimport)
-#  define DLL_EXPORT
-#endif
-
-#define XERCES_MFC_SUPPORT
-
-#if (_MSC_VER >= 1400)
-#  define XERCES_HAVE_INTRIN_H 1
-#  define XERCES_HAVE_EMMINTRIN_H 1
-#  define XERCES_HAVE_CPUID_INTRINSIC
-#  define XERCES_HAVE_SSE2_INTRINSIC
+#if XERCES_INCLUDE_WCHAR_H
+#	include <wchar.h>
 #endif
 
 // ---------------------------------------------------------------------------
@@ -142,7 +133,7 @@ typedef XMLUInt64			        XMLFileLoc;
 //  Force on the Xerces debug token if it is on in the build environment
 // ---------------------------------------------------------------------------
 #if defined(_DEBUG)
-#  define XERCES_DEBUG
+#define XERCES_DEBUG
 #endif
 
 #endif
