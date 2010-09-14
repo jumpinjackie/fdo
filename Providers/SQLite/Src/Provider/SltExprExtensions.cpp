448440c51710176d7f171ec25617bde84e136e48
romicadascalescu <romicadascalescu@example.org>
2010-09-14 14:46:03 +0000

    Ticket #663 SQLite: Avoid adding many brackets leading to parser error  - In case the filter processor adds too many brackets around filter chunks the sqlite parser fails with error. In case no brackets or less brackets are added the sqlite parser is OK.  - Fixed an additional issue: provider returns all NULL geometries when envelope intersects is used. Fixed by checking the geometry value and if is NULL return false
    git-svn-id: http://svn.osgeo.org/fdo/trunk@5661 d98eaee6-4212-0410-900c-dcc49bf69b99

--- Providers/SQLite/Src/Provider/SltExprExtensions.cpp
+++ Providers/SQLite/Src/Provider/SltExprExtensions.cpp
@@ -1543,13 +1543,6 @@ static void nullvalueFunc(sqlite3_context *context, int argc, sqlite3_value **ar
 
 static void spatialOpFunc(sqlite3_context *context, int argc, sqlite3_value **argv)
 {
-    // in case we have set 1 to aux arg the we have return since function was already evaluated
-    if((int)sqlite3_get_auxdata(context, 1) == 1)
-    {
-        sqlite3_result_int(context, 1);
-        return;
-    }
-
     assert(argc == 2);
 
     //args must be both blobs or text -- else fail.
@@ -1560,6 +1553,13 @@ static void spatialOpFunc(sqlite3_context *context, int argc, sqlite3_value **ar
         )
     {
         sqlite3_result_int(context, 0);
+        return;
+    }
+
+    // in case we have set 1 to aux arg the we have return since function was already evaluated
+    if((int)sqlite3_get_auxdata(context, 1) == 1)
+    {
+        sqlite3_result_int(context, 1);
         return;
     }
 
