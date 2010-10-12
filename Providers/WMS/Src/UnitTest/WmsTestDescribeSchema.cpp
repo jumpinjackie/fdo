6b90ffc2bc45572ad5130cad53c7500137afbf9b
gregboone <gregboone@example.org>
2010-10-12 16:49:28 +0000

    Ticket [https://trac.osgeo.org/fdo/ticket/695]: Implement FDO RFC 53 - Fix axis order issue in WMS provider (PORT)
    git-svn-id: http://svn.osgeo.org/fdo/branches/3.5@5749 d98eaee6-4212-0410-900c-dcc49bf69b99

--- Providers/WMS/Src/UnitTest/WmsTestDescribeSchema.cpp
+++ Providers/WMS/Src/UnitTest/WmsTestDescribeSchema.cpp
@@ -62,7 +62,7 @@ void WmsTestDescribeSchema::testServer2 ()
 {
     try 
     {
-	    TestServer(L"http://wms.jpl.nasa.gov/wms.cgi", 21);
+	    TestServer(L"http://wms.jpl.nasa.gov/wms.cgi", 16);
     }
     catch (FdoException* e)
     {
