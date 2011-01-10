6dbd54fe53b82a6f4bc513e48e948397f418294d
brentrobinson <brentrobinson@example.org>
2011-01-10 13:01:24 +0000

    Ticket[https://trac.osgeo.org/fdo/ticket/647]: Reworked case where database is not specified to try to connect to the user database first and then fall back to the postgres database if first connect fails. This works better in environments where each user has a database but the postgres database has a low connection limit.
    git-svn-id: http://svn.osgeo.org/fdo/trunk@5855 d98eaee6-4212-0410-900c-dcc49bf69b99

--- Providers/GenericRdbms/Src/PostGis/Driver/connect.c
+++ Providers/GenericRdbms/Src/PostGis/Driver/connect.c
@@ -19,6 +19,7 @@
 #include "stdafx.h"
 
 #include "connect.h"
+#include "disconnect.h"
 #include "set_err_msg.h"
 #include "pgconn_status.h"
 #include "Inc/ut.h"
@@ -142,99 +143,112 @@ int postgis_connect(postgis_context_def* context,
         assert(NULL != pghost);
         assert(NULL != pgport);
         
-        for (conn_index = 0;
-            ((conn_index < max_connects) && (RDBI_SUCCESS == ret) && context->postgis_current_connect2 == -1 );
-            conn_index++)
+        char* lDbName = dbname;
+
+        int attempt = 0;
+
+        // When no database specified, make two attempts to connect:
+        //    - 1st attempt tries to connect to database with same name as user
+        //    - If 1st attempt fails to establish 2 connections, 2nd attempt
+        //      falls back to connecting to the postgres database.
+
+        for ( attempt = 0; attempt < 2; attempt++ )
         {
-            if (NULL == context->postgis_connections[conn_index])
+            // Establish two connections:
+            //   postgis_current_connect - for most operations
+            //   postgis_current_connect2 - for schema modifications (e.g. create table).
+
+            for (conn_index = 0;
+                ((conn_index < max_connects) && (RDBI_SUCCESS == ret) && context->postgis_current_connect2 == -1 );
+                conn_index++)
             {
-                /*
-                 * Establish connection with PostgreSQL database.
-                 * Default the database to the postgres database.
-                 */
-                conn = PQsetdbLogin(pghost, pgport, pgoptions, pgtty, (dbname && (dbname[0] != '\0')) ? dbname : POSTGIS_DEFAULT_DBNAME, pglogin, pgpwd);
-                
-                ret = postgis_pgconn_status(conn);
-                if (RDBI_SUCCESS != ret)
+                if (NULL == context->postgis_connections[conn_index])
                 {
-                    /* Retrieve connection error and
-                     * set as last error message for context.
+                    /*
+                     * Establish connection.
+                     * Default the database to NULL (same name as the user).
                      */
-                    pch = PQerrorMessage(conn);
-                    postgis_set_err_msg(context, pch);
-
-                    /* If we have an error message, set return to GENERIC_ERROR
-                     * so that the error message will propogate up.
-                     */
-                    if ( strlen(pch) > 0 ) 
-                        ret = RDBI_GENERIC_ERROR;
-                    PQfinish(conn);
-                    conn = NULL;
-
-                    if ( (!dbname) || (dbname[0] == '\0') ) 
-                    {
-                        /* Connecting to postgres database failed. Try again, this time leaving
-                         * dbname NULL, which will connect to a database with same name as the user.
-                         */
-                        conn = PQsetdbLogin(pghost, pgport, pgoptions, pgtty, (const char*) NULL, pglogin, pgpwd);
-                        int ret2 = postgis_pgconn_status(conn);
-
-                        if (RDBI_SUCCESS == ret2)
-                        {
-                            /* 2nd connect successful, get rid of error message */
-                            postgis_set_err_msg(context, "");
-                            ret = RDBI_SUCCESS;
-                        }
-                        else
-                        {
-                            /* 2nd connect failed, give up */
-                            PQfinish(conn);
-                            conn = NULL;
-                        }
-                    }
-
-                }
-
-                if (RDBI_SUCCESS == ret)
-                {
+                    conn = PQsetdbLogin(pghost, pgport, pgoptions, pgtty, (lDbName && (lDbName[0] != '\0')) ? lDbName : (const char*) NULL, pglogin, pgpwd);
                     
-                    if (-1 == context->postgis_current_connect)
+                    ret = postgis_pgconn_status(conn);
+                    if (RDBI_SUCCESS != ret)
                     {
-                        context->postgis_connect_count++;
-                        context->postgis_current_connect = conn_index;
-                        context->postgis_connections[conn_index] = conn;
-                        context->postgis_in_transaction[conn_index] = -1;
-                        /* Index is used as a connection ID. */
-                        *connect_id = conn_index;
-                        
-                        ret = RDBI_SUCCESS;
-                    }
-                    else if (-1 == context->postgis_current_connect2)
-                    {
-                        context->postgis_connect_count++;
-                        context->postgis_current_connect2 = conn_index;
-                        context->postgis_connections[conn_index] = conn;
-                        context->postgis_in_transaction[conn_index] = -1;
-                        ret = RDBI_SUCCESS;
-                    }
-
-                    if( PQsetClientEncoding( conn, "UTF8") != RDBI_SUCCESS )
-                    {
-                        // If we cannot set the client character set, then alot of operation may go wrong
+                        /* Retrieve connection error and
+                         * set as last error message for context.
+                         */
                         pch = PQerrorMessage(conn);
                         postgis_set_err_msg(context, pch);
-                        
+
+                        /* If we have an error message, set return to GENERIC_ERROR
+                         * so that the error message will propagate up.
+                         */
+                        if ( strlen(pch) > 0 ) 
+                            ret = RDBI_GENERIC_ERROR;
                         PQfinish(conn);
                         conn = NULL;
                     }
 
-                    if( conn != NULL )
+                    if (RDBI_SUCCESS == ret)
                     {
-                        PQsetNoticeProcessor( conn, localNoticeProcessor, (void*)context);
+                        
+                        if (-1 == context->postgis_current_connect)
+                        {
+                            context->postgis_connect_count++;
+                            context->postgis_current_connect = conn_index;
+                            context->postgis_connections[conn_index] = conn;
+                            context->postgis_in_transaction[conn_index] = -1;
+                            /* Index is used as a connection ID. */
+                            *connect_id = conn_index;
+                            
+                            ret = RDBI_SUCCESS;
+                        }
+                        else if (-1 == context->postgis_current_connect2)
+                        {
+                            context->postgis_connect_count++;
+                            context->postgis_current_connect2 = conn_index;
+                            context->postgis_connections[conn_index] = conn;
+                            context->postgis_in_transaction[conn_index] = -1;
+                            ret = RDBI_SUCCESS;
+                        }
+
+                        if( PQsetClientEncoding( conn, "UTF8") != RDBI_SUCCESS )
+                        {
+                            // If we cannot set the client character set, then alot of operation may go wrong
+                            pch = PQerrorMessage(conn);
+                            postgis_set_err_msg(context, pch);
+                            
+                            PQfinish(conn);
+                            conn = NULL;
+                        }
+
+                        if( conn != NULL )
+                        {
+                            PQsetNoticeProcessor( conn, localNoticeProcessor, (void*)context);
+                        }
                     }
                 }
-            }
-        } // for
+            } // for
+
+            // Done if one of the following is true:
+            //    - connected successfully
+            //    - make 2 attempts.
+            //    - tried to connect to a particular database
+            //    - user is postgres (no need to retry connecting to postgres database)
+            if ( (RDBI_SUCCESS == ret) || ( attempt > 0) || (lDbName && (lDbName[0] != '\0')) || (strcmp(pglogin,POSTGIS_DEFAULT_DBNAME) == 0) )
+                break;
+
+            // 1st attempt failed to connect to user database. This can happen if user database
+            // does not exist or connection limit for database was exceeded.
+            // Set up 2nd attempt:
+
+            // Close any connections that were successful. This can happen if there only the 1st
+            // connection was successful.
+            postgis_disconnect(context);
+
+            // try connecting to postgres database.
+            lDbName = POSTGIS_DEFAULT_DBNAME;
+            ret = RDBI_SUCCESS;
+        } //for
     }
 
     return ret;
