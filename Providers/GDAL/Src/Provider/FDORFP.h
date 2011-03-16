2698d78150907d2900a3e30ff7abe46e66f80055
gregboone <gregboone@example.org>
2011-03-16 21:06:12 +0000

    Ticket [https://trac.osgeo.org/fdo/ticket/754]: Clean Up Build
    git-svn-id: http://svn.osgeo.org/fdo/trunk@5922 d98eaee6-4212-0410-900c-dcc49bf69b99

--- Providers/GDAL/Src/Provider/FDORFP.h
+++ Providers/GDAL/Src/Provider/FDORFP.h
@@ -27,7 +27,10 @@
 
 #ifdef _WIN32
 #pragma once
+#define WIN32_LEAN_AND_MEAN		// Exclude rarely-used stuff from Windows headers
+#include <windows.h>
 #endif //_WIN32
+
 
 #include <GdalFile/FdoGrfpProvider.h>
 #include <FdoCommon.h>
