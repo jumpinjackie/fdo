a7abcd9f0bcb83a67db487fd216d3c46b1014f7f
gregboone <gregboone@example.org>
2011-03-17 18:13:02 +0000

    Ticket [https://trac.osgeo.org/fdo/ticket/754]: Clean Up Build
    git-svn-id: http://svn.osgeo.org/fdo/trunk@5935 d98eaee6-4212-0410-900c-dcc49bf69b99

--- Utilities/SchemaMgr/Src/Sm/Lp/stdafx.h
+++ Utilities/SchemaMgr/Src/Sm/Lp/stdafx.h
@@ -23,7 +23,8 @@
 
 #ifdef _WIN32
 #pragma once
-#define WIN32_LEAN_AND_MEAN		// Exclude rarely-used stuff from Windows headers
+#define WIN32_LEAN_AND_MEAN	    // Exclude rarely-used stuff from Windows headers
+#include <windows.h>
 #endif
 
 #include <cstring>
