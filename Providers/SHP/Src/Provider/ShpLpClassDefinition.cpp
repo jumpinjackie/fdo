c2ab8a8f7f688b5845945672e94f25ec1abd7ca3
danstoica <danstoica@example.org>
2012-01-16 19:23:41 +0000

    Ticket #807 SHP Provider: Multi-polygon geometry type is not exposed via GetGeometryComponentTypes()
    See http://trac.osgeo.org/fdo/ticket/807 for details.
    
    git-svn-id: http://svn.osgeo.org/fdo/trunk@6349 d98eaee6-4212-0410-900c-dcc49bf69b99

--- Providers/SHP/Src/Provider/ShpLpClassDefinition.cpp
+++ Providers/SHP/Src/Provider/ShpLpClassDefinition.cpp
@@ -752,19 +752,22 @@ FdoGeometricPropertyDefinition* ShpLpClassDefinition::ConvertPhysicalToLogicalGe
                 break;
             case ePolygonShape:
                 geomTypesDest[0] = FdoGeometryType_Polygon;
-                geomTypeCount=1;
+                geomTypesDest[1] = FdoGeometryType_MultiPolygon;
+                geomTypeCount=2;
                 has_z = false;
                 has_m = false;
                 break;
             case ePolygonZShape:
                 geomTypesDest[0] = FdoGeometryType_Polygon;
-                geomTypeCount=1;
+                geomTypesDest[1] = FdoGeometryType_MultiPolygon;
+                geomTypeCount=2;
                 has_z = true;
                 has_m = shp->HasMData ();
                 break;
             case ePolygonMShape:
                 geomTypesDest[0] = FdoGeometryType_Polygon;
-                geomTypeCount=1;
+                geomTypesDest[1] = FdoGeometryType_MultiPolygon;
+                geomTypeCount=2;
                 has_z = false;
                 has_m = true;
                 break;
