84e41cd9e9ccebf55072e55af3cb0fb0c16d409e
romicadascalescu <romicadascalescu@example.org>
2012-04-05 16:26:29 +0000

    Ticket [https://trac.osgeo.org/fdo/ticket/825]: Sqlite provider : String Primary key on a view + a small additional fix for PK read-only On behalf of bscott
    git-svn-id: http://svn.osgeo.org/fdo/branches/3.6@6434 d98eaee6-4212-0410-900c-dcc49bf69b99

--- Providers/SQLite/Src/Provider/SltMetadata.cpp
+++ Providers/SQLite/Src/Provider/SltMetadata.cpp
@@ -367,8 +367,9 @@ FdoClassDefinition* SltMetadata::ToClass()
                         dpd->SetIsSystem((detail & 0x01) != 0);
                         if ((detail & 0x02) != 0)
                         {
+                            propMarkedAsReadOnly = true;
                             // if we have a view we might have an enforced PK 
-                            if (pTable->pSelect != NULL && (dt == FdoDataType_Int16 || dt == FdoDataType_Int32 || dt == FdoDataType_Int64))
+                            if (pTable->pSelect != NULL && (dt == FdoDataType_Int16 || dt == FdoDataType_Int32 || dt == FdoDataType_Int64 || dt == FdoDataType_String))
                             {
                                 // in case there are more properties marked as read only avoid set a PK
                                 if (!enforcedPK.size())
