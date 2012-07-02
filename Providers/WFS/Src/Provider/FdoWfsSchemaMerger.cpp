5ce2b27d6ebbc8b0fdf35950ac86c61455d1974c
stevenxu <stevenxu@example.org>
2012-07-02 01:51:37 +0000

    This submission is integrated from changeset 6437 to fix the ticket [https://trac.osgeo.org/fdo/ticket/826] in branch 3.7 code side stream.
    Ticket[https://trac.osgeo.org/fdo/ticket/826]: WFS from DigitalGlobel fails
    
    This problem occurred from FDO 3.6 and there are two problems result in the 
    failure of DigitalGlobel.
    
    1. New element name was involved in Fdo.dll: 
    
    Http request "https://services.digitalglobe.com/catalogservice/wfsaccess?
    CONNECTID=7ae07ed7%2Dda2d
    %2D4d65%2Dac04%2D8220b20c1b93&version=1.1.0&service=WFS&REQUEST=GetFeature&TYPEN
    AME=DigitalGlobe%3AImageInMosaicFeature"
    It is used in FDO 3.6 and 3.7.
    It returns one new XML element named as "featureMembers".
    
    Http request "https://services.digitalglobe.com/catalogservice/wfsaccess?
    request=GetFeature&CONNECTID=7ae07ed7%2Dda2d
    %2D4d65%2Dac04%2D8220b20c1b93&version=1.0.0&service=WFS&REQUEST=GetFeature&TYPEN
    AME=DigitalGlobe%3AImageInMosaicFeature"
    It is used in FDO 3.5.
    It returns the XML element "featureMember".
    
    Both of these Http requests will return one XML file which contains all feature 
    information.
    The parser to this XML is hard-coded in the file \\fdo_depot\Fdo\Unmanaged\Src
    \Fdo\Xml\FeaturePropertyReaderImpl.cpp and it only accepts the name 
    "featureMember".
    This fix modified the code to handle both "featureMembers" and "featureMember" .
    
    2. One parser error in WFSProvider.dll:
    
    In FDO 3.6 and 3.7 the method "FdoWfsSchemaMerger::getXSDName" got the wrong 
    result form URL 
    "http://services.digitalglobe.com:80/wfsservice/schemas/gml/3.1.1/base/gml.xsd?
    CONNECTID=7ae07ed7-da2d-4d65-ac04-8220b20c1b93"
    The right result is "gml.xsd" instead of "gml.xsd?CONNECTID=7ae07ed7-da2d-4d65-
    ac04-8220b20c1b93".
    
    This fix improved this parser to get the right XSD name.
    This problem hasn't occurred in FDO 3.5 for this code path hasn't been exercised 
    in it.
    
    git-svn-id: http://svn.osgeo.org/fdo/branches/3.7@6494 d98eaee6-4212-0410-900c-dcc49bf69b99

--- Providers/WFS/Src/Provider/FdoWfsSchemaMerger.cpp
+++ Providers/WFS/Src/Provider/FdoWfsSchemaMerger.cpp
@@ -263,16 +263,19 @@ FdoStringP FdoWfsSchemaMerger::_getXSDName(FdoStringP location)
 
 	size_t len = location.GetLength();
 	size_t i;
+    size_t pos = len -1; 
 	for (i = len - 1;i>=0;i--)
 	{
-         if (*((FdoString*)location + i) == FdoWfsGlobals::SLASH)
+        if (*((FdoString*)location + i ) == FdoWfsGlobals::QUESTION)
+            pos = i - 1;
+        else if (*((FdoString*)location + i) == FdoWfsGlobals::SLASH) 
               break;
 	}
 
 	if ( i == -1)
 		rv = L"";
 	else
-		rv = location.Mid(i + 1,len - i);
+		rv = location.Mid(i + 1, pos - i); 
 	return rv;
 
 }
