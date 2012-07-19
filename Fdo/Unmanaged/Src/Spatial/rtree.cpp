5c96226714047f6615fc1161c4c5d674cf38c65f
danstoica <danstoica@example.org>
2012-07-19 16:44:11 +0000

    Implement FDO RFC 63 "In-memory Spatial Index" (3) Fixed Linux build. Mainly disabled USE_SSE for Linux.
    git-svn-id: http://svn.osgeo.org/fdo/trunk@6512 d98eaee6-4212-0410-900c-dcc49bf69b99

--- Fdo/Unmanaged/Src/Spatial/rtree.cpp
+++ Fdo/Unmanaged/Src/Spatial/rtree.cpp
@@ -28,6 +28,14 @@
 #include <vector>
 #include <wchar.h>
 
+#ifndef max
+#define max(a,b)            (((a) > (b)) ? (a) : (b))
+#endif
+
+#ifndef min
+#define min(a,b)            (((a) < (b)) ? (a) : (b))
+#endif
+
 namespace fdo {
 
 //\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\
