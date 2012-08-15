218c4463adf46a2c3ca50c840b932ba61e3e3a89
stevenxu <stevenxu@example.org>
2012-08-15 07:10:51 +0000

    This submission is integrated from changeset 6437 to fix the ticket #826 in sanbox branch 3.7 code stream.
    Ticket#826: WFS from DigitalGlobel? fails
    
    This problem occurred from FDO 3.6 and there are two problems result in the 
    failure of DigitalGlobel?.
    
    1. New element name was involved in Fdo.dll: 
    
    Http request " https://services.digitalglobe.com/catalogservice/wfsaccess?
    CONNECTID=7ae07ed7%2Dda2d
    %2D4d65%2Dac04%2D8220b20c1b93&version=1.1.0&service=WFS&REQUEST=GetFeature?&TYPEN
    AME=DigitalGlobe?%3AImageInMosaicFeature"
    It is used in FDO 3.6 and 3.7.
    It returns one new XML element named as "featureMembers".
    
    Http request " https://services.digitalglobe.com/catalogservice/wfsaccess?
    request=GetFeature?&CONNECTID=7ae07ed7%2Dda2d
    %2D4d65%2Dac04%2D8220b20c1b93&version=1.0.0&service=WFS&REQUEST=GetFeature?&TYPEN
    AME=DigitalGlobe?%3AImageInMosaicFeature"
    It is used in FDO 3.5.
    It returns the XML element "featureMember".
    
    Both of these Http requests will return one XML file which contains all feature 
    information.
    The parser to this XML is hard-coded in the file \\fdo_depot\Fdo\Unmanaged\Src
    \Fdo\Xml\FeaturePropertyReaderImpl?.cpp and it only accepts the name 
    "featureMember".
    This fix modified the code to handle both "featureMembers" and "featureMember" .
    
    2. One parser error in WFSProvider.dll:
    
    In FDO 3.6 and 3.7 the method "FdoWfsSchemaMerger::getXSDName" got the wrong 
    result form URL 
    " http://services.digitalglobe.com:80/wfsservice/schemas/gml/3.1.1/base/gml.xsd?
    CONNECTID=7ae07ed7-da2d-4d65-ac04-8220b20c1b93"
    The right result is "gml.xsd" instead of "gml.xsd?CONNECTID=7ae07ed7-da2d-4d65-
    ac04-8220b20c1b93".
    
    This fix improved this parser to get the right XSD name.
    This problem hasn't occurred in FDO 3.5 for this code path hasn't been exercised 
    in it.
    
    
    git-svn-id: http://svn.osgeo.org/fdo/sandbox/adsk/3.7@6546 d98eaee6-4212-0410-900c-dcc49bf69b99

--- Fdo/Unmanaged/Src/Fdo/Xml/FeaturePropertyReaderImpl.cpp
+++ Fdo/Unmanaged/Src/Fdo/Xml/FeaturePropertyReaderImpl.cpp
@@ -630,9 +630,9 @@ FdoXmlFeaturePropertyReaderImpl::GmlBaseType FdoXmlFeaturePropertyReaderImpl::ge
         case ParsingState_FeatureCollection:
             {
                 // similar as above
-                if ((wcscmp(elementUri, FdoXml::mGmlUri) == 0 &&
-                    wcscmp(elementName, L"featureMember") == 0) ||
-                    isTypeOf(elementName, elementUri, FdoGml212::mFeatureAssociation, false))
+                if ((wcscmp(elementUri, FdoXml::mGmlUri) == 0 
+                    && (wcscmp(elementName, L"featureMember") == 0 || wcscmp(elementName, L"featureMembers") == 0))  
+                    || isTypeOf(elementName, elementUri, FdoGml212::mFeatureAssociation, false)) 
                     rv = GmlBaseType_FeatureAssociation;
                 else if (wcscmp(elementUri, FdoXml::mGmlUri) == 0 &&
                     wcscmp(elementName, L"boundedBy") == 0)
