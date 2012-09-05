90a4da0ad1221fe6c659258af7c2a8313b64ab6c
Jackie Ng <jumpinjackie@gmail.com>
2012-09-05 16:34:36 +0000

    #847: This is the initial submission for the recipe files needed to create NuGet packages for FDO. The package structure mirrors the tarball structure. That is: There is a package for FDO core and the individual providers with a SDK meta-package. These packages come in 32 and 64-bit flavours. This currently uses the beta 1 file listing (as that's the only 3.7 release with an ArcSDE provider)
    git-svn-id: http://svn.osgeo.org/fdo/branches/3.7@6566 d98eaee6-4212-0410-900c-dcc49bf69b99

---
+++ nuget/x86/fdo-core-mgd/tools/net40/GetFdoPostBuildCmd.ps1
@@ -0,0 +1,5 @@
+﻿$solutionDir = [System.IO.Path]::GetDirectoryName($dte.Solution.FullName) + "\"
+$path = $installPath.Replace($solutionDir, "`$(SolutionDir)")
+
+$FdoPostBuildCmd = "
+copy /y `"`$(ProjectDir)fdo-libs\*`" `"`$(TargetDir)`""
