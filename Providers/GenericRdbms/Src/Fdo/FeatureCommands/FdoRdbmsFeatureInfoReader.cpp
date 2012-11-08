0c5ea1922bd0523f2ad7f4bb42761cc5b5a7f654
Jackie Ng <jumpinjackie@gmail.com>
2012-11-08 15:44:52 +0000

    [https://trac.osgeo.org/fdo/ticket/852]: Backport to 3.7. This fix is too important to leave just in trunk.
    git-svn-id: http://svn.osgeo.org/fdo/branches/3.7@6628 d98eaee6-4212-0410-900c-dcc49bf69b99

--- Providers/GenericRdbms/Src/Fdo/FeatureCommands/FdoRdbmsFeatureInfoReader.cpp
+++ Providers/GenericRdbms/Src/Fdo/FeatureCommands/FdoRdbmsFeatureInfoReader.cpp
@@ -52,7 +52,7 @@ FdoRdbmsFeatureInfoReader* FdoRdbmsFeatureInfoReader::Create(FdoPropertyValueCol
 
 FdoClassDefinition *FdoRdbmsFeatureInfoReader::GetClassDefinition()
 {
-    if( ! mCalled || mFeatInfoCollection == NULL )
+    if(mFeatInfoCollection == NULL )
         throw FdoCommandException::Create(NlsMsgGet(FDORDBMS_57, strEndOfRecordExp));
 
     FdoPtr<FdoDataPropertyDefinition> property;
@@ -72,6 +72,8 @@ FdoClassDefinition *FdoRdbmsFeatureInfoReader::GetClassDefinition()
 
     classDefinition->SetIsAbstract(false);
     properties = classDefinition->GetProperties();
+    FdoPtr<FdoDataPropertyDefinitionCollection> idProps = classDefinition->GetIdentityProperties();
+    const FdoSmLpDataPropertyDefinitionCollection * idPropDefs = mClassDefinition->RefIdentityProperties();
 
     for (int i=0; i<mFeatInfoCollection->GetCount(); i++)
     {
@@ -84,6 +86,11 @@ FdoClassDefinition *FdoRdbmsFeatureInfoReader::GetClassDefinition()
         property->SetDataType(dataValue->GetDataType());
         dataValue->Release();
         properties->Add(property);
+
+        if (idPropDefs->IndexOf(id->GetName()) >= 0) 
+        {
+            idProps->Add(property);
+        }
     }
 
     return classDefinition;
