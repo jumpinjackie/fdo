49a1e68dc2f9ae595b6812b0b25125d87947a962
brentrobinson <brentrobinson@example.org>
2012-11-13 20:43:39 +0000

    Ticket[https://trac.osgeo.org/fdo/ticket/853]: ODBC: Add owner to SQLPrimaryKeysW call so that primary keys, for tables not in dbo, can be retrieved.
    Also fixed a memory corruption in PhysicalElementMappingCollection.h
     
    
    git-svn-id: http://svn.osgeo.org/fdo/trunk@6632 d98eaee6-4212-0410-900c-dcc49bf69b99

--- Fdo/Unmanaged/Inc/Fdo/Commands/Schema/PhysicalElementMappingCollection.h
+++ Fdo/Unmanaged/Inc/Fdo/Commands/Schema/PhysicalElementMappingCollection.h
@@ -218,7 +218,7 @@ public:
         if (m_parent)
         {
             // Cast drops const
-            FdoPtr<OBJ>    pitem = (OBJ*)value;
+            OBJ*            pitem = (OBJ*)value;
             FdoPhysicalElementMappingP   pparent = pitem->GetParent();
 
             if ( pparent == m_parent )
