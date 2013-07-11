330086bf19bb8dc962bd89fa7ef4b5366fe954e6
gregboone <gregboone@example.org>
2013-07-11 23:49:35 +0000

    Ticket [https://trac.osgeo.org/fdo/ticket/761]: Full 64-bit Linux Support
    git-svn-id: http://svn.osgeo.org/fdo/trunk@6823 d98eaee6-4212-0410-900c-dcc49bf69b99

--- Thirdparty/apache/build2.sh
+++ Thirdparty/apache/build2.sh
@@ -17,25 +17,133 @@
 ## Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 ##
 
+TYPEACTION=buildinstall
+TYPEARCHITECTURE=32
+
+### study parameters ###
+while test $# -gt 0
+do
+  arg="$1"
+  shift
+  case $arg in
+  -h |--h | --help)
+    SHOWHELP=yes
+    break
+    ;;
+  -b | --b | --build)
+    if test "$1" == ""; then
+        echo "$arg Invalid parameter $1"
+        exit 1
+    else
+        TYPEARCHITECTURE="$1"
+    fi
+    shift
+    ;;
+  -a | --a | --action)
+    if test "$1" == buildinstall; then
+        TYPEACTION=buildinstall
+    elif test "$1" == build; then
+        TYPEACTION=build
+    elif test "$1" == install; then
+        TYPEACTION=install
+    elif test "$1" == uninstall; then
+        TYPEACTION=uninstall
+    elif test "$1" == clean; then
+        TYPEACTION=clean
+    else
+        echo "$arg Invalid parameter $1"
+	exit 1
+    fi
+    shift
+    ;;
+  -*)
+    echo "The command option is not recognized: $arg"
+    echo "Please use the format:"
+    SHOWHELP=yes
+    break
+    ;;
+
+  *)
+    echo "The command is not recognized: $arg"
+    echo "Please use the format:"
+    SHOWHELP=yes
+    break
+    ;;
+  esac
+done
+### end of study parameters ###
+
+if test "$SHOWHELP" == yes; then
+
+   echo "*******************************************************************"
+   echo "build2.sh   [--h]"
+   echo "            [--a Action]"
+   echo "            [--b BuildArchicture]"
+   echo " "
+   echo "Help:                  --h[elp]"
+   echo "Action:                --a[ction] buildinstall(default), build, install, uninstall, clean"
+   echo "ConfigArchitecture:    --b[build] 32(default), 64"
+   echo "*******************************************************************"
+
+   exit 0
+fi
 
 export XERCESCROOT=$PWD/xerces
 export XALANCROOT=$PWD/xalan
 export NLSDIR=$XALANCROOT/src/xalanc/NLS
 
+if test "$TYPEARCHITECTURE" == "32" ; then
+if test "$HOSTTYPE" == "i686" ; then
+if [[ "$CPPFLAGS" != *"-march=i686"* ]]; then
+CPPFLAGS="$CPPFLAGS -march=i686"
+echo "Exporting CPPFLAGS: "$CPPFLAGS""
+export CPPFLAGS
+fi
+fi
+fi
+
+pushd . >& /dev/null
+
 cd xerces
+
 chmod a+x ./configure
 chmod a+x ./runConfigure
 chmod a+x config/pretty-make
-./runConfigure -p linux -P "$XERCESCROOT"
-make
-sudo -E make install
-cd ..
+
+./runConfigure -p linux -P "$XERCESCROOT" -b "$TYPEARCHITECTURE"
+
+if test "$TYPEACTION" == clean ; then
+   sudo make clean
+fi
+
+if test "$TYPEACTION" == buildinstall || test "$TYPEACTION" == build ; then
+   sudo make
+fi
+
+if test "$TYPEACTION" == buildinstall || test "$TYPEACTION" == install ; then
+   sudo make install
+fi
+
+popd >& /dev/null
+
+pushd . >& /dev/null
 
 cd xalan
+
 chmod a+x ./configure
 chmod a+x ./runConfigure
-./runConfigure -p linux -P "$XALANCROOT" 
-make
-## sudo -E make install 
-cd ..
+
+./runConfigure -p linux -P "$XALANCROOT" -b "$TYPEARCHITECTURE"
+
+if test "$TYPEACTION" == clean ; then
+   sudo make clean
+fi
+
+if test "$TYPEACTION" == buildinstall || test "$TYPEACTION" == build ; then
+   sudo make
+fi
+
+popd >& /dev/null
+
+
 
