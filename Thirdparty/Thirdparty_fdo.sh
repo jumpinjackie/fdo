f67cd95f86ba08b47b7aafa60b3554dc0a6c6f39
gregboone <gregboone@example.org>
2013-07-11 23:54:12 +0000

    Ticket #761: Full 64-bit Linux Support
    git-svn-id: http://svn.osgeo.org/fdo/trunk@6828 d98eaee6-4212-0410-900c-dcc49bf69b99

--- Thirdparty/Thirdparty_fdo.sh
+++ Thirdparty/Thirdparty_fdo.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 ##
-## Copyright (C) 2004-2006  Autodesk, Inc.
+## Copyright (C) 2013  Autodesk, Inc.
 ## 
 ## This library is free software; you can redistribute it and/or
 ## modify it under the terms of version 2.1 of the GNU Lesser
@@ -22,17 +22,106 @@
 ## apache, linux/cppunit                ##
 ##########################################
 
+
+TYPEACTION=buildinstall
+TYPEARCHITECTURE=32
+
+### study parameters ###
+while test $# -gt 0
+do
+  arg="$1"
+  shift
+  case $arg in
+  -h |--h | --help)
+    SHOWHELP=yes
+    break
+    ;;
+  -b | --b | --build)
+    if test "$1" == ""; then
+        echo "$arg Invalid parameter $1"
+        exit 1
+    else
+        TYPEARCHITECTURE="$1"
+    fi
+    shift
+    ;;
+  -a | --a | --action)
+    if test "$1" == buildinstall; then
+        TYPEACTION=buildinstall
+    elif test "$1" == build; then
+        TYPEACTION=build
+    elif test "$1" == install; then
+        TYPEACTION=install
+    elif test "$1" == uninstall; then
+        TYPEACTION=uninstall
+    elif test "$1" == clean; then
+        TYPEACTION=clean
+    else
+        echo "$arg Invalid parameter $1"
+	exit 1
+    fi
+    shift
+    ;;
+  -*)
+    echo "The command option is not recognized: $arg"
+    echo "Please use the format:"
+    SHOWHELP=yes
+    break
+    ;;
+
+  *)
+    echo "The command is not recognized: $arg"
+    echo "Please use the format:"
+    SHOWHELP=yes
+    break
+    ;;
+  esac
+done
+### end of study parameters ###
+
+
+if test "$SHOWHELP" == yes; then
+
+   echo "*******************************************************************"
+   echo "Thirdparty_fdo.sh   [--h]"
+   echo "                    [--a Action]"
+   echo "                    [--b BuildArchicture]"
+   echo " "
+   echo "Help:                  --h[elp]"
+   echo "Action:                --a[ction] buildinstall(default), build, install, uninstall, clean"
+   echo "ConfigArchitecture:    --b[uild] 32(default), 64"
+   echo "*******************************************************************"
+
+   exit 0
+fi
+
+CMDEX="-b $TYPEARCHITECTURE -a $TYPEACTION"
+
+## Apache Xalan/Xerces
 pushd . >& /dev/null
 cd apache
 echo Building apche
 chmod a+x ./build2.sh
-sudo -E sh ./build2.sh
+sudo sh ./build2.sh $CMDEX
 popd >& /dev/null
 
+
+## CPPUnit
 pushd . >& /dev/null
 cd linux/cppunit
 echo Building linux/cppunit
-chmod a+x ./build
-sudo -E sh ./build
+chmod a+x ./build.sh
+sudo sh ./build.sh $CMDEX
 popd >& /dev/null
+
+
+## GDAL
+pushd . >& /dev/null
+cd gdal
+echo Building gdal
+chmod a+x ./build.sh
+sudo sh ./build.sh $CMDEX
+popd >& /dev/null
+
+
 
