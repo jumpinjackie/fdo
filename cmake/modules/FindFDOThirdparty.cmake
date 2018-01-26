82c79263f6600588e23ef43861e0954db3148de4
Jackie Ng <jumpinjackie@gmail.com>
2018-01-26 16:38:30 +0000

    [https://trac.osgeo.org/fdo/ticket/940]: CMake build changes:  - Disable rtmp for internal libcurl build (this is for media content streaming, which is not needed by us)  - Add wrapper scripts to faciliate the CMake build (based on equivalent scripts in the recent MapGuide CMake build support):    - cmake_bootstrap.sh: This script builds thirdparty components (what gets built is controlled by flags in the script) to a separate source tree and writes these flags to an env_vars.sh script    - cmake_build.sh: This script configures a CMake build in a directory of your choice and then sets up the CMake build based on the variables in env_vars.sh generated by cmake_bootstrap.sh and then starts:       - ninja if you passed the --ninja flag       - make if you did not  - CMake module updates:    - Use find_library() for internal boost libraries. The key is using NO_DEFAULT_PATH so that find_library() does not try to look in the usual system paths.    - Look for system-installed cppunit static library as that is what is built for the internal cppunit      - Fix GenericRdbms unit test projects to include cppunit in the static library link set
    git-svn-id: http://svn.osgeo.org/fdo/trunk@7627 d98eaee6-4212-0410-900c-dcc49bf69b99

--- cmake/modules/FindFDOThirdparty.cmake
+++ cmake/modules/FindFDOThirdparty.cmake
@@ -2,99 +2,107 @@
 # libraries
 
 if( ALL_THIRDPARTY )
-	set( INTERNAL_CPPUNIT true )
-	set( INTERNAL_GDAL true )
-	set( INTERNAL_OPENSSL true )
-	set( INTERNAL_MYSQL true )
-	set( INTERNAL_POSTGRESQL true )
-	set( INTERNAL_BOOST true )
-	set( INTERNAL_CURL true )
-	set( INTERNAL_XERCESC true )
-	set( INTERNAL_XALANC true )
+    set( INTERNAL_CPPUNIT true )
+    set( INTERNAL_GDAL true )
+    set( INTERNAL_OPENSSL true )
+    set( INTERNAL_MYSQL true )
+    set( INTERNAL_POSTGRESQL true )
+    set( INTERNAL_BOOST true )
+    set( INTERNAL_CURL true )
+    set( INTERNAL_XERCESC true )
+    set( INTERNAL_XALANC true )
 endif( ALL_THIRDPARTY )
 
 # Check GDAL or add ThirdParty
 if( NOT INTERNAL_GDAL )
-	find_package(GDAL REQUIRED)
+    find_package(GDAL REQUIRED)
 else( NOT INTERNAL_GDAL )
-	set( LOCAL_GDAL TRUE )
-	set( GDAL_LIBRARIES  ${FDOTHIRDPARTY}/gdal/lib )
-	set( GDAL_INCLUDE_DIR  ${FDOTHIRDPARTY}/gdal 
-		${FDOTHIRDPARTY}/gdal/include
-		${FDOTHIRDPARTY}/gdal/ogr/ogrsf_frmts
-	)
-	message (STATUS "GDAL Include Dir: ${GDAL_INCLUDE_DIR}")
-	find_library( GDAL_LIBRARY NAMES gdal
-		PATHS ${GDAL_LIBRARIES}
-	)
+    set( LOCAL_GDAL TRUE )
+    set( GDAL_LIBRARIES  ${FDOTHIRDPARTY}/gdal/lib )
+    set( GDAL_INCLUDE_DIR  ${FDOTHIRDPARTY}/gdal 
+        ${FDOTHIRDPARTY}/gdal/include
+        ${FDOTHIRDPARTY}/gdal/ogr/ogrsf_frmts
+    )
+    message (STATUS "GDAL Include Dir: ${GDAL_INCLUDE_DIR}")
+    find_library( GDAL_LIBRARY NAMES gdal
+        PATHS ${GDAL_LIBRARIES}
+        NO_DEFAULT_PATH
+    )
+    message (STATUS "Using internal gdal: ${GDAL_LIBRARY}")
 endif( NOT INTERNAL_GDAL )
 
 # Check MySQL or add ThirdParty
 if( NOT INTERNAL_MYSQL )
-	find_package(MySQL REQUIRED)
+    find_package(MySQL REQUIRED)
 else( NOT INTERNAL_MYSQL )
-	set( LOCAL_MYSQL TRUE )
+    set( LOCAL_MYSQL TRUE )
 endif( NOT INTERNAL_MYSQL )
 
 # Check PostgreSQL or add Thirdparty
 if (NOT INTERNAL_POSTGRESQL)
-	find_package(PostgreSQL REQUIRED)
+    find_package(PostgreSQL REQUIRED)
 else (NOT INTERNAL_POSTGRESQL)
-	set ( LOCAL_POSTGRESQL TRUE )
-	set ( POSTGRESQL_INCLUDE_DIR ${FDOTHIRDPARTY}/pgsql/include/ )
-	set ( POSTGRESQL_LIBRARIES ${FDOTHIRDPARTY}/pgsql/lib/libpq.a )
-	set ( POSTGRESQL_FOUND TRUE )
-	mark_as_advanced ( POSTGRESQL_INCLUDE_DIR POSTGRESQL_LIBRARIES )
+    set ( LOCAL_POSTGRESQL TRUE )
+    set ( POSTGRESQL_INCLUDE_DIR ${FDOTHIRDPARTY}/pgsql/include/ )
+    set ( POSTGRESQL_LIBRARIES ${FDOTHIRDPARTY}/pgsql/lib/libpq.a )
+    set ( POSTGRESQL_FOUND TRUE )
+    mark_as_advanced ( POSTGRESQL_INCLUDE_DIR POSTGRESQL_LIBRARIES )
 endif (NOT INTERNAL_POSTGRESQL)
 
 # Check for Apache or ThirdParty
 if( NOT INTERNAL_APACHE )
 else( NOT INTERNAL_APACHE )
-	set( LOCAL_APACHE TRUE )
+    set( LOCAL_APACHE TRUE )
 endif( NOT INTERNAL_APACHE )
 
 # Check for cppunit or ThirdParty
 if( NOT INTERNAL_CPPUNIT )
-	find_package( CppUnit )
+    find_package( CppUnit )
 else( NOT INTERNAL_CPPUNIT )
-	set( LOCAL_CPPUNIT TRUE )
-	set( CPPUNIT_LIBRARY_DIR  ${FDOTHIRDPARTY}/linux/cppunit/lib )
-	set( CPPUNIT_INCLUDE_DIR  ${FDOTHIRDPARTY}/linux/cppunit/include )
-	find_library ( CPPUNIT_LIBRARIES NAMES cppunit
-		PATHS ${CPPUNIT_LIBRARY_DIR}
-	)
-	set( CPPUNIT_FOUND TRUE )
-	mark_as_advanced( CPPUNIT_LIBRARY_DIR CPPUNIT_INCLUDE_DIR )
+    set( LOCAL_CPPUNIT TRUE )
+    set( CPPUNIT_LIBRARY_DIR  ${FDOTHIRDPARTY}/linux/cppunit/lib )
+    set( CPPUNIT_INCLUDE_DIR  ${FDOTHIRDPARTY}/linux/cppunit/include )
+    find_library ( CPPUNIT_LIBRARIES NAMES cppunit
+        PATHS ${CPPUNIT_LIBRARY_DIR}
+        NO_DEFAULT_PATH
+    )
+    set( CPPUNIT_FOUND TRUE )
+    message (STATUS "Using internal cppunit: ${CPPUNIT_LIBRARIES}")
+    mark_as_advanced( CPPUNIT_LIBRARY_DIR CPPUNIT_INCLUDE_DIR )
 endif( NOT INTERNAL_CPPUNIT )
 
 # Check for xerces-c or ThirdParty
 if( NOT INTERNAL_XERCESC )
-	find_package( XercesC REQUIRED )
+    find_package( XercesC REQUIRED )
 else( NOT INTERNAL_XERCESC )
-	set( LOCAL_XERCESC TRUE )
-	set( XERCESC_LIBRARY_DIR  ${FDOTHIRDPARTY}/apache/xerces/lib/ )
-	set( XERCESC_INCLUDE_DIR  ${FDOTHIRDPARTY}/apache/xerces/src/ )
-	find_library( XERCESC_LIBRARIES NAMES xerces-c
-		PATHS ${XERCESC_LIBRARY_DIR}
-	)
-	set( XERCESC_FOUND TRUE )
-	mark_as_advanced( XERCESC_LIBRARY_DIR XERCESC_INCLUDE_DIR )
+    set( LOCAL_XERCESC TRUE )
+    set( XERCESC_LIBRARY_DIR  ${FDOTHIRDPARTY}/apache/xerces/src/.libs )
+    set( XERCESC_INCLUDE_DIR  ${FDOTHIRDPARTY}/apache/xerces/src/ )
+    find_library( XERCESC_LIBRARIES NAMES xerces-c
+        PATHS ${XERCESC_LIBRARY_DIR}
+        NO_DEFAULT_PATH
+    )
+    set( XERCESC_FOUND TRUE )
+    message (STATUS "Using internal xerces-c: ${XERCESC_LIBRARIES}")
+    mark_as_advanced( XERCESC_LIBRARY_DIR XERCESC_INCLUDE_DIR )
 endif( NOT INTERNAL_XERCESC )
 
 # Check for xalan-c or ThirdParty
 if( NOT INTERNAL_XALANC )
-	find_package( XalanC REQUIRED )
+    find_package( XalanC REQUIRED )
 else( NOT INTERNAL_XALANC )
-	set( LOCAL_XALANC TRUE )
-	set( XALANC_LIBRARY_DIR  ${FDOTHIRDPARTY}/apache/xalan/lib/ )
-	set( XALANC_INCLUDE_DIR  ${FDOTHIRDPARTY}/apache/xalan/src/ 
-		${FDOTHIRDPARTY}/apache/xalan/nls/include/
-	)
-	find_library( XALANC_LIBRARIES NAMES xalan-c xalanMsg
-		PATHS ${XALANC_LIBRARY_DIR}
-	)
-	set( XALANC_FOUND TRUE )
-	mark_as_advanced( XALANC_LIBRARY_DIR XALANC_INCLUDE_DIR )
+    set( LOCAL_XALANC TRUE )
+    set( XALANC_LIBRARY_DIR  ${FDOTHIRDPARTY}/apache/xalan/lib/ )
+    set( XALANC_INCLUDE_DIR  ${FDOTHIRDPARTY}/apache/xalan/src/ 
+        ${FDOTHIRDPARTY}/apache/xalan/nls/include/
+    )
+    find_library( XALANC_LIBRARIES NAMES xalan-c xalanMsg
+        PATHS ${XALANC_LIBRARY_DIR}
+        NO_DEFAULT_PATH
+    )
+    set( XALANC_FOUND TRUE )
+    message (STATUS "Using internal xalan-c: ${XALANC_LIBRARIES}")
+    mark_as_advanced( XALANC_LIBRARY_DIR XALANC_INCLUDE_DIR )
 endif( NOT INTERNAL_XALANC )
 
 # SqLite ThirdParty
@@ -104,53 +112,73 @@ mark_as_advanced( SQLITE_INCLUDE_DIR SQLITE_DIR )
 
 # Check for Boost or ThirdParty
 if( NOT INTERNAL_BOOST )
-	set (Boost_USE_MULTITHREADED ON) # be specific, we're after -mt versions of boost libs
-	find_package( Boost COMPONENTS thread date_time chrono system program_options REQUIRED )
+    set (Boost_USE_MULTITHREADED ON) # be specific, we're after -mt versions of boost libs
+    find_package( Boost COMPONENTS thread date_time chrono system program_options REQUIRED )
 else( NOT INTERNAL_BOOST )
-	set( LOCAL_BOOST TRUE )
-	set( Boost_INCLUDE_DIRS ${FDOTHIRDPARTY}/boost/ )
-	exec_program(
-	      ${CMAKE_CXX_COMPILER}
-	      ARGS                    -dumpversion
-	      OUTPUT_VARIABLE _compiler_output)
-	string(REGEX REPLACE ".* ([0-9]\\.[0-9]\\.[0-9]) .*" "\\1"
-	         gcc_compiler_version ${_compiler_output})
-	message (STATUS "GCC Version is: ${gcc_compiler_version}")
-	# HACK: I give up trying to grok find_library(), we want it to search the super-complex path under ${FDOTHIRDPARTY}
-	# but it still gives us /usr/lib. So set these directly
-	set (Boost_THREAD_LIBRARY ${FDOTHIRDPARTY}/boost/bin.v2/libs/thread/build/gcc-${gcc_compiler_version}/release/link-static/threading-multi/libboost_thread.a )
-	set (Boost_DATE_TIME_LIBRARY ${FDOTHIRDPARTY}/boost/bin.v2/libs/thread/build/gcc-${gcc_compiler_version}/release/link-static/threading-multi/libboost_date_time.a )
-	set (Boost_SYSTEM_LIBRARY ${FDOTHIRDPARTY}/boost/bin.v2/libs/thread/build/gcc-${gcc_compiler_version}/release/link-static/threading-multi/libboost_system.a )
-	set (Boost_CHRONO_LIBRARY ${FDOTHIRDPARTY}/boost/bin.v2/libs/thread/build/gcc-${gcc_compiler_version}/release/link-static/threading-multi/libboost_chrono.a )
-	set (Boost_PROGRAM_OPTIONS_LIBRARY ${FDOTHIRDPARTY}/boost/bin.v2/libs/thread/build/gcc-${gcc_compiler_version}/release/link-static/threading-multi/libboost_program_options.a )
-	set (Boost_THREAD_FOUND TRUE)
-	set (Boost_DATE_TIME_FOUND TRUE)
-	mark_as_advanced ( Boost_INCLUDE_DIRS Boost_THREAD_LIBRARY Boost_DATE_TIME_LIBRARY )
+    set( LOCAL_BOOST TRUE )
+    set( Boost_INCLUDE_DIRS ${FDOTHIRDPARTY}/boost/ )
+    exec_program(
+          ${CMAKE_CXX_COMPILER}
+          ARGS                    -dumpversion
+          OUTPUT_VARIABLE _compiler_output)
+    string(REGEX REPLACE ".* ([0-9]\\.[0-9]\\.[0-9]) .*" "\\1"
+             gcc_compiler_version ${_compiler_output})
+    message (STATUS "GCC Version is: ${gcc_compiler_version}")
+    find_library(Boost_THREAD_LIBRARY
+        NAMES boost_thread
+        PATHS ${FDOTHIRDPARTY}/boost/bin.v2/libs/thread/build/gcc-${gcc_compiler_version}/release/link-static/threading-multi
+        NO_DEFAULT_PATH
+    )
+    find_library(Boost_DATE_TIME_LIBRARY
+        NAMES boost_date_time
+        PATHS ${FDOTHIRDPARTY}/boost/bin.v2/libs/date_time/build/gcc-${gcc_compiler_version}/release/link-static/threading-multi
+        NO_DEFAULT_PATH
+    )
+    find_library(Boost_SYSTEM_LIBRARY
+        NAMES boost_system
+        PATHS ${FDOTHIRDPARTY}/boost/bin.v2/libs/system/build/gcc-${gcc_compiler_version}/release/link-static/threading-multi
+        NO_DEFAULT_PATH
+    )
+    find_library(Boost_CHRONO_LIBRARY
+        NAMES boost_chrono
+        PATHS ${FDOTHIRDPARTY}/boost/bin.v2/libs/chrono/build/gcc-${gcc_compiler_version}/release/link-static/threading-multi
+        NO_DEFAULT_PATH
+    )
+    find_library(Boost_PROGRAM_OPTIONS_LIBRARY
+        NAMES boost_program_options
+        PATHS ${FDOTHIRDPARTY}/boost/bin.v2/libs/program_options/build/gcc-${gcc_compiler_version}/release/link-static/threading-multi
+        NO_DEFAULT_PATH
+    )
+    message (STATUS "Using internal boost: ${Boost_THREAD_LIBRARY} ${Boost_DATE_TIME_LIBRARY} ${Boost_SYSTEM_LIBRARY} ${Boost_CHRONO_LIBRARY} ${Boost_PROGRAM_OPTIONS_LIBRARY}")
+    mark_as_advanced ( Boost_INCLUDE_DIRS Boost_THREAD_LIBRARY Boost_DATE_TIME_LIBRARY Boost_SYSTEM_LIBRARY Boost_CHRONO_LIBRARY Boost_PROGRAM_OPTIONS_LIBRARY)
 endif( NOT INTERNAL_BOOST )
 
 # Check for Curl or ThirdParty
 if( NOT INTERNAL_CURL )
-	find_package( CURL REQUIRED )
+    find_package( CURL REQUIRED )
 else( NOT INTERNAL_CURL )
-	set( LOCAL_CURL TRUE )
-	set( CURL_INCLUDE_DIRS ${FDOTHIRDPARTY}/libcurl/include/ )
-	find_library( CURL_LIBRARIES NAMES curl
-		PATHS ${FDOTHIRDPARTY}/libcurl/lib/ )
-	mark_as_advanced( CURL_INCLUDE_DIRS CURL_LIBRARIES )
+    set( LOCAL_CURL TRUE )
+    set( CURL_INCLUDE_DIRS ${FDOTHIRDPARTY}/libcurl/include/ )
+    find_library( CURL_LIBRARIES NAMES curl
+        PATHS ${FDOTHIRDPARTY}/libcurl/lib/linux
+        NO_DEFAULT_PATH
+    )
+    message (STATUS "Using internal curl: ${CURL_LIBRARIES}")
+    mark_as_advanced( CURL_INCLUDE_DIRS CURL_LIBRARIES )
 endif( NOT INTERNAL_CURL )
 
 # Check for OpenSSL or ThirdParty
 if( NOT INTERNAL_OPENSSL )
-	find_package( OpenSSL REQUIRED )
+    find_package( OpenSSL REQUIRED )
 else( NOT INTERNAL_OPENSSL )
-	set( LOCAL_OPENSSL TRUE )
-	set( OPENSSL_INCLUDE_DIR ${FDOTHIRDPARTY}/openssl/ )
-	set( OPENSSL_LIBRARY ${FDOTHIRDPARTY}/openssl/lib/linux/libssl.a )
-	set( OPENSSL_CRYPTO_LIBRARY ${FDOTHIRDPARTY}/openssl/lib/linux/libcrypto.a )
-	set( OPENSSL_LIBRARIES ${OPENSSL_LIBRARY}
-		${OPENSSL_CRYPTO_LIBRARY} )
-	message (STATUS "Using OpenSSL: ${OPENSSL_LIBRARIES}")
-	mark_as_advanced( OPENSSL_INCLUDE_DIR OPENSSL_LIBRARIES )
+    set( LOCAL_OPENSSL TRUE )
+    set( OPENSSL_INCLUDE_DIR ${FDOTHIRDPARTY}/openssl/ )
+    set( OPENSSL_LIBRARY ${FDOTHIRDPARTY}/openssl/lib/linux/libssl.a )
+    set( OPENSSL_CRYPTO_LIBRARY ${FDOTHIRDPARTY}/openssl/lib/linux/libcrypto.a )
+    set( OPENSSL_LIBRARIES ${OPENSSL_LIBRARY}
+        ${OPENSSL_CRYPTO_LIBRARY} )
+    message (STATUS "Using internal OpenSSL: ${OPENSSL_LIBRARIES}")
+    mark_as_advanced( OPENSSL_INCLUDE_DIR OPENSSL_LIBRARIES )
 endif( NOT INTERNAL_OPENSSL )
 
 # TODO
---
+++ cmake_build.sh
@@ -0,0 +1,175 @@
+#!/bin/sh
+
+CMAKE_BUILD_DIR=
+THIRDPARTY_WORK_DIR=
+BUILD_DBXML=0
+BUILD_LINUXAPT=0
+USE_NINJA=0
+SANITIZE_ADDRESS=OFF
+USE_LD_GOLD=OFF
+WITH_SDF=FALSE
+WITH_SHP=FALSE
+WITH_SQLITE=FALSE
+WITH_OGR=FALSE
+WITH_GDAL=FALSE
+WITH_WFS=FALSE
+WITH_WMS=FALSE
+WITH_GENERICRDBMS=FALSE
+WITH_KINGORACLE=FALSE
+ORACLE_INCLUDE=
+ORACLE_LIB=
+ORACLE_OCI_VERSION=110
+while [ $# -gt 0 ]; do    # Until you run out of parameters...
+    case "$1" in
+        --cmake_build_dir)
+            CMAKE_BUILD_DIR="$2"
+            shift
+            ;;
+        --thirdparty_working_dir)
+            THIRDPARTY_WORK_DIR="$2"
+            shift
+            ;;
+        --ninja)
+            USE_NINJA=1
+            ;;
+        --with-oracle-include)
+            ORACLE_INCLUDE="$2"
+            shift
+            ;;
+        --with-oracle-lib)
+            ORACLE_LIB="$2"
+            shift
+            ;;
+        --wtih-oci-version)
+            ORACLE_OCI_VERSION="$2"
+            shift
+            ;;
+        --with-asan)
+            SANITIZE_ADDRESS=ON
+            ;;
+        --with-all-providers)
+            WITH_SDF=TRUE
+            WITH_SHP=TRUE
+            WITH_SQLITE=TRUE
+            WITH_OGR=TRUE
+            WITH_GDAL=TRUE
+            WITH_WFS=TRUE
+            WITH_WMS=TRUE
+            WITH_GENERICRDBMS=TRUE
+            WITH_KINGORACLE=TRUE
+            ;;
+        --with-sdf)
+            WITH_SDF=TRUE
+            ;;
+        --with-shp)
+            WITH_SHP=TRUE
+            ;;
+        --with-sqlite)
+            WITH_SQLITE=TRUE
+            ;;
+        --with-ogr)
+            WITH_OGR=TRUE
+            ;;
+        --with-gdal)
+            WITH_GDAL=TRUE
+            ;;
+        --with-wfs)
+            WITH_WFS=TRUE
+            ;;
+        --with-wms)
+            WITH_WMS=TRUE
+            ;;
+        --with-genericrdbms)
+            WITH_GENERICRDBMS=TRUE
+            ;;
+        --with-kingoracle)
+            WITH_KINGORACLE=TRUE
+            ;;
+        --help)
+            echo "Usage: $0 (options)"
+            echo "Options:"
+            echo "  --cmake_build_dir [CMake build directory]"
+            echo "  --thirdparty_working_dir [thirdparty working directory]"
+            echo "  --with-asan [build with ASAN]"
+            echo "  --ninja [Use ninja build system]"
+            echo "  --with-sdf [Enable SDF Provider]"
+            echo "  --with-shp [Enable SHP Provider]"
+            echo "  --with-sqlite [Enable SQLite Provider]"
+            echo "  --with-ogr [Enable OGR Provider]"
+            echo "  --with-gdal [Enable GDAL Provider]"
+            echo "  --with-wfs [Enable WFS Provider]"
+            echo "  --with-wms [Enable WMS Provider]"
+            echo "  --with-genericrdbms [Enable GenericRdbms Providers]"
+            echo "  --with-kingoracle [Enable King Oracle Provider]"
+            echo "  --with-all-providers [Enable all providers]"
+            echo "  --with-oracle-include [Oracle client include path]"
+            echo "  --with-oracle-lib [Oracle client lib path]"
+            echo "  --with-oci-version [Oracle client version]"
+            echo "  --help [Display usage]"
+            exit
+            ;;
+    esac
+    shift   # Check next set of parameters.
+done
+
+SOURCE_DIR=`pwd`
+
+check_build()
+{
+    error=$?
+    if [ $error -ne 0 ]; then
+        echo "$LIB_NAME: Error build failed ($error)................."
+        # Return back to this dir before bailing
+        cd $SOURCE_DIR
+        exit $error
+    fi
+}
+
+if [ ! -f $THIRDPARTY_WORK_DIR/env_vars.sh ]; then
+    echo "ERROR: Could not find env_vars.sh in $THIRDPARTY_WORK_DIR"
+    echo "       This file should exist if you ran cmake_bootstrap.sh with (--oem_working_dir $THIRDPARTY_WORK_DIR)"
+    exit 1;
+fi
+
+echo "Reading variables from thirdparty build"
+source $THIRDPARTY_WORK_DIR/env_vars.sh
+
+# Validate
+if test "$BUILD_CPU" != "32" -a "$BUILD_CPU" != "64"; then
+    echo "--build must be 32 or 64"
+    exit 1;
+fi
+if test "$BUILD_CONFIG" != "Release" -a "$BUILD_CONFIG" != "Debug"; then
+    echo "--config must be Release or Debug"
+    exit 1;
+fi
+
+echo "Current source dir is: $SOURCE_DIR"
+echo "Building for: $BUILD_CPU-bit"
+echo "Building in: $BUILD_CONFIG"
+echo "Using Ninja build: $USE_NINJA"
+echo "CMake build directory is: $CMAKE_BUILD_DIR" 
+echo "Thirdparty Working Directory is: $THIRDPARTY_WORK_DIR"
+
+echo "Creating CMake build directory"
+mkdir -p $CMAKE_BUILD_DIR
+
+CMAKE_INTERNAL_ARGS="-DINTERNAL_CPPUNIT=$INTERNAL_CPPUNIT -DINTERNAL_GDAL=$INTERNAL_GDAL -DINTERNAL_OPENSSL=$INTERNAL_OPENSSL -DINTERNAL_MYSQL=$INTERNAL_MYSQL -DINTERNAL_POSTGRESQL=$INTERNAL_POSTGRESQL -DINTERNAL_BOOST=$INTERNAL_BOOST -DINTERNAL_CURL=$INTERNAL_CURL -DINTERNAL_XERCESC=$INTERNAL_XERCESC -DINTERNAL_XALANC=$INTERNAL_XALANC -DINTERNAL_THIRDPARTY_DIR=$THIRDPARTY_WORK_DIR"
+CMAKE_ARGS="-DFDO_CPU=$BUILD_CPU -DCMAKE_BUILD_TYPE=$BUILD_CONFIG -DUSE_LD_GOLD=$USE_LD_GOLD -DSANITIZE_ADDRESS=$SANITIZE_ADDRESS -DWITH_SDF=$WITH_SDF -DWITH_SHP=$WITH_SHP -DWITH_SQLITE=$WITH_SQLITE -DWITH_WFS=$WITH_WFS -DWITH_WMS=$WITH_WMS -DWITH_OGR=$WITH_OGR -DWITH_GDAL=$WITH_GDAL -DWITH_GENERICRDBMS=$WITH_GENERICRDBMS -DWITH_KINGORACLE=$WITH_KINGORACLE -DORACLE_PATH_INCLUDES=$ORACLE_INCLUDE -DORACLE_PATH_LIB=$ORACLE_LIB -DORACLE_OCI_VERSION=$ORACLE_OCI_VERSION $CMAKE_INTERNAL_ARGS"
+
+echo "CMake args: $CMAKE_ARGS"
+
+# Now for the main event
+echo "Setting up CMake in: $CMAKE_BUILD_DIR"
+cd $CMAKE_BUILD_DIR
+if test $USE_NINJA -eq 1; then
+    cmake -G Ninja $SOURCE_DIR $CMAKE_ARGS
+    check_build
+    ninja
+    check_build
+else
+    cmake $SOURCE_DIR $CMAKE_ARGS
+    check_build
+    make
+    check_build
+fi
