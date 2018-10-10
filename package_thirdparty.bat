50ac698352ff811bcc301fabfb3e24e673149182
Jackie Ng <jumpinjackie@gmail.com>
2018-10-10 14:33:35 +0000

    Add package_thirdparty.bat which collects all the dll/lib/pdbs files from a thirdparty build and packages up into a fdo_thirdparty.7z archive.
    On subsequent builds from a pristine working copy, one can simply extract the contents of this archive over the wc and proceed straight to the main FDO core/providers build.
    
    Fixes [https://trac.osgeo.org/fdo/ticket/959]
    
    git-svn-id: http://svn.osgeo.org/fdo/trunk@7727 d98eaee6-4212-0410-900c-dcc49bf69b99

---
+++ package_thirdparty.bat
@@ -0,0 +1,19 @@
+@echo off
+setlocal EnableExtensions EnableDelayedExpansion
+set "INTEXTFILE=thirdparty_filelist_temp.txt"
+set "OUTTEXTFILE=thirdparty_filelist.txt"
+set "SEARCHTEXT=%CD%\"
+set "REPLACETEXT="
+where /R Thirdparty\ *.dll *.lib *.pdb Xerces_autoconf_config.hpp LocalMsgIndex.hpp > %INTEXTFILE%
+rem Catch everything in gdal\include as well
+dir Thirdparty\gdal\include /S /B >> %INTEXTFILE%
+for /f "delims=" %%A in ('type "%INTEXTFILE%"') do (
+    set "string=%%A"
+    set "modified=!string:%SEARCHTEXT%=%REPLACETEXT%!"
+    echo !modified!>>"%OUTTEXTFILE%"
+)
+
+del "%INTEXTFILE%"
+rename "%OUTTEXTFILE%" "%INTEXTFILE%"
+7z a fdo_thirdparty.7z @%INTEXTFILE%
+endlocal
