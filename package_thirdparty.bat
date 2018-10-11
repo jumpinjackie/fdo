8fed46e228c5d1c3b0ca1cc9229c3ae0e369dd90
Jackie Ng <jumpinjackie@gmail.com>
2018-10-11 11:39:53 +0000

    [https://trac.osgeo.org/fdo/ticket/959]: Win32 debug build revealed that it modified opensslconf.h, so we should package that file as well.
    git-svn-id: http://svn.osgeo.org/fdo/trunk@7729 d98eaee6-4212-0410-900c-dcc49bf69b99

--- package_thirdparty.bat
+++ package_thirdparty.bat
@@ -4,7 +4,7 @@ set "INTEXTFILE=thirdparty_filelist_temp.txt"
 set "OUTTEXTFILE=thirdparty_filelist.txt"
 set "SEARCHTEXT=%CD%\"
 set "REPLACETEXT="
-where /R Thirdparty\ *.dll *.lib *.pdb Xerces_autoconf_config.hpp LocalMsgIndex.hpp > %INTEXTFILE%
+where /R Thirdparty\ *.dll *.lib *.pdb Xerces_autoconf_config.hpp LocalMsgIndex.hpp opensslconf.h > %INTEXTFILE%
 rem Catch everything in gdal\include as well
 dir Thirdparty\gdal\include /S /B >> %INTEXTFILE%
 for /f "delims=" %%A in ('type "%INTEXTFILE%"') do (
