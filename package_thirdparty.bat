e36882f787c79b3bfc71cdf610d08ba312589574
Jackie Ng <jumpinjackie@gmail.com>
2018-11-01 12:42:50 +0000

    Refine package_thirdparty.bat to not include junk pdbs and libs
    git-svn-id: http://svn.osgeo.org/fdo/trunk@7749 d98eaee6-4212-0410-900c-dcc49bf69b99

--- package_thirdparty.bat
+++ package_thirdparty.bat
@@ -4,6 +4,16 @@ set "INTEXTFILE=thirdparty_filelist_temp.txt"
 set "OUTTEXTFILE=thirdparty_filelist.txt"
 set "SEARCHTEXT=%CD%\"
 set "REPLACETEXT="
+rem These unused pdbs/libs will bloat our archive so delete them before packaging
+if exist Thirdparty\gdal\apps\*.pdb del Thirdparty\gdal\apps\*.pdb
+if exist Thirdparty\gdal\apps\*.lib del Thirdparty\gdal\apps\*.lib
+if exist Thirdparty\openssl\test\*.pdb del Thirdparty\openssl\test\*.pdb
+if exist Thirdparty\openssl\test\*.lib del Thirdparty\openssl\test\*.lib
+if exist Thirdparty\openssl\fuzz\*.pdb del Thirdparty\openssl\fuzz\*.pdb
+if exist Thirdparty\openssl\apps\*.pdb del Thirdparty\openssl\apps\*.pdb
+if exist Thirdparty\openssl\apps\*.lib del Thirdparty\openssl\apps\*.lib
+if exist Thirdparty\openssl\*.pdb del Thirdparty\openssl\*.pdb
+if exist Thirdparty\openssl\*.lib del Thirdparty\openssl\*.lib
 where /R Thirdparty\ *.dll *.lib *.pdb Xerces_autoconf_config.hpp LocalMsgIndex.hpp opensslconf.h > %INTEXTFILE%
 rem Catch everything in gdal\include as well
 dir Thirdparty\gdal\include /S /B >> %INTEXTFILE%
